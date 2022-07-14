1684d7130c4e872b3a2ef49cd183d2d405a2be5b
Bob van Luijt <bob@semi.technology>
2022-07-14 17:26:11 +0200

    Bump Slack users

--- js/main.js
+++ js/main.js
@@ -162,23 +162,23 @@ if(document.getElementById('homepage-stats-container')){
     // get stars
     getUrl('https://api.github.com/repos/semi-technologies/weaviate', 'text', function(status, data){
         var result = JSON.parse(data)['watchers'];
-        animateValue(document.getElementById('data-stargazers'), 0, result, 1820);
+        animateValue(document.getElementById('data-stargazers'), 0, result, 1120);
     });
     // set downloads
     getUrl('https://europe-west1-semi-production.cloudfunctions.net/docker-hub-pulls', 'text', function(status, data){
         var result = data;
         console.log(result);
         console.log(parseInt(result.replaceAll(',', '')));
-        animateValue(document.getElementById('data-downloads'), 0, parseInt(result.replaceAll(',', '')), 1820);
+        animateValue(document.getElementById('data-downloads'), 0, parseInt(result.replaceAll(',', '')), 1120);
     });
     // get countries
     getUrl('https://us-central1-semi-production.cloudfunctions.net/website-visitors', 'text', function(status, data){
         var result = data;
-        animateValue(document.getElementById('data-visitors'), 0, data, 1820);
+        animateValue(document.getElementById('data-visitors'), 0, data, 1120);
     });
     // get Slack users
-    var slackUsers = 856;
-    animateValue(document.getElementById('data-slack'), 0, slackUsers, 1820);
+    var slackUsers = 888;
+    animateValue(document.getElementById('data-slack'), 0, slackUsers, 1120);
 }
 
 // handle more info request
