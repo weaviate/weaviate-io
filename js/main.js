dae7b5b247a96dbaf0723fca4ce677b79a7a49eb
Bob van Luijt <bob@semi.technology>
2022-08-24 14:52:10 +0200

    update main

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ if(document.getElementById('homepage-stats-container')){
         animateValue(document.getElementById('data-visitors'), 0, data, 820);
     });
     // get Slack users
-    var slackUsers = 954;
+    var slackUsers = 986;
     animateValue(document.getElementById('data-slack'), 0, slackUsers, 820);
 }
 
