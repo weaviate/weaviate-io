ade90ea2d5a8136beef992d017910689fe728fed
Bob van Luijt <bob@semi.technology>
2022-09-26 16:39:53 +0200

    minor updates to roadmap

--- tools/create-roadmap.py
+++ tools/create-roadmap.py
@@ -8,16 +8,19 @@ labels = requests.get('https://api.github.com/repos/semi-technologies/weaviate/l
 for label in labels.json():
     if label['name'].find('backlog') >= 0 or label['name'].find('planned-') >= 0:
         include_labels.append(label['name'])
-        roadmap[label['name']] = []
+        roadmap[label['name']] = {
+            'description': label['description'],
+            'items': []
+        }
 
 # Collect all issues
 for label in include_labels:
     issues = requests.get('https://api.github.com/repos/semi-technologies/weaviate/issues?per_page=1000&labels=' + label)
     for issue in issues.json():
-        roadmap[label].append({
+        roadmap[label]['items'].append({
             'title': issue['title'],
             'url': issue['html_url'],
-            '+1': issue['reactions']['+1']
+            '+1': issue['reactions']['+1'],
         })
 
 # order 
