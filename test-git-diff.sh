a08ecb40cc82f217bdcfcabf8ea1685936bcac27
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-10-27 11:11:16 +0200

    test1

---
+++ test-git-diff.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# Set default ignore domains
+URL_IGNORES="/console.semi.technology/,/weaviate-newsletter.semi.technology/,/demo.dataset.playground.semi.technology/,/vectors.network/,/codepen.io/,/linkedin.com/,/twitter.com/,/t.co/,/arxiv.org/,/semi-technologies\/weaviate-io\/tree/,/computerhope.com/"
+
+# Get the main branch – this is needed as Travis does only a shallow fetch
+git remote set-branches --add origin main
+git fetch
+
+echo "HEAD: $(git merge-base origin/main HEAD)"
+
+# GET github diff between this branch and main
+read -ra DIFF_FILES <<< `git diff --name-only --diff-filter=AC $(git merge-base origin/main HEAD) developers/ | sed -e "s/ /\\\ /g"`
+
+# then add them all to a string containing all the new .html files that htmlproofer should ignore
+DIFF_IGNORES=""
+echo ======NEW_FILES_TO_IGNORE_BY_htmlproofer======
+for i in "${DIFF_FILES[@]}"; do
+    echo "DIFF FILE: $i"
+    if [[ $i =~ ^developers/.* ]]
+    then
+        FILE_TO_IGNORE=https://weaviate.io\/${i%.md}.html
+        DIFF_IGNORES="${DIFF_IGNORES},$FILE_TO_IGNORE"
+        echo $FILE_TO_IGNORE
+    fi
+done
+echo ==============================================
+
+STOP HERE
