0d587bd2e5b20423d593a15e699ac5db02e87b53
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-10-28 12:43:45 +0200

    update

--- _build_scripts/slack-release-message.sh
+++ _build_scripts/slack-release-message.sh
@@ -2,8 +2,6 @@
 set -e
 
 # Prepare the message and send it to Slack
-NETLIFY_LOC=$(grep -r 'Website Draft URL:' netlify.out)
-NETLIFY_LOC_STRP=$(echo ${NETLIFY_LOC:29})
 MESSAGE="{ \"text\": \"Hey $AUTHOR_NAME - your website is live at: 🔥 https://weaviate.io 🔥 \" }"
 
 echo $MESSAGE > payload.json
