7e0c8253ee2a46ba8869b768ae0fcf2f6a4a3e83
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-11-02 19:55:47 +0100

    update slack messaging

--- _build_scripts/slack-netlify-message.sh
+++ _build_scripts/slack-netlify-message.sh
@@ -6,7 +6,7 @@ NETLIFY_LOC=$(grep -r 'Website Draft URL:' netlify.out)
 NETLIFY_LOC_STRP=$(echo ${NETLIFY_LOC:29})
 MESSAGE="{ \"text\": \"Hey $AUTHOR_NAME - your website (\`$TRAVIS_BRANCH\`) is live at: $NETLIFY_LOC_STRP \" }"
 
-echo $MESSAGE > payload.json
+echo $MESSAGE > payload_netlify.json
 
 # Send the slack message
-curl -X POST -H 'Content-type: application/json' -d @payload.json https://hooks.slack.com/services/$SLACK_BOT
+curl -X POST -H 'Content-type: application/json' -d @payload_netlify.json https://hooks.slack.com/services/$SLACK_BOT
--- _build_scripts/slack-release-message.sh
+++ _build_scripts/slack-release-message.sh
@@ -4,7 +4,7 @@ set -e
 # Prepare the message and send it to Slack
 MESSAGE="{ \"text\": \"Hey $AUTHOR_NAME - your website is live at: 🔥 https://weaviate.io 🔥 \" }"
 
-echo $MESSAGE > payload.json
+echo $MESSAGE > payload_release.json
 
 # Send the slack message
-curl -X POST -H 'Content-type: application/json' -d @payload.json https://hooks.slack.com/services/$SLACK_BOT
+curl -X POST -H 'Content-type: application/json' -d @payload_release.json https://hooks.slack.com/services/$SLACK_BOT
