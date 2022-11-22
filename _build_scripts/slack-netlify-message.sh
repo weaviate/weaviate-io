5b7c7d9173a74f31972a6fd40aaa08284d71c60a
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-11-22 10:41:23 +0100

    testing <message> & push

--- _build_scripts/slack-netlify-message.sh
+++ _build_scripts/slack-netlify-message.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
+# Get commit message
+commit_message="$(git log -1 $TRAVIS_COMMIT --pretty="%s%b")"
+
+# Replace &, <, and > – as per Slack API instructions
+commit_message=${commit_message//&/&amp;}
+commit_message=${commit_message//</&lt;}
+commit_message=${commit_message//>/&gt;}
+
 # Prepare the message and send it to Slack
 NETLIFY_LOC=$(grep -r 'Website Draft URL:' netlify.out)
 NETLIFY_LOC_STRP=$(echo ${NETLIFY_LOC:29})
-MESSAGE="{ \"text\": \"Hey $AUTHOR_NAME - your *weaviate website* build (\`$TRAVIS_BRANCH\`) is live at: $NETLIFY_LOC_STRP \" }"
+MESSAGE="{ \"text\": \"Hey $AUTHOR_NAME - your *weaviate website* build (\`$TRAVIS_BRANCH\`) is live at: $NETLIFY_LOC_STRP\n> $commit_message \" }"
 
 echo $MESSAGE > payload_netlify.json
 
