2995a56595314ad61ce2afecdc3524e83bfb06d0
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-11-24 18:42:39 +0100

    add message to slack

--- _build_scripts/publish-to-netlify.sh
+++ _build_scripts/publish-to-netlify.sh
@@ -6,16 +6,9 @@ mv build/robots.txt build/robots.txt.live
 echo -e "User-agent: *
 Disallow: /" >> build/robots.txt
 
-
 # sudo netlify deploy --dir=build --site=tangerine-buttercream-20c32f >> netlify.out
 netlify deploy --dir=build --site=tangerine-buttercream-20c32f >> netlify.out
 
 # Bring back the original robots.txt file
 rm build/robots.txt 
 mv build/robots.txt.live build/robots.txt
-
-# Temp share link to netlify
-NETLIFY_LOC=$(grep -r 'Website Draft URL:' netlify.out)
-NETLIFY_LOC_STRP=$(echo ${NETLIFY_LOC:29})
-
-echo $NETLIFY_LOC_STRP
