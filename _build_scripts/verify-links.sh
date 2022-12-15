e136b59ead229b7a058420f155bcf8546d929101
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-12-15 17:51:06 +0100

    final final test

--- _build_scripts/verify-links.sh
+++ _build_scripts/verify-links.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 
-# Note, |github.com/.*github.com/| is to ignore meta links that include blog co-authors
-URL_IGNORES="jsonlines.org/|arxiv.org/|github.com/.*github.com/|huggingface.co/"
+URL_IGNORES="jsonlines.org/|arxiv.org/|huggingface.co/"
+URL_IGNORES_DOCUSAURUS="github.com/.*github.com/|github.com/semi-technologies/weaviate-io/tree/"
+# Note #1 github.com/.*github.com/ - is to ignore meta links that include blog co-authors
+# Note #2 github.com/semi-technologies/weaviate-io/tree/ - is for edit on github links
 
 echo "**************************************
 Starting Link Verification
 PATH ${NETLIFY_URL}
-URL_IGNORES ${URL_IGNORES}
+URL_IGNORES ${URL_IGNORES}|${URL_IGNORES_DOCUSAURUS}
 **************************************"
 
 linkinator $NETLIFY_URL \
 --recurse \
---skip "${URL_IGNORES}" \
+--skip "${URL_IGNORES}|${URL_IGNORES_DOCUSAURUS}" \
 --timeout 10000 \
 --verbosity error \
 --url-rewrite-search "https://weaviate.io" \
