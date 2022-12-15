a295775c97e3af6986d889a2152fa6610726e94a
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-12-15 15:42:35 +0100

    test

--- _build_scripts/verify-links.sh
+++ _build_scripts/verify-links.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-linkinator $NETLIFY_URL
+linkinator $NETLIFY_URL \
+--recurse \
+--skip 'weaviate.io|github.com/*.,|arxiv.org/|jsonlines.org/' \
+--timeout 10000 \
+--verbosity error
