f0d38db6cd707e92894f33cddeebdbef5bab9323
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-12-15 18:00:40 +0100

    output broken links as JSON

--- _build_scripts/verify-links.sh
+++ _build_scripts/verify-links.sh
@@ -2,20 +2,27 @@
 set -e
 
 URL_IGNORES="jsonlines.org/|arxiv.org/|huggingface.co/"
-URL_IGNORES_DOCUSAURUS="github.com/.*github.com/|github.com/semi-technologies/weaviate-io/tree/"
+DOCUSAURUS_IGNORES="github.com/.*github.com/|github.com/semi-technologies/weaviate-io/tree/"
 # Note #1 github.com/.*github.com/ - is to ignore meta links that include blog co-authors
 # Note #2 github.com/semi-technologies/weaviate-io/tree/ - is for edit on github links
 
 echo "**************************************
 Starting Link Verification
-PATH ${NETLIFY_URL}
-URL_IGNORES ${URL_IGNORES}|${URL_IGNORES_DOCUSAURUS}
+PATH: ${NETLIFY_URL}
+URL_IGNORES: ${URL_IGNORES}|${DOCUSAURUS_IGNORES}
 **************************************"
 
 linkinator $NETLIFY_URL \
 --recurse \
---skip "${URL_IGNORES}|${URL_IGNORES_DOCUSAURUS}" \
+--skip "${URL_IGNORES}|${DOCUSAURUS_IGNORES}" \
 --timeout 10000 \
 --verbosity error \
 --url-rewrite-search "https://weaviate.io" \
---url-rewrite-replace "${NETLIFY_URL}"
+--url-rewrite-replace "${NETLIFY_URL}" \
+--format JSON
+
+# USE search/replace to test validity of links on Nelify, as they might not yet exist on weaviate.io
+
+echo "**************************************
+Link Verification Complete
+**************************************"
