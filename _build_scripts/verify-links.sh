6ac923565321cb9bb4e6df5fbdda4631c138bef1
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-12-15 16:05:56 +0100

    another take

--- _build_scripts/verify-links.sh
+++ _build_scripts/verify-links.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "BEFORE"
+
 URL_IGNORES="weaviate.io|github.com/*.,|arxiv.org/|jsonlines.org/"
 
-linkinator $NETLIFY_URL --recurse --skip $URL_IGNORES --timeout 10000 --verbosity error
+linkinator $NETLIFY_URL --recurse --skip "${URL_IGNORES}" --timeout 10000 --verbosity error
+
+echo "AFTER"
