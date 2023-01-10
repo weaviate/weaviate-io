7c84c8dd944a2fccc31e293d6d9ee4e002c4fb02
Wera <weronika@semi.technology>
2023-01-10 16:34:27 +0100

    add tabs on homepage

--- src/components/HomepageWhatYouCanDo/index.jsx
+++ src/components/HomepageWhatYouCanDo/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import * as Tabs from '@radix-ui/react-tabs';
 
 export default function HomepageWhatYouCanDo() {
   return (
@@ -12,29 +13,51 @@ export default function HomepageWhatYouCanDo() {
         </p>
       </div>
       <div className={styles.module}>
-        <div className={styles.info}>
-          <div className={styles.leftBorder}>
-            <h3>Search</h3>
-            <p>
-              Go beyond keyword matching, find meaning in antything <br /> from
-              images to molecules.
-            </p>
+        <Tabs.Root className={styles.tabs} defaultValue="tab1">
+          <div className={styles.left}>
+            <Tabs.List
+              className={styles.tabsList}
+              aria-label="What you can do with Weaviate"
+            >
+              <Tabs.Trigger
+                className={styles.tabsTrigger}
+                value="tab1"
+                disabled={false}
+              >
+                <h3>Search</h3>
+                <p>
+                  Go beyond keyword matching, find meaning in antything <br />{' '}
+                  from images to molecules.
+                </p>
+              </Tabs.Trigger>
+              <Tabs.Trigger className={styles.tabsTrigger} value="tab2">
+                <h3>Recommendations</h3>
+                <p>
+                  Provide top match recommendations seamlessly with minimal
+                  setup.
+                </p>
+              </Tabs.Trigger>
+              <Tabs.Trigger className={styles.tabsTrigger} value="tab3">
+                <h3>Integrations</h3>
+                <p>
+                  Use AI models, datasets and services such as GTP-3, Wikipedia
+                  and Jira.
+                </p>
+              </Tabs.Trigger>
+            </Tabs.List>
           </div>
-          <div>
-            <h3>Recomendations</h3>
-            <p>
-              Provide top match recommendations seamlessly with minimal setup.
-            </p>
+          <div className={styles.right}>
+            <Tabs.Content className={styles.tabsContent} value="tab1">
+              <p className={styles.code}></p>
+            </Tabs.Content>
+            <Tabs.Content className={styles.tabsContent} value="tab2">
+              <p className={styles.code}></p>
+            </Tabs.Content>
+            <Tabs.Content className={styles.tabsContent} value="tab3">
+              <p className={styles.code}></p>
+            </Tabs.Content>
           </div>
-          <div>
-            <h3>Integrations</h3>
-            <p>
-              Use AI models, datasets and services such as GTP-3, Wikipedia and
-              Jira.
-            </p>
-          </div>
-        </div>
-        <div className={styles.code}></div>
+        </Tabs.Root>
       </div>
     </div>
   );
