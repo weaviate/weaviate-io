7f913042d02c85a4088be696f83a3464a038bf52
Wera <weronika@semi.technology>
2023-01-11 12:22:58 +0100

    newsletter desc

--- src/components/HomepageNewsletter/index.jsx
+++ src/components/HomepageNewsletter/index.jsx
@@ -5,6 +5,10 @@ export default function HomepageNewsletter() {
   return (
     <div className="container">
       <div className={styles.box}>
+        <div className={styles.text}>
+          <h2>Stay updated</h2>
+          <span>Subscribe for our newsletter</span>
+        </div>
         <div className={styles.iframe}>
           <iframe
             src="https://embeds.beehiiv.com/15b21ebd-decd-433b-ada8-2d405e345f2e?slim=true"
