e3bc36c6857d0a9e0130a8b13fa13c342feb0061
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-01-13 18:52:10 +0100

    Revert "test without netlify"
    This reverts commit 169b872c2cb4e94a4d9f4f33db8c91920c86661d.

--- _build_scripts/verify-links.sh
+++ _build_scripts/verify-links.sh
@@ -8,10 +8,11 @@ DOCUSAURUS_IGNORES="github.com/.*github.com/|github.com/semi-technologies/weavia
 
 echo "**************************************
 Starting Link Verification
+PATH: ${NETLIFY_URL}
 URL_IGNORES: ${URL_IGNORES}|${DOCUSAURUS_IGNORES}
 **************************************"
 
-linkinator https://63c1935248a6be2b9bab8a86--tangerine-buttercream-20c32f.netlify.app \
+linkinator $NETLIFY_URL \
 --recurse \
 --skip "${URL_IGNORES}|${DOCUSAURUS_IGNORES}" \
 --timeout 5000 \
