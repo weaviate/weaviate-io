79c6b3c95a009b77f4e92998b834b11ed05ee584
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-01-13 17:46:41 +0100

    test plausible

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,10 +66,11 @@ const config = {
                         },
                     },
                     // Add hotjar
-                    {
-                        tagName: 'script',
-                        innerHTML: `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3237492,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
-                    }],
+                    // {
+                    //     tagName: 'script',
+                    //     innerHTML: `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3237492,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+                    // }
+                ],
                 };
             },
         })
