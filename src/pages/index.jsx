4ed14203eabc62912a86e59b2423c4713d0955f4
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-01-17 16:46:52 +0100

    refactor home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,16 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import { MetaSEO } from '@site/src/theme/MetaSEO';
 
-import HomepageHeader from '@site/src/components/HomepageHeader';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import HomepageWhatYouCanDo from '@site/src/components/HomepageWhatYouCanDo';
-import HomepageLearn from '@site/src/components/HomepageLearn';
-import HomepageJoinCommunity from '@site/src/components/HomepageJoinCommunity';
-import HomepageReady from '@site/src/components/HomepageReady';
-import HomepageNewsletter from '@site/src/components/HomepageNewsletter';
-import HomepageCompany from '@site/src/components/HomepageCompany';
-import HomepageIntegrations from '@site/src/components/HomepageIntegrations';
-import HomepageTestimonials from '@site/src/components/HomepageTestimonials';
+import HomepageHeader from '@site/src/components/Home/Header';
+import HomepageFeatures from '@site/src/components/Home/Features';
+import HomepageWhatYouCanDo from '@site/src/components/Home/WhatYouCanDo';
+import HomepageLearn from '@site/src/components/Home/Learn';
+import HomepageJoinCommunity from '@site/src/components/Home/JoinCommunity';
+import HomepageReady from '@site/src/components/Home/Ready';
+import HomepageNewsletter from '@site/src/components/Home/Newsletter';
+import HomepageCompany from '@site/src/components/Home/Company';
+import HomepageIntegrations from '@site/src/components/Home/Integrations';
+import HomepageTestimonials from '@site/src/components/Home/Testimonials';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
