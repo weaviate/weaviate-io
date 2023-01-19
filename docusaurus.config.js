f8aba9db376e70f68a925b63530058837d804c9b
Bob van Luijt <bob@semi.technology>
2023-01-19 18:38:28 +0100

    Set organizationName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'semi-technologies', // Usually your GitHub org/user name.
+    organizationName: 'weaviate', // Usually your GitHub org/user name.
     projectName: 'weaviate-io', // Usually your repo name.
     plugins: [
         'docusaurus-plugin-sass',
