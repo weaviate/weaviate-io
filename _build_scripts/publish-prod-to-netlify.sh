b3f17c65ffd914c14972cd867f2fb60d8c15aeb4
Bob van Luijt <bob@weaviate.io>
2023-01-22 15:12:33 +0300

    tmp robots block

--- _build_scripts/publish-prod-to-netlify.sh
+++ _build_scripts/publish-prod-to-netlify.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 set -e
 
+##
+# Remove when in prod
+##
+# For Netlify - change robot.txt to Disallow - so that crawlers will ignore it
+mv build/robots.txt build/robots.txt.live
+echo -e "User-agent: *
+Disallow: /" >> build/robots.txt
+##
+# End REMOVE block
+##
+
 # deploy
 netlify deploy --prod --dir=build --site=weaviate-io
+
+##
+# Remove when in prod
+##
+# Bring back the original robots.txt file
+rm build/robots.txt 
+mv build/robots.txt.live build/robots.txt
+##
+# End REMOVE block
+##
