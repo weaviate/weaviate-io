c33af45f86c2d18c73c18678639f46ae307e8ea0
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-02-28 18:41:40 +0100

    remove wrong import

--- src/remark/remark-replace.js
+++ src/remark/remark-replace.js
@@ -1,6 +1,5 @@
 const visit = require('unist-util-visit');
 const fs = require('fs');
-const { config } = require('process');
 
 // Read file config, which contains weaviate_version, etc.
 const readSiteConfig = () => {
