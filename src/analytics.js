547572883d5cd8f9af5792cfe6b62f39c0e1cfb0
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-03-14 15:28:55 +0100

    [site] Update site search analytics - for search result selected (#543)
    * add analytics for selected search results

--- src/analytics.js
+++ src/analytics.js
@@ -1,4 +1,9 @@
-export const captureSiteSearchAnalytics = (searchTerm) =>
+export const analyticsSiteSearched = (searchTerm) =>
     plausible('Site Search', {
         props: { searchTerm }
+    })
+
+export const analyticsSiteSearchSelected = (searchTerm, uri) =>
+    plausible('Site Search Selected', {
+        props: { searchTerm, uri }
     })
