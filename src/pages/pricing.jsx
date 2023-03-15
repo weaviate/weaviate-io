c5bcd5d2afacacb5d237f3d00f48c9994263cdfd
Wera <41889633+Weroiko@users.noreply.github.com>
2023-03-15 18:39:48 +0100

    pricing page one more time (#546)
    * pricing page one more time
    
    * Update text
    
    ---------
    
    Co-authored-by: Wera <weronika@semi.technology>
    Co-authored-by: Bob van Luijt <bob@weaviate.io>

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '@theme/Layout';
-import { MetaSEO } from '/src/theme/MetaSEO';
-
-import PricingHeader from '/src/components/Pricing/Header';
-import PricingCalculator from '/src/components/Pricing/Calculator';
-import PricingPlan from '/src/components/Pricing/Plan';
-import PricingFAQ from '/src/components/Pricing/FAQ';
-import ContactUsForm from '/src/components/ContactUsForm';
+import PricingHeader from '../components/Pricing/Header';
+import PricingCalculator from '../components/Pricing/Calculator';
+import PricingPlan from '../components/Pricing/Plan';
+import PricingFAQ from '../components/Pricing/FAQ';
+import ContactUsForm from '../components/ContactUsForm';
+import HybridBusinessCritical from '../components/Pricing/HybridBusinessCritical';
 
 export default function PricingPage() {
+  const [selectedType, setSelectedType] = useState('saas');
+
   return (
     <div className="custom-page">
       <Layout>
-        <MetaSEO img="og/wcs/_title.jpg" />
-        <PricingHeader />
-        <PricingCalculator />
-        <PricingPlan />
+        <PricingHeader
+          selectedType={selectedType}
+          handleSelected={setSelectedType}
+        />
+        {selectedType === 'saas' ? (
+          <>
+            <PricingPlan />
+            <PricingCalculator />
+          </>
+        ) : (
+          <HybridBusinessCritical />
+        )}
         <PricingFAQ />
         <ContactUsForm />
       </Layout>
