71042953d675619c8247f614366c284650458a33
Dan Dascalescu <dan@semi.technology>
2023-03-21 17:42:55 +0100

    Rebase onto main

--- src/components/GiscusComment/index.jsx
+++ src/components/GiscusComment/index.jsx
@@ -2,27 +2,45 @@ import React from 'react';
 import Giscus from "@giscus/react";
 import { useColorMode } from '@docusaurus/theme-common';
 import { useDoc } from '@docusaurus/theme-common/internal';
+import { useBlogPost } from '@docusaurus/theme-common/internal';
 
-export default function GiscusComment() {
+const GiscusParams = {
+  repo: "weaviate/weaviate-io",
+  repoId: "R_kgDOG2Daiw",
+  category: "Docs/blog comments",
+  categoryId: "DIC_kwDOG2Dai84CVAeq",
+  mapping: "specific",
+  strict: "0",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  lang: "en",
+  crossorigin: "anonymous",
+  loading: "eager",
+};
+
+export function GiscusDocComment() {
   const { colorMode } = useColorMode();
   const { frontMatter } = useDoc();
 
   return (
-      <Giscus
-        repo="weaviate/weaviate-io"
-        repoId="R_kgDOG2Daiw"
-        category="Docs/blog comments"
-        categoryId="DIC_kwDOG2Dai84CVAeq"
-        mapping="specific"
-        term={frontMatter.title}
-        strict="0"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="bottom"
-        theme={colorMode}
-        lang="en"
-        crossorigin="anonymous"
-        loading="eager"
-      />
+    <Giscus
+      {...GiscusParams}
+      term={frontMatter.title}
+      theme={colorMode}
+    />
+  );
+}
+
+export function GiscusBlogPostComment() {
+  const { colorMode } = useColorMode();
+  const { frontMatter } = useBlogPost();
+
+  return (
+    <Giscus
+      {...GiscusParams}
+      term={frontMatter.title}
+      theme={colorMode}
+    />
   );
 }
