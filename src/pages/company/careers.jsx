05c7fd35ef42214091a53321f9c61e060783723a
Igor Lamas <igorlamas1994@gmail.com>
2023-04-03 21:16:11 -0300

    error ssr new page

--- src/pages/company/careers.jsx
+++ src/pages/company/careers.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Layout from '@theme/Layout';
+import { MetaSEO } from '/src/theme/MetaSEO';
+
 import CareersHeader from '../../components/Careers/Header';
 import CareersIntro from '../../components/Careers/Intro';
 import CareersInterviewProcess from '../../components/Careers/InterviewProcess';
@@ -10,6 +12,7 @@ export default function CareersPage() {
   return (
     <div className="custom-page">
       <Layout>
+        <MetaSEO img="og/company/careers.jpg" />
         <CareersHeader />
         <CareersIntro />
         <CareersInterviewProcess />
