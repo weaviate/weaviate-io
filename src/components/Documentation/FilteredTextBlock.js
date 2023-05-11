774a456a6068ea80cd6f2b2b100bfb3a43099ad0
JP Hwang <61258750+databyjp@users.noreply.github.com>
2023-05-11 23:25:47 +1000

    Add optional flag includestartMarker in FilteredTextBlock

--- src/components/Documentation/FilteredTextBlock.js
+++ src/components/Documentation/FilteredTextBlock.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
 
-const FilteredTextBlock = ({ text, startMarker, endMarker, language }) => {
+const FilteredTextBlock = ({ text, startMarker, endMarker, language, includeStartMarker='false' }) => {
   // Filter out lines that are before the start marker, and lines with or after the end marker
+  includeStartMarker = includeStartMarker == 'true';
   const lines = text.split('\n');
   let withinMarkers = false;
   const filteredLines = lines
     .filter((line) => {
       if (line.includes(startMarker)) {
         withinMarkers = true;
-        return false;
+        return includeStartMarker;
       }
 
       if (line.includes(endMarker)) {
