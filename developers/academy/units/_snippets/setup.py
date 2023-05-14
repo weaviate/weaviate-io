c1e714740eaf53b8228b7291da79efb1c72478ab
JP Hwang <me@jphwang.com>
2023-05-14 11:10:59 +1000

    [docs] fix additional instances of auth.AuthApiKey

--- developers/academy/units/_snippets/setup.py
+++ developers/academy/units/_snippets/setup.py
@@ -3,7 +3,7 @@ import weaviate
 
 client = weaviate.Client(
     url="https://edu-demo.weaviate.network",
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key="learn-weaviate"),  # A read-only API Key for the Weaviate instance
+    auth_client_secret=weaviate.AuthApiKey(api_key="learn-weaviate"),  # A read-only API Key for the Weaviate instance
 )
 
 client.is_ready()  # This should return `True`
@@ -15,7 +15,7 @@ import weaviate
 
 client = weaviate.Client(
     url="https://edu-demo.weaviate.network",
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key="learn-weaviate"),  # A read-only API Key for the Weaviate instance
+    auth_client_secret=weaviate.AuthApiKey(api_key="learn-weaviate"),  # A read-only API Key for the Weaviate instance
     additional_headers={
         "X-OpenAI-Api-Key": "YOUR-OPENAI-API-KEY",  # Replace this with YOUR OpenAI API key
     }
@@ -43,7 +43,7 @@ import json
 
 client = weaviate.Client(
     url="https://edu-demo.weaviate.network",
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key="learn-weaviate"),  # A read-only API Key for the Weaviate instance
+    auth_client_secret=weaviate.AuthApiKey(api_key="learn-weaviate"),  # A read-only API Key for the Weaviate instance
     additional_headers={
         "X-OpenAI-Api-Key": "YOUR-OPENAI-API-KEY",  # Replace this with YOUR OpenAI API key
     }
