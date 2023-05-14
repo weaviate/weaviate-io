6b4a99746a7d6d1e60ab4fa6fc59c74b6227c372
JP Hwang <me@jphwang.com>
2023-05-15 00:11:31 +1000

    update quickstart js/ts w async await and add tests

--- tests/test_quickstart.py
+++ tests/test_quickstart.py
@@ -1,3 +1,4 @@
+import subprocess
 import pytest
 import utils
 
@@ -12,3 +13,33 @@ import utils
 def test_python_script(empty_weaviates, script_loc):
     proc_script = utils.load_and_prep_script(script_loc)
     exec(proc_script)
+
+
+@pytest.mark.parametrize(
+    "script_loc",
+    [
+        "./_includes/code/quickstart.autoschema.endtoend.js",
+    ],
+)
+def test_js(empty_weaviates, script_loc):
+    try:
+        # If the script throws an error, this will raise a CalledProcessError
+        subprocess.check_call(['node', script_loc])
+    except subprocess.CalledProcessError as error:
+        pytest.fail(f'Script {script_loc} failed with error: {error}')
+
+
+@pytest.mark.parametrize(
+    "script_loc",
+    [
+        "./_includes/code/quickstart.autoschema.endtoend.ts",
+    ],
+)
+def test_ts(empty_weaviates, script_loc):
+    command = ["npx", "ts-node", "-O", '{ "module": "commonjs" }', script_loc]
+
+    try:
+        # If the script throws an error, this will raise a CalledProcessError
+        subprocess.check_call(command)
+    except subprocess.CalledProcessError as error:
+        pytest.fail(f'Script {script_loc} failed with error: {error}')
