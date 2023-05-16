bdaf9ee578912950adbf43fe606892d8d4c8ecd6
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-05-16 15:56:48 +0200

    add tags to blogs

--- src/theme/BlogPostItems/index.js
+++ src/theme/BlogPostItems/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BlogPostProvider} from '@docusaurus/theme-common/internal';
 import BlogPostItem from '@theme/BlogPostItem';
 import styles from './styles.module.scss';
-import { LinkButton, ButtonContainer } from '/src/theme/Buttons';
+
 export default function BlogPostItems({
   items,
   component: BlogPostItemComponent = BlogPostItem,
@@ -30,10 +30,18 @@ export default function BlogPostItems({
                 {BlogPostContent.metadata.description}
               </p>
 
+              {BlogPostContent.metadata.tags.length > 0 &&
+              <div className={styles.blogTags}>
+                <span className={styles.blogTagsLabel}>Tags:</span>
+                {BlogPostContent.metadata.tags.map(tag=>(
+                  <a className={styles.blogTag} href={tag.permalink}>{tag.label}</a>
+                ))}
+              </div>
+              }
+
               <div className={styles.blogCardInfo}>
                 <span>{BlogPostContent.metadata.formattedDate} · {Math.round(BlogPostContent.metadata.readingTime)} min read</span>
               </div>
-              {/* <BlogPostContent /> */}
             </div>
           </BlogPostItemComponent>
         </BlogPostProvider>
