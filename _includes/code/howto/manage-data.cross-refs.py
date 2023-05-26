1fc54ae532e25e09113142c1a3ac38a55985ec91
JP Hwang <me@jphwang.com>
2023-05-26 14:51:50 +1000

    bugfix in assert code

--- _includes/code/howto/manage-data.cross-refs.py
+++ _includes/code/howto/manage-data.cross-refs.py
@@ -16,16 +16,16 @@ museums_id = "fec50326-dfa1-53c9-90e8-63d0240bd933"
 # https://stackoverflow.com/questions/76171703/in-weaviate-how-to-remove-a-property-in-existing-class/76177363#76177363
 # class_name is required to avoid the warning to use "class namespaced APIs"
 def del_prop(uuid: str, prop_name: str, class_name: str) -> None:
+    global client
     object_data = client.data_object.get(uuid, class_name=class_name)
     if prop_name in object_data["properties"]:
-        del object_data["properties"][prop_name]
+        object_data["properties"].pop(prop_name)
     client.data_object.replace(object_data["properties"], class_name, uuid)
 
 
-# Instantiate the client with the user/password and OpenAI api key
+# Instantiate the client anonymously
 client = weaviate.Client(
-    "http://localhost:8080",  # Replace with your Weaviate URL
-    auth_client_secret=weaviate.AuthApiKey("YOUR-WEAVIATE-API-KEY"),  # If authentication is on. Replace w/ your Weaviate instance API key
+    "https://anon-endpoint.weaviate.network",  # Replace with your Weaviate URL
 )
 
 # ===========================================================
@@ -72,24 +72,24 @@ if not client.schema.contains({"classes": class_definitions}):
 # OneWay Python
 client.data_object.reference.add(
     from_class_name="JeopardyQuestion",
-    from_uuid="00ff6900-e64f-5d94-90db-c8cfa3fc851b",
+    from_uuid=sf_id,
     from_property_name="hasCategory",
     to_class_name="JeopardyCategory",
-    to_uuid="20ffc68d-986b-5e71-a680-228dba18d7ef",
+    to_uuid=us_cities_id,
 )
 # END OneWay Python
 
 # Test results
 sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion")
 assert sf["class"] == "JeopardyQuestion"
-assert any(category["href"] == f"/v1/objects/JeopardyCategory/{us_cities_id}"
-           for category in sf["properties"]["hasCategory"])
+xrefs = [category["href"] for category in sf["properties"]["hasCategory"]]
+assert f"/v1/objects/JeopardyCategory/{us_cities_id}" in xrefs
 # END Test results
 
 
-# =======================================
-# ===== Add bidirectional cross-ref =====
-# =======================================
+# # =======================================
+# # ===== Add bidirectional cross-ref =====
+# # =======================================
 
 # Delete any existing cross-references from the source and target objects
 del_prop(sf_id, "hasCategory", "JeopardyQuestion")
@@ -127,10 +127,10 @@ us_cities = client.data_object.get(uuid=us_cities_id, class_name="JeopardyCatego
 
 assert sf["class"] == "JeopardyQuestion"
 assert us_cities["class"] == "JeopardyCategory"
-assert any(category["href"] == "/v1/objects/JeopardyCategory/20ffc68d-986b-5e71-a680-228dba18d7ef"
-           for category in sf["properties"]["hasCategory"])
-assert any(category["href"] == "/v1/objects/JeopardyQuestion/00ff6900-e64f-5d94-90db-c8cfa3fc851b"
-           for category in us_cities["properties"]["hasQuestion"])
+xrefs = [category["href"] for category in sf["properties"]["hasCategory"]]
+assert f"/v1/objects/JeopardyCategory/{us_cities_id}" in xrefs
+xrefs = [category["href"] for category in us_cities["properties"]["hasQuestion"]]
+assert f"/v1/objects/JeopardyQuestion/{sf_id}" in xrefs
 # END Test results
 
 
@@ -164,10 +164,9 @@ client.data_object.reference.add(
 # Test results
 sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion")
 assert sf["class"] == "JeopardyQuestion"
-assert any(category["href"] == f"/v1/objects/JeopardyCategory/{us_cities_id}"
-           for category in sf["properties"]["hasCategory"])
-assert any(category["href"] == f"/v1/objects/JeopardyCategory/{museums_id}"
-           for category in sf["properties"]["hasCategory"])
+xrefs = [category["href"] for category in sf["properties"]["hasCategory"]]
+assert f"/v1/objects/JeopardyCategory/{us_cities_id}" in xrefs
+assert f"/v1/objects/JeopardyCategory/{museums_id}" in xrefs
 # END Test results
 
 
@@ -181,53 +180,51 @@ assert any(category["href"] == f"/v1/objects/JeopardyCategory/{museums_id}"
 
 # TODO: 💥 blows up with 'repo.putobject code:500 err:import into index jeopardyquestion: shard jeopardyquestion_wuyf2SngXb8a: Validate vector index for [0 255 105 0 230 79 93 148 144 219 200 207 163 252 133 27]: new node has a vector with length 0. Existing nodes have vectors with length 1536'
 #  before, the objects look OK:
-#  sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion"); print(json.dumps(sf, indent=2))  # has the reference to fec50326-dfa1-53c9-90e8-63d0240bd933
-#  cat = client.data_object.get(uuid="fec50326-dfa1-53c9-90e8-63d0240bd933", class_name="JeopardyCategory"); print(json.dumps(cat, indent=2))  # exists
+sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion"); print(json.dumps(sf, indent=2))  # has the reference to fec50326-dfa1-53c9-90e8-63d0240bd933
+cat = client.data_object.get(uuid="fec50326-dfa1-53c9-90e8-63d0240bd933", class_name="JeopardyCategory"); print(json.dumps(cat, indent=2))  # exists
 
-# client.data_object.reference.delete(
-#     from_class_name="JeopardyQuestion",
-#     from_uuid="00ff6900-e64f-5d94-90db-c8cfa3fc851b",
-#     from_property_name="hasCategory",
-#     to_class_name="JeopardyCategory",
-#     to_uuid="fec50326-dfa1-53c9-90e8-63d0240bd933",
-# )
+client.data_object.reference.delete(
+    from_class_name="JeopardyQuestion",
+    from_uuid="00ff6900-e64f-5d94-90db-c8cfa3fc851b",
+    from_property_name="hasCategory",
+    to_class_name="JeopardyCategory",
+    to_uuid="fec50326-dfa1-53c9-90e8-63d0240bd933",
+)
 # END Delete Python
-#
-# # Test results
-# sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion")
-# print(json.dumps(sf, indent=2))
-# assert sf["class"] == "JeopardyQuestion"
-# assert sf["properties"]["hasCategory"] == [{
-#     "beacon": f"weaviate://localhost/JeopardyCategory/{us_cities_id}",
-#     "href": f"/v1/objects/JeopardyCategory/{us_cities_id}"
-# }]
-# # END Test results
+
+# Test results
+sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion")
+assert sf["class"] == "JeopardyQuestion"
+assert sf["properties"]["hasCategory"] == [{
+    "beacon": f"weaviate://localhost/JeopardyCategory/{us_cities_id}",
+    "href": f"/v1/objects/JeopardyCategory/{us_cities_id}"
+}]
+# END Test results
 
 
-# ============================
-# ===== Update cross-ref =====
-# ============================
+# # ============================
+# # ===== Update cross-ref =====
+# # ============================
 
 # Update Python
 # In the "San Francisco" JeopardyQuestion object, set the "hasCategory" cross-reference only to "MUSEUMS"
 # https://weaviate-python-client.readthedocs.io/en/stable/weaviate.data.references.html#weaviate.data.references.Reference.update
 
 # TODO: also 💥blows up with 'repo.putobject code:500 err:import into index jeopardyquestion: shard jeopardyquestion_X7SHjqOnQRlL: Validate vector index for [0 255 105 0 230 79 93 148 144 219 200 207 163 252 133 27]: new node has a vector with length 0. Existing nodes have vectors with length 1536'
-# client.data_object.reference.update(
-#     from_class_name="JeopardyQuestion",
-#     from_uuid="00ff6900-e64f-5d94-90db-c8cfa3fc851b",
-#     from_property_name="hasCategory",
-#     to_class_names=["JeopardyCategory"],
-#     to_uuids=["fec50326-dfa1-53c9-90e8-63d0240bd933"],
-# )
-# # END Update Python
-#
-# # Test results
-# sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion")
-# print(json.dumps(sf, indent=2))
-# assert sf["class"] == "JeopardyQuestion"
-# assert sf["properties"]["hasCategory"] == [{
-#     "beacon": f"weaviate://localhost/JeopardyCategory/{museums_id}",
-#     "href": f"/v1/objects/JeopardyCategory/{museums_id}"
-# }]
-# # END Test results
+client.data_object.reference.update(
+    from_class_name="JeopardyQuestion",
+    from_uuid="00ff6900-e64f-5d94-90db-c8cfa3fc851b",
+    from_property_name="hasCategory",
+    to_class_names=["JeopardyCategory"],
+    to_uuids=["fec50326-dfa1-53c9-90e8-63d0240bd933"],
+)
+# END Update Python
+
+# Test results
+sf = client.data_object.get(uuid=sf_id, class_name="JeopardyQuestion")
+assert sf["class"] == "JeopardyQuestion"
+assert sf["properties"]["hasCategory"] == [{
+    "beacon": f"weaviate://localhost/JeopardyCategory/{museums_id}",
+    "href": f"/v1/objects/JeopardyCategory/{museums_id}"
+}]
+# END Test results
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -6,53 +6,70 @@ import subprocess
 @pytest.mark.parametrize(
     "script_loc",
     [
-        "./_includes/code/howto.exhausive.retrieval.py"
+        "./_includes/code/howto/manage-data.cross-refs.py"
     ],
 )
-def test_py(empty_weaviates, script_loc):
+def test_python_script(empty_weaviates, script_loc):
+    # proc_script = utils.load_and_prep_script(script_loc)
+    # exec(proc_script)
     temp_proc_script_loc = utils.load_and_prep_temp_file(
         script_loc,
         lang="py",
-        custom_replace_pairs=utils.edu_readonly_replacements
     )
     exec(temp_proc_script_loc.read_text())
 
 
-@pytest.mark.parametrize(
-    "script_loc",
-    [
 
-    ],
-)
-def test_js(empty_weaviates, script_loc):
-    temp_proc_script_loc = utils.load_and_prep_temp_file(
-        script_loc,
-        lang="js",
-        custom_replace_pairs=utils.edu_readonly_replacements
-    )
-    try:
-        # If the script throws an error, this will raise a CalledProcessError
-        subprocess.check_call(['node', temp_proc_script_loc])
-    except subprocess.CalledProcessError as error:
-        pytest.fail(f'Script {temp_proc_script_loc} failed with error: {error}')
+# @pytest.mark.parametrize(
+#     "script_loc",
+#     [
+#         "./_includes/code/howto.exhausive.retrieval.py"
+#     ],
+# )
+# def test_py(empty_weaviates, script_loc):
+#     temp_proc_script_loc = utils.load_and_prep_temp_file(
+#         script_loc,
+#         lang="py",
+#         custom_replace_pairs=utils.edu_readonly_replacements
+#     )
+#     exec(temp_proc_script_loc.read_text())
 
 
-@pytest.mark.parametrize(
-    "script_loc",
-    [
+# @pytest.mark.parametrize(
+#     "script_loc",
+#     [
 
-    ],
-)
-def test_ts(empty_weaviates, script_loc):
-    temp_proc_script_loc = utils.load_and_prep_temp_file(
-        script_loc,
-        lang="ts",
-        custom_replace_pairs=utils.edu_readonly_replacements
-    )
-    command = ["node", " --loader=ts-node/esm", temp_proc_script_loc]
+#     ],
+# )
+# def test_js(empty_weaviates, script_loc):
+#     temp_proc_script_loc = utils.load_and_prep_temp_file(
+#         script_loc,
+#         lang="js",
+#         custom_replace_pairs=utils.edu_readonly_replacements
+#     )
+#     try:
+#         # If the script throws an error, this will raise a CalledProcessError
+#         subprocess.check_call(['node', temp_proc_script_loc])
+#     except subprocess.CalledProcessError as error:
+#         pytest.fail(f'Script {temp_proc_script_loc} failed with error: {error}')
 
-    try:
-        # If the script throws an error, this will raise a CalledProcessError
-        subprocess.check_call(command)
-    except subprocess.CalledProcessError as error:
-        pytest.fail(f'Script {temp_proc_script_loc} failed with error: {error}')
+
+# @pytest.mark.parametrize(
+#     "script_loc",
+#     [
+
+#     ],
+# )
+# def test_ts(empty_weaviates, script_loc):
+#     temp_proc_script_loc = utils.load_and_prep_temp_file(
+#         script_loc,
+#         lang="ts",
+#         custom_replace_pairs=utils.edu_readonly_replacements
+#     )
+#     command = ["node", " --loader=ts-node/esm", temp_proc_script_loc]
+
+#     try:
+#         # If the script throws an error, this will raise a CalledProcessError
+#         subprocess.check_call(command)
+#     except subprocess.CalledProcessError as error:
+#         pytest.fail(f'Script {temp_proc_script_loc} failed with error: {error}')
