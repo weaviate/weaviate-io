c297f63eaf916f3a3bbb21b442d13f6b9d794d0d
Wera <weronika@weaviate.io>
2023-06-02 12:06:48 +0200

    add apple podcasts btn

--- src/components/Podcast/Header/index.jsx
+++ src/components/Podcast/Header/index.jsx
@@ -13,18 +13,24 @@ export default function PodcastHeader() {
         customers.
       </p>
       <ButtonContainer position="left">
-        <LinkButton color="accent"
+        <LinkButton
+          color="accent"
           link="https://www.youtube.com/playlist?list=PLTL2JUbrY6tW-KOQfOek8dtUmPgGQj3F0"
         >
           Subscribe to the YouTube Channel <i className="fab fa-youtube"></i>
         </LinkButton>
-        <LinkButton
-          link="https://open.spotify.com/show/4TlG6dnrWYdgN2YHpoSnM7"
-        >
+        <LinkButton link="https://open.spotify.com/show/4TlG6dnrWYdgN2YHpoSnM7">
           Open in Spotify <i className="fab fa-spotify"></i>
         </LinkButton>
         <LinkButton
-          color="outlined" link="https://anchor.fm/s/cffc3468/podcast/rss"
+          color="outlined"
+          link="https://podcasts.apple.com/gb/podcast/weaviate-podcast/id1680897946"
+        >
+          Apple podcasts <i class="fa-solid fa-podcast"></i>
+        </LinkButton>
+        <LinkButton
+          color="outlined"
+          link="https://anchor.fm/s/cffc3468/podcast/rss"
         >
           Weaviate RSS feed <i className="fa-solid fa-square-rss"></i>
         </LinkButton>
