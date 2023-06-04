5a1da441ba99994044fbb40cc67cba13fa0eaeb2
JP Hwang <me@jphwang.com>
2023-06-04 15:24:48 +0400

    add draft module 4

--- developers/academy/units/104_queries_2/_snippets/40_qna.py
+++ developers/academy/units/104_queries_2/_snippets/40_qna.py
@@ -10,12 +10,11 @@ client = weaviate.Client(
     },
 )
 
-# ===== Basic Single Prompt Generative Query =====
+# ===== SimpleQnA Query =====
 
 # SimpleAskQuery
 ask = {
   "question": "How many championships does Lewis Hamilton have?",
-  "properties": ["wiki_summary"]
 }
 
 response = (
@@ -59,4 +58,55 @@ assert response["data"]["Get"]["WikiArticle"][0]["_additional"].keys() == {"answ
   }
 }
 # END Expected SimpleAskQuery results
+"""
+
+# ===== QnA Query with Properties =====
+
+# AskQueryWithProperties
+ask = {
+  "question": "How many championships does Lewis Hamilton have?",
+  "properties": ["title"]
+}
+
+response = (
+  client.query
+  .get("WikiArticle", ["title", "_additional {answer {hasAnswer property result startPosition endPosition} }"])
+  .with_ask(ask)
+  .with_limit(1)
+  .do()
+)
+
+print(json.dumps(response, indent=2))
+# END AskQueryWithProperties
+
+# Tests
+assert "WikiArticle" in response["data"]["Get"]
+assert len(response["data"]["Get"]["WikiArticle"]) == 1
+assert response["data"]["Get"]["WikiArticle"][0].keys() == {"title", "_additional"}
+assert response["data"]["Get"]["WikiArticle"][0]["_additional"].keys() == {"answer"}
+# End test
+
+"""
+# Expected AskQueryWithProperties results
+{
+  "data": {
+    "Get": {
+      "WikiArticle": [
+        {
+          "_additional": {
+            "answer": {
+              "endPosition": 0,
+              "hasAnswer": true,
+              "property": "",
+              "result": " Lewis Hamilton has seven World Drivers' Championship titles.",
+              "startPosition": 0
+            }
+          },
+          "title": "Lewis Hamilton"
+        }
+      ]
+    }
+  }
+}
+# END Expected AskQueryWithProperties results
 """
