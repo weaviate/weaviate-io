6cc58fa933ddb21de5798cc0208505795fd1c20c
Dan Dascalescu <dan@weaviate.io>
2023-06-06 17:59:26 +0200

    Add Jest example

--- _includes/code/embedded.ts
+++ _includes/code/embedded.ts
@@ -1,10 +1,17 @@
 // Blog: 2023-06-06, embedded-local-weaviate
 // run with: node --loader=ts-node/esm embedded.ts
-// START example
-import assert from 'assert';
+// START 10lines  // START TestExample
 import weaviate, { EmbeddedOptions } from 'weaviate-ts-embedded';
 
-const client = weaviate.client(new EmbeddedOptions());
+const client = weaviate.client(new EmbeddedOptions(
+  // END 10lines  // START TestExample
+  {
+    env: {
+      'OPENAI_APIKEY': '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',
+    },
+  }
+  // END TestExample  // START 10lines
+));
 
 await client.embedded.start();
 
@@ -18,7 +25,43 @@ const createdObject = await client.data
 
 console.log(JSON.stringify(createdObject, null, 2));
 
-assert.equal(createdObject.properties['hello'], 'World!');  // test
+// END 10lines
 
+// Test
+import assert from 'assert';
+assert.equal(createdObject?.properties?.['hello'], 'World!');
+
+// START TestExample
+
+await client.schema.classCreator().withClass({
+  class: 'Wine',
+  vectorizer: 'text2vec-openai',
+}).do();
+
+await client.data.creator()
+  .withClassName('Wine')
+  .withProperties({
+    name: 'Chardonnay',
+    review: 'Goes well with fish!',
+  })
+  .do();
+
+const response = await client.graphql
+  .get()
+  .withClassName('Wine')
+  .withNearText({
+    concepts: ['great for seafood'],
+  })
+  .withFields('name review')
+  .do();
+
+test('Test nearText', function () {
+  assert.equal(
+    response.data.Get['Wine'][0]['review'],
+    'Goes well with fish!'
+  );
+});
+
+// END TestExample  // START 10lines  // START TestExample
 await client.embedded.stop();
-// END example
+// END 10lines  // END TestExample
