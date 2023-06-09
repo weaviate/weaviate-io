97a6d41a7f10d5ae8c1e80e31a309936ebbdda14
JP Hwang <61258750+databyjp@users.noreply.github.com>
2023-06-09 22:30:30 +0100

    [Academy] Add academy module4 (#887)

--- tests/test_academy.py
+++ tests/test_academy.py
@@ -6,20 +6,6 @@ import utils
     "script_loc",
     [
         "./developers/academy/units/_snippets/setup.py",
-    ],
-)
-def test_against_edu_demo(empty_weaviates, script_loc):
-    temp_proc_script_loc = utils.load_and_prep_temp_file(
-        script_loc,
-        lang="py",
-        custom_replace_pairs=utils.edu_readonly_replacements
-    )
-    exec(temp_proc_script_loc.read_text())
-
-
-@pytest.mark.parametrize(
-    "script_loc",
-    [
         "./developers/academy/units/103_schema_and_imports/_snippets/05_create_instance.py",
         "./developers/academy/units/103_schema_and_imports/_snippets/20_schema.py",
         "./developers/academy/units/103_schema_and_imports/_snippets/30_import.py",
@@ -29,3 +15,21 @@ def test_against_edu_demo(empty_weaviates, script_loc):
 def test_against_empty_weaviate(empty_weaviates, script_loc):
     proc_script = utils.load_and_prep_script(script_loc)
     exec(proc_script)
+
+
+@pytest.mark.parametrize(
+    "script_loc",
+    [
+        "./developers/academy/units/104_queries_2/_snippets/10_bm25.py",
+        "./developers/academy/units/104_queries_2/_snippets/20_hybrid.py",
+        "./developers/academy/units/104_queries_2/_snippets/30_generative.py",
+        "./developers/academy/units/104_queries_2/_snippets/40_qna.py",
+    ],
+)
+def test_against_edu_demo(empty_weaviates, script_loc):
+    temp_proc_script_loc = utils.load_and_prep_temp_file(
+        script_loc,
+        lang="py",
+        custom_replace_pairs=utils.edu_readonly_replacements
+    )
+    exec(temp_proc_script_loc.read_text())
