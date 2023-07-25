a86fa4ae05570b3547706f482c51cde53511fd90
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-07-25 14:59:53 +0200

    [docs] UX update for roadmap

--- src/theme/Roadmap/index.jsx
+++ src/theme/Roadmap/index.jsx
@@ -6,14 +6,15 @@ export function Roadmap({label}) {
   const [roadmapItems, setRoadmap] = useState([]);
 
   useEffect(() => {
-    fetch(`https://api.github.com/repos/weaviate/weaviate/issues?per_page=1000&labels=${label}`)
+    fetch(`https://api.github.com/repos/weaviate/weaviate/issues?per_page=1000&labels=${label}&state=all`)
     .then(response => response.json())
     .then(data => {
       const parsed_data = data.map(item => { return {
         id: item.number,
         url: item.html_url,
         title: item.title,
-        score: item.reactions['+1']
+        score: item.reactions['+1'],
+        state: item.state
       }})
       .sort((a, b) => b.score - a.score);
 
@@ -25,9 +26,12 @@ export function Roadmap({label}) {
     <ul>
       {roadmapItems.map((item) => {
           if (!item) return null;
+
+          const icon = (item.state === 'open') ? 'far fa-star' : 'fas fa-check'
+
           return (
             <li className={styles.roadmapItem} key={item.id}>
-              <i className="fas fa-star"></i>
+              <i className={icon}></i>
               <span className={styles.score}>[{item.score}]</span>
               <a href={item.url} target="_blank">{item.title}</a>
             </li>
