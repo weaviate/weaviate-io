9720a4870fa787171c87c8dece36b6d4851e30b0
JP Hwang <61258750+databyjp@users.noreply.github.com>
2023-07-31 10:54:31 +0100

    [site] update slack link (#1009)
    * update slack link
    
    * add openai to link ignorelist

--- _build_scripts/verify-links-build-dev.sh
+++ _build_scripts/verify-links-build-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-URL_IGNORES="jsonlines.org/|arxiv.org/|linkedin.com/in/|crunchbase.com|www.nytimes.com|www.researchgate.net|www.meetup.com|wiki.pathmind.com|twitter.com|towardsdatascience.com|medium.com|chat.openai.com|www.hr-brew.com"
+URL_IGNORES="jsonlines.org/|arxiv.org/|linkedin.com/in/|crunchbase.com|www.nytimes.com|www.researchgate.net|www.meetup.com|wiki.pathmind.com|twitter.com|towardsdatascience.com|medium.com|openai.com|www.hr-brew.com"
 GITHUB_IGNORES="github.com|github.com/weaviate/howto*"
 DEV_BUILD_LINKS_TO_IGNORE="assets/files|https://weaviate.io"
 
