49d2232d9295356c4861522c57038a96a4f217d9
Wera <weronika@weaviate.io>
2023-08-08 09:23:58 +0200

    update verify-links

--- _build_scripts/verify-links.sh
+++ _build_scripts/verify-links.sh
@@ -2,7 +2,7 @@
 set -e
 set -o errexit # stop script immediately on error
 
-URL_IGNORES="jsonlines.org/|arxiv.org/|huggingface.co/|linkedin.com/in/|crunchbase.com|www.nytimes.com|www.researchgate.net|cohere.com|www.meetup.com|wiki.pathmind.com|twitter.com|chat.openai.com|platform.openai.com*|www.ing.com|towardsdatascience.com|medium.com|github.com/weaviate-tutorials/cohere-multilingual-test|hackernoon.com/"
+URL_IGNORES="jsonlines.org/|arxiv.org/|linkedin.com/in/|crunchbase.com|www.nytimes.com|www.researchgate.net|www.meetup.com|wiki.pathmind.com|twitter.com|towardsdatascience.com|medium.com|openai.com|www.hr-brew.com|www.battery.com"
 DOCUSAURUS_IGNORES="github.com/.*github.com/|github.com/weaviate/weaviate-io|github.com/weaviate/howto-weaviate-retrieval-plugin*"
 # Note #1 github.com/.*github.com/ - is to ignore meta links that include blog co-authors
 # Note #2 github.com/weaviate/weaviate-io/tree/ - is for edit on github links
