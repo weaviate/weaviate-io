4da39cf7d71e6d5198a7d95e14488d97672ce566
Sebastian Witalec <sebawita@users.noreply.github.com>
2023-08-25 18:31:17 +0200

    Update workshops.jsx

--- src/pages/learn/workshops.jsx
+++ src/pages/learn/workshops.jsx
@@ -5,6 +5,13 @@ import styles from '/src/components/Workshop/styles.module.scss';
 export default function Workshops() {
   return (
     <Layout title="Workshops" description="Weaviate Workshops Page">
+      <Head>
+        <script
+          src="https://static.elfsight.com/platform/platform.js"
+          data-use-service-core
+          async
+        ></script>
+      </Head>
       <div className="custom-page">
         <div className="container">
           <div className={styles.box}>
@@ -16,11 +23,6 @@ export default function Workshops() {
               information and details on our service.
             </p>
           </div>
-          <script
-            src="https://static.elfsight.com/platform/platform.js"
-            data-use-service-core
-            async
-          ></script>
           <div className="elfsight-app-01a3e7d9-f320-4491-a464-8339fafe3e80"></div>
         </div>
       </div>
