1478db601942441932843295fe8d16c79da464a8
AJ Chan <achan99@gmail.com>
2023-09-07 08:47:32 -0700

    Update llamaindex blog post with dependency imports and update Simple… (#1089)

--- _build_scripts/verify-links-build-dev.sh
+++ _build_scripts/verify-links-build-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-URL_IGNORES="jsonlines.org/|arxiv.org/|linkedin.com/in/|crunchbase.com|www.nytimes.com|www.researchgate.net|www.meetup.com|wiki.pathmind.com|twitter.com|towardsdatascience.com|medium.com|openai.com|www.hr-brew.com|www.battery.com|docs.cohere.com|www.googletagmanager.com|12factor.net|github.com|github.com/weaviate/howto*|instagram.com|kaggle.com"
+URL_IGNORES="jsonlines.org/|arxiv.org/|linkedin.com/in/|crunchbase.com|www.nytimes.com|www.researchgate.net|www.meetup.com|wiki.pathmind.com|twitter.com|towardsdatascience.com|medium.com|openai.com|www.hr-brew.com|www.battery.com|docs.cohere.com/|www.googletagmanager.com|12factor.net|github.com|github.com/weaviate/howto*|instagram.com|kaggle.com"
 GITHUB_IGNORES="github.com|github.com/weaviate/howto*"
 DEV_BUILD_LINKS_TO_IGNORE="assets/files|downloads/docker-templates|https://weaviate.io"
 
