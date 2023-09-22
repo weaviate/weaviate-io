c798f8bd5a264d669ea42ff1c2de81796280b735
Dave Cuthbert <david@weaviate.io>
2023-09-22 12:56:41 -0400

    148 investigate branch renaming

--- _build_scripts/publish-draft-to-netlify.sh
+++ _build_scripts/publish-draft-to-netlify.sh
@@ -6,8 +6,11 @@ mv build/robots.txt build/robots.txt.live
 echo -e "User-agent: *
 Disallow: /" >> build/robots.txt
 
+BR_NAME=$(cat .git/HEAD | cut -c 17-)
+
+./node_modules/.bin/netlify deploy --dir=build --alias ${BR_NAME} --site=tangerine-buttercream-20c32f > netlify.out
+
 # sudo netlify deploy --dir=build --site=tangerine-buttercream-20c32f >> netlify.out
-./node_modules/.bin/netlify deploy --dir=build --site=tangerine-buttercream-20c32f > netlify.out
 
 # Bring back the original robots.txt file
 rm build/robots.txt
