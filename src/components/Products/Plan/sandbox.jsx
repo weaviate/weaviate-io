c3f2cca0202065d7d0d402fba8eb2a8eed90f34c
Shan-Weaviate <shan@weaviate.io>
2023-10-03 13:47:21 +0100

    Sandbox Fix

--- src/components/Products/Plan/sandbox.jsx
+++ src/components/Products/Plan/sandbox.jsx
@@ -11,21 +11,21 @@ export default function PricingSandBox() {
         <li>
           <div className={styles.checkIcon}></div> 14 days lifetime
         </li>
-      </div>
-      <div className={styles.features}>
         <li>
           <div className={styles.checkIcon}></div> Monitoring
         </li>
+      </div>
+      <div className={styles.features}>
         <li>
           <div className={styles.checkIcon}></div>
           <a href="https://weaviate.io/slack"> Public Slack</a>
         </li>
-      </div>
-      <div className={styles.features}>
         <li>
           <div className={styles.checkIcon}></div>
           <span>Community support</span>
         </li>
+      </div>
+      <div className={styles.features}>
         <li>
           <div className={styles.checkIcon}></div> Single AZ
         </li>
