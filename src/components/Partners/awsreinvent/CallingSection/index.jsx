6e97eb4b0902f2000cf58a7f4fb24c9ee316d58a
Shan-Weaviate <shan@weaviate.io>
2023-10-19 11:50:47 +0100

    aws-reinvent-updates

--- src/components/Partners/awsreinvent/CallingSection/index.jsx
+++ src/components/Partners/awsreinvent/CallingSection/index.jsx
@@ -10,13 +10,12 @@ export default function CallingSection() {
       <div className="container">
         <div className={styles.header}>
           <h2>
-            Calling all developers, data scientists, and AI builders of all
-            levels
+            Calling developers, data scientists, and AI builders of all levels
           </h2>
           <p>
             Meet with our leadership team to hear what’s ahead for AI-native
-            applications, explore hands-on<br></br> demos and learn more about
-            the power of Weaviate on AWS. 
+            applications, explore hands-on demos<br></br> and learn more about
+            the power of Weaviate on AWS.
           </p>
         </div>
         <div className={styles.features}>
@@ -27,19 +26,18 @@ export default function CallingSection() {
               Vector databases are an emerging piece of the modern data stack,
               purpose-built for AI, that empower use cases like semantic search,
               retrieval augmented generation (RAG), and generative feedback
-              loops. Hear our CEO Bob van Luijt share the benefits of using an
+              loops.Hear our CEO Bob van Luijt share the benefits of using an
               open source vector database as the foundation of your AI-native
               stack.
             </p>
           </div>
           <div className={styles.box}>
             <div className={styles.icon2}></div>
-            <h4 className={styles.title}>Meet with us, get a gift</h4>
+            <h4 className={styles.title}>Meet with us</h4>
             <p className={styles.subTitle}>
-              Book a meeting at BRERA Osteria or right in our booth to learn
-              about Weaviate and explore our product roadmap. Get exclusive
-              gifts like {gift} when you attend a 1:1 meeting with one of our
-              team members. Request a time to meet and we’ll get back to you to
+              Book time to meet 1:1 with our team at BRERA osteria or right in
+              our booth to learn about Weaviate and explore our latest
+              offerings. Request a time to meet and we’ll get back to you to
               confirm your attendance.
             </p>
           </div>
--- src/components/Partners/awsreinvent/ContactForm/contactForm.jsx
+++ src/components/Partners/awsreinvent/ContactForm/contactForm.jsx
@@ -3,67 +3,21 @@ import styles from './styles.module.scss';
 import Link from '@docusaurus/Link';
 
 export default function ContactForm() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '//js-eu1.hsforms.net/forms/embed/v2.js';
-    document.body.appendChild(script);
-
-    script.addEventListener('load', () => {
-      // @TS-ignore
-      if (window.hbspt) {
-        // @TS-ignore
-        window.hbspt.forms.create({
-          portalId: '143457533',
-          formId: '9f950fe9-da45-40dd-87fc-cfebf4b1a5e2',
-          target: '#hubspotForm',
-        });
-      }
-    });
-  }, []);
-
   return (
     <div className={styles.contactBackground} id="meetingForm">
       <div className="container">
         <div className={styles.contactContainer}>
           <div className={styles.contactSection}>
-            <h2 className={styles.title}>Book a meeting with the team</h2>
-            <div id="hubspotForm"></div>
-            {/*  <form
-              className={styles.formContact}
-              action="https://formcarry.com/s/FZwYB5WwuG"
-              method="POST"
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSen-orc85VQyajMjTLrULDJUuk2b8Tfl2gsZ-yzExgFxSzJCA/viewform?embedded=true"
+              width="600"
+              height="1738"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
             >
-              <h2 className={styles.title}>Contact us </h2>
-              <div className={styles.links}>
-                <p>
-                  Do you have any questions? Let’s get this conversation
-                  started.
-                </p>
-              </div>
-
-              <input
-                type="input"
-                name="formName"
-                placeholder="First Name"
-              ></input>
-              <span></span>
-              <input
-                type="email"
-                name="formEmail"
-                placeholder="Work Email Address"
-              ></input>
-              <span></span>
-
-              <textarea
-                type="text"
-                name="formMessage"
-                className={styles.formMessage}
-                placeholder="Message"
-              ></textarea>
-              <button type="submit" name="formSumbit">
-                Send
-              </button>
-            </form> */}
+              Loading…
+            </iframe>
           </div>
         </div>
       </div>
