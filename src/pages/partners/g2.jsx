cf132389388d0aa4504a129a75fae4e363ba3aa6
Shan-Weaviate <shan@weaviate.io>
2023-11-14 17:24:46 +0000

    Update g2.jsx

--- src/pages/partners/g2.jsx
+++ src/pages/partners/g2.jsx
@@ -4,7 +4,6 @@ import Link from '@docusaurus/Link';
 import Header from '/src/components/Partners/G2/Header';
 import Introduction from '../../components/Partners/G2/Introduction/introduction';
 import RewardProcess from '/src/components/Partners/G2/RewardProcess';
-import CallingSection from '/src/components/Partners/G2/CallingSection';
 import ContactForm from '../../components/Partners/G2/ContactForm/contactForm';
 import ThemeSwitch from '/src/components/ThemeSwitch';
 
