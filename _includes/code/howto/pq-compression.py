cc1c20a8b02c41603a314a6c74ffb01a2bd16fa3
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2023-11-16 12:37:28 -0500

    lint py4 file

--- _includes/code/howto/pq-compression.py
+++ _includes/code/howto/pq-compression.py
@@ -13,8 +13,10 @@ client = weaviate.connect_to_local(
     port=8080,
     grpc_port=50051,
     headers={
-        "X-OpenAI-Api-Key": os.environ[ "OPENAI_API_KEY" ] # Replace with your inference API key
-    }
+        "X-OpenAI-Api-Key": os.environ[
+            "OPENAI_API_KEY"
+        ]  # Replace with your inference API key
+    },
 )
 
 # ==============================
@@ -27,14 +29,16 @@ import requests
 import json
 
 # Download the data
-resp = requests.get( 'https://raw.githubusercontent.com/weaviate-tutorials/intro-workshop/main/data/jeopardy_1k.json' )
+resp = requests.get(
+    "https://raw.githubusercontent.com/weaviate-tutorials/intro-workshop/main/data/jeopardy_1k.json"
+)
 
 # Load the data so you can see what it is
 data = json.loads(resp.text)
 
 # Parse the JSON and preview it
-print( type( data ), len( data ) )
-print( json.dumps( data[1], indent=2 ) )
+print(type(data), len(data))
+print(json.dumps(data[1], indent=2))
 
 # END DownloadData
 
@@ -48,7 +52,9 @@ import weaviate.classes as wvc
 
 client = weaviate.connect_to_local(
     headers={
-        "X-OpenAI-Api-Key": os.environ['OPENAI_API_KEY'] # Replace with your OpenAI API key
+        "X-OpenAI-Api-Key": os.environ[
+            "OPENAI_API_KEY"
+        ]  # Replace with your OpenAI API key
     }
 )
 
@@ -60,7 +66,7 @@ client.is_ready()
 # =====  INITIAL SCHEMA =====
 # ==============================
 
-# START InitialSchema 
+# START InitialSchema
 
 client.collections.create(
     name="JeopardyCategory",
@@ -68,41 +74,43 @@ client.collections.create(
     vectorizer_config=wvc.Configure.Vectorizer.text2vec_openai(),
     generative_config=wvc.Configure.Generative.openai(),
     properties=[
-        wvc.Property( name="title", data_type=wvc.DataType.TEXT ),
-    ]
+        wvc.Property(name="title", data_type=wvc.DataType.TEXT),
+    ],
 )
 
-# END InitialSchema 
+# END InitialSchema
 
 # ==============================
 # =====  LOAD DATA =====
 # ==============================
 
+
 # START LoadData
 def parse_data():
-  object_list = []
-  for obj in data:
-     object_list.append( 
-        {
-            "question": obj[ "Question" ], 
-            "answer": obj[ "Answer" ], 
-            "round": obj[ "Round" ] 
-         }
-     )
-     
-  return( object_list )
-  
-jeopardy = client.collections.get( "JeopardyCategory" )
-jeopardy.data.insert_many( parse_data() )
+    object_list = []
+    for obj in data:
+        object_list.append(
+            {
+                "question": obj["Question"],
+                "answer": obj["Answer"],
+                "round": obj["Round"],
+            }
+        )
+
+    return object_list
+
+
+jeopardy = client.collections.get("JeopardyCategory")
+jeopardy.data.insert_many(parse_data())
 
 # Check upload
-response = jeopardy.aggregate.over_all( total_count=True )
+response = jeopardy.aggregate.over_all(total_count=True)
 
 # Should equal the number of objects uploaded
-print( response.total_count )
+print(response.total_count)
 
 
-# END LoadData 
+# END LoadData
 
 # ==============================
 # =====  UPDATE SCHEMA =====
@@ -112,13 +120,11 @@ print( response.total_count )
 
 import weaviate.classes as wvc
 
-jeopardy = client.collections.get( "JeopardyCategory" )
+jeopardy = client.collections.get("JeopardyCategory")
 jeopardy.config.update(
-   vector_index_config=wvc.Reconfigure.vector_index(
-      pq_enabled=True,
-      pq_segments=96,
-      pq_training_limit=100000
-   )
+    vector_index_config=wvc.Reconfigure.vector_index(
+        pq_enabled=True, pq_segments=96, pq_training_limit=100000
+    )
 )
 
 
@@ -130,15 +136,14 @@ jeopardy.config.update(
 
 # START GetSchema
 
-jeopardy = client.collections.get( "JeopardyCategory" )
+jeopardy = client.collections.get("JeopardyCategory")
 config = jeopardy.config.get()
 pq_config = config.vector_index_config.pq
 
 # print some of the config properties
-print( f"Enabled: { pq_config.enabled }" )
-print( f"Training: { pq_config.training_limit }" )
-print( f"Segments: { pq_config.segments }" )
-print( f"Centroids: { pq_config.centroids }" )
+print(f"Enabled: { pq_config.enabled }")
+print(f"Training: { pq_config.training_limit }")
+print(f"Segments: { pq_config.segments }")
+print(f"Centroids: { pq_config.centroids }")
 
 # END GetSchema
-
