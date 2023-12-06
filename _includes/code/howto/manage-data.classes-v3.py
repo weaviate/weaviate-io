dc147a28fa3be6394833394b17c9608976e89fe2
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2023-12-05 21:21:06 -0500

    staging review

--- _includes/code/howto/manage-data.classes-v3.py
+++ _includes/code/howto/manage-data.classes-v3.py
@@ -48,7 +48,19 @@ if client.schema.exists(class_name):
     client.schema.delete_class(class_name)
 
 # START CreateCollectionWithProperties
-class_obj = {"class": class_name}
+class_obj = {
+    'class': 'Article',
+    'properties': [
+        {
+            'name': 'title',
+            'dataType': ['text'],
+        },
+        {
+            'name': 'body',
+            'dataType': ['text'],
+        },
+    ],
+}
 
 client.schema.create_class(class_obj)  # returns null on success
 # END CreateCollectionWithProperties
