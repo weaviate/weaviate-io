cab123b186185f9971f01db95e84be65d83696a9
JP Hwang <me@jphwang.com>
2023-12-13 10:37:36 +0000

    wip

---
+++ _includes/code/graphql.filters.nearText.py
@@ -0,0 +1,25 @@
+import weaviate
+import weaviate.classes as wvc
+from weaviate.collections.classes.grpc import Move
+import os
+
+client = weaviate.connect_to_local(
+    headers={
+        "X-OpenAI-Api-Key": os.getenv("OPENAI_APIKEY"),
+    }
+)
+
+publications = client.collections.get("Publication")
+
+response = publications.query.near_text(
+    query="fashion",
+    distance=0.56,
+    move_to=Move(force=0.85, concepts="haute couture"),
+    move_away=Move(force=0.45, concepts="finance"),
+    return_metadata=wvc.MetadataQuery(distance=True),
+    limit=2
+)
+
+for o in response.objects:
+    print(o.properties)
+    print(o.metadata)
