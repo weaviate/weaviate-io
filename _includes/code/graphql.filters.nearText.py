5940e33e583bb0e1f16e086d5db01ec22702f914
JP Hwang <me@jphwang.com>
2023-12-13 11:04:55 +0000

    Add PyV4 examples for t2v module pages

---
+++ _includes/code/graphql.filters.nearText.py
@@ -0,0 +1,47 @@
+# START-ANY
+import weaviate
+import weaviate.classes as wvc
+from weaviate.collections.classes.grpc import Move
+import os
+
+client = weaviate.connect_to_local(
+    headers={
+        # END-ANY
+        # START NearTextAWS
+        "X-AWS-Access-Key": "YOUR_ACCESS_KEY",
+        "X-AWS-Secret-Key": "YOUR_SECRET_KEY",
+        # END NearTextAWS
+        # START NearTextCohere
+        "X-Cohere-Api-Key": os.getenv("COHERE_APIKEY"),
+        # END NearTextCohere
+        # START NearTextOpenAI
+        "X-OpenAI-Api-Key": os.getenv("OpenAI_APIKEY"),
+        # END NearTextOpenAI
+        # START NearTextGoogle
+        "X-Palm-Api-Key": "YOUR_GOOGLE_APIKEY",
+        # END NearTextGoogle
+        # START NearTextHuggingface
+        "X-HuggingFace-Api-Key": "YOUR_HUGGINGFACE_APIKEY",
+        # END NearTextHuggingface
+        # START NearTextJinaai
+        "X-Jinaai-Api-Key": "YOUR_JINAAI_APIKEY",
+        # END NearTextJinaai
+        # START-ANY
+    }
+)
+
+publications = client.collections.get("Publication")
+
+response = publications.query.near_text(
+    query="fashion",
+    distance=0.6,
+    move_to=Move(force=0.85, concepts="haute couture"),
+    move_away=Move(force=0.45, concepts="finance"),
+    return_metadata=wvc.MetadataQuery(distance=True),
+    limit=2
+)
+
+for o in response.objects:
+    print(o.properties)
+    print(o.metadata)
+# END-ANY
