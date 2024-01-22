bec6f5236cc3aa286ab5d4024f7139aabd44f8e1
JP Hwang <61258750+databyjp@users.noreply.github.com>
2024-01-22 17:08:33 +0000

    20240122/update qs typo (#1570)
    * Update requirements.txt; docker files & quickstart code
    
    * fix missed git commit

--- _includes/code/quickstart/endtoend.py
+++ _includes/code/quickstart/endtoend.py
@@ -20,6 +20,7 @@ client.close()
 import weaviate
 import weaviate.classes as wvc
 # END EndToEndExample  # END InstantiationExample  # END NearTextExample
+
 # ===== import data =====  # EndToEndExample
 import requests
 import json
@@ -179,4 +180,7 @@ assert obj_count.total_count == 10
 
 client.collections.delete("Question")  # Cleanup after
 
+# DockerInstantiationExample  # EndToEndExample  # InstantiationExample  # NearTextExample
+
 client.close()
+# END DockerInstantiationExample  # END EndToEndExample  # END InstantiationExample  # END NearTextExample
