8745b479f7ac861436fc21c4494e01f007a4540d
JP Hwang <me@jphwang.com>
2024-01-25 14:30:32 +0000

    test updates

--- tests/test_api_gql.py
+++ tests/test_api_gql.py
@@ -6,6 +6,13 @@ import utils
     "script_loc",
     [
         "./_includes/code/graphql.get.simple.py",
+        "./_includes/code/graphql.additional.py",
+        "./_includes/code/graphql.filters.py",
+        "./_includes/code/graphql.metadata.py",
+        "./_includes/code/graphql.search-operators.py",
+        "./_includes/code/graphql.filters.nearText.py",
+        "./_includes/code/graphql.filters.nearText.generic.py",
+        "./_includes/code/graphql.aggregate.simple.py",
         "./_includes/code/graphql.get.beacon.v3.py",
     ],
 )
