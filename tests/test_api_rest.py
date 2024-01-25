2ca26aa11cfd5de35581475ff73766b660a5c376
JP Hwang <me@jphwang.com>
2024-01-25 11:43:39 +0000

    Add PyV4 code to rest/nodes

---
+++ tests/test_api_rest.py
@@ -0,0 +1,15 @@
+import pytest
+import utils
+
+
+@pytest.mark.parametrize(
+    "script_loc",
+    [
+        "_includes/code/rest.meta.py",
+        "_includes/code/rest.objects.py",
+        "_includes/code/rest.schema.py",
+    ],
+)
+def test_py(empty_weaviates, script_loc):
+    proc_script = utils.load_and_prep_script(script_loc)
+    exec(proc_script)
