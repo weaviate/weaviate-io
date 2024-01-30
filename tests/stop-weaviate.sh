4f8d4448e069216834ed41f78a87841e610c54fd
JP Hwang <61258750+databyjp@users.noreply.github.com>
2024-01-30 15:06:59 -0800

    Add 2nd anon WV to test suite (#1613)

--- tests/stop-weaviate.sh
+++ tests/stop-weaviate.sh
@@ -3,4 +3,5 @@
 echo "Stop Docker compose"
 docker-compose -f ./tests/docker-compose.yml down  --timeout 30
 docker-compose -f ./tests/docker-compose-anon.yml down  --timeout 30
+docker-compose -f ./tests/docker-compose-anon-2.yml down  --timeout 30
 docker-compose -f ./tests/docker-compose-three-nodes.yml down  --timeout 30
