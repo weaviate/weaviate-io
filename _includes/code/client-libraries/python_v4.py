ef654a90c185581b177a284ddbbafac10901af98
JP Hwang <me@jphwang.com>
2024-02-01 15:34:56 +0000

    Update example

--- _includes/code/client-libraries/python_v4.py
+++ _includes/code/client-libraries/python_v4.py
@@ -190,7 +190,7 @@ with weaviate.connect_to_wcs(
     cluster_url=os.getenv("WCS_DEMO_URL"),  # Replace with your WCS URL
     auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY"))  # Replace with your WCS key
 ) as client:  # Use this context manager to ensure the connection is closed
-    client.collections.get()
+    client.collections.list_all()
 # END WCSQuickStartInstantiation
 
 
