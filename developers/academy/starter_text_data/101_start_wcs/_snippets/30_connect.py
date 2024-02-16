ac69c8df93771fbbf90af73aa74f9731b4516093
JP Hwang <me@jphwang.com>
2024-02-16 12:33:09 +0000

    Update WCS code

--- developers/academy/starter_text_data/101_start_wcs/_snippets/30_connect.py
+++ developers/academy/starter_text_data/101_start_wcs/_snippets/30_connect.py
@@ -1,10 +1,14 @@
-# WCSInstantiation
-import weaviate
 import os
 
+your_wcs_url = os.getenv("WCS_DEMO_URL")
+your_wcs_key = os.getenv("WCS_DEMO_RO_KEY")
+
+# WCSInstantiation
+import weaviate
+
 client = weaviate.connect_to_wcs(
-    cluster_url=os.getenv("WCS_DEMO_URL"),  # Replace with your WCS URL
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY"))  # Replace with your WCS key
+    cluster_url=your_wcs_url,  # Replace with your WCS URL
+    auth_credentials=weaviate.auth.AuthApiKey(your_wcs_key)  # Replace with your WCS key
 )
 # END WCSInstantiation
 
@@ -15,9 +19,77 @@ assert client.is_live()  # This will raise an exception if the client is not liv
 
 
 # GetMeta
+import json
+
 metainfo = client.get_meta()
-print(metainfo)
+print(json.dumps(metainfo, indent=2))  # Print the meta information in a readable format
 # END GetMeta
+
+
+'''
+# OutputGetMeta
+{
+  "hostname": "http://[::]:8080",
+  "modules": {
+    "backup-gcs": {
+      "bucketName": "weaviate-wcs-prod-cust-europe-west2-workloads-backups",
+      "rootName": "8616b69e-f8d2-4547-ad92-70b9557591c0"
+    },
+    "generative-aws": {
+      "documentationHref": "https://docs.aws.amazon.com/bedrock/latest/APIReference/welcome.html",
+      "name": "Generative Search - AWS"
+    },
+    "generative-cohere": {
+      "documentationHref": "https://docs.cohere.com/reference/generate",
+      "name": "Generative Search - Cohere"
+    },
+    "generative-openai": {
+      "documentationHref": "https://platform.openai.com/docs/api-reference/completions",
+      "name": "Generative Search - OpenAI"
+    },
+    "generative-palm": {
+      "documentationHref": "https://cloud.google.com/vertex-ai/docs/generative-ai/chat/test-chat-prompts",
+      "name": "Generative Search - Google PaLM"
+    },
+    "qna-openai": {
+      "documentationHref": "https://platform.openai.com/docs/api-reference/completions",
+      "name": "OpenAI Question & Answering Module"
+    },
+    "ref2vec-centroid": {},
+    "reranker-cohere": {
+      "documentationHref": "https://txt.cohere.com/rerank/",
+      "name": "Reranker - Cohere"
+    },
+    "text2vec-aws": {
+      "documentationHref": "https://cloud.google.com/vertex-ai/docs/generative-ai/embeddings/get-text-embeddings",
+      "name": "AWS Module"
+    },
+    "text2vec-cohere": {
+      "documentationHref": "https://docs.cohere.ai/embedding-wiki/",
+      "name": "Cohere Module"
+    },
+    "text2vec-huggingface": {
+      "documentationHref": "https://huggingface.co/docs/api-inference/detailed_parameters#feature-extraction-task",
+      "name": "Hugging Face Module"
+    },
+    "text2vec-jinaai": {
+      "documentationHref": "https://jina.ai/embeddings/",
+      "name": "JinaAI Module"
+    },
+    "text2vec-openai": {
+      "documentationHref": "https://platform.openai.com/docs/guides/embeddings/what-are-embeddings",
+      "name": "OpenAI Module"
+    },
+    "text2vec-palm": {
+      "documentationHref": "https://cloud.google.com/vertex-ai/docs/generative-ai/embeddings/get-text-embeddings",
+      "name": "Google PaLM Module"
+    }
+  },
+  "version": "1.23.8"
+}
+# END OutputGetMeta
+'''
+
 
 client.close()
 
@@ -27,8 +99,8 @@ import weaviate
 import os
 
 client = weaviate.connect_to_wcs(
-    cluster_url=os.getenv("WCS_DEMO_URL"),  # Replace with your WCS URL
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY"))  # Replace with your WCS key
+    cluster_url=your_wcs_url,  # Replace with your WCS URL
+    auth_credentials=weaviate.auth.AuthApiKey(your_wcs_key)  # Replace with your WCS key
 )
 
 try:
@@ -45,10 +117,12 @@ finally:  # This will always be executed, even if an exception is raised
 import weaviate
 import os
 
+# Context manager (`with`) will close connection upon exit
 with weaviate.connect_to_wcs(
-    cluster_url=os.getenv("WCS_DEMO_URL"),  # Replace with your WCS URL
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY"))  # Replace with your WCS key
-) as client:  # Use this context manager to ensure the connection is closed
+    cluster_url=your_wcs_url,  # Replace with your WCS URL
+    auth_credentials=weaviate.auth.AuthApiKey(your_wcs_key)  # Replace with your WCS key
+) as client:
+
     # Work with the client here - e.g.:
     assert client.is_live()
     pass
