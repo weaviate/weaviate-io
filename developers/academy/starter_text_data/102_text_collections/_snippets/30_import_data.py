20dd3007ee8d9cf269e168a0dd4cdd8555fa28da
JP Hwang <me@jphwang.com>
2024-02-17 12:10:42 +0000

    Add 102_text_collections

--- developers/academy/starter_text_data/102_text_collections/_snippets/30_import_data.py
+++ developers/academy/starter_text_data/102_text_collections/_snippets/30_import_data.py
@@ -8,6 +8,7 @@ import weaviate
 import pandas as pd
 import requests
 import datetime
+import json
 from weaviate.util import generate_uuid5
 
 client = weaviate.connect_to_wcs(
@@ -29,22 +30,38 @@ resp = requests.get(data_url)
 df = pd.DataFrame(resp.json())
 
 try:
+    # Get the collection
     movies = client.collections.get("Movie")
+
+    # Enter context manager
     with movies.batch.dynamic() as batch:
-        for i, movie in df[:10].iterrows():
+
+        # Loop through the data
+        for i, movie in df.iterrows():
+
+            # Convert data types
             release_date = datetime.datetime.strptime(movie["release_date"], "%Y-%m-%d")
+            genre_ids = json.loads(movie["genre_ids"])
+            # Build the object payload
             movie_obj = {
                 "title": movie["title"],
                 "overview": movie["overview"],
                 "vote_average": movie["vote_average"],
-                "genre_ids": movie["genre_ids"],
+                "genre_ids": genre_ids,
                 "release_date": release_date,
                 "tmdb_id": movie["id"]
             }
             batch.add_object(
                 properties=movie_obj,
                 uuid=generate_uuid5(movie["id"])
+                # references=reference_obj  # You can add references here
             )
+            # Batcher automatically sends batches
+
+    # Check for failed objects
+    if len(movies.batch.failed_objects) > 0:
+        print(f"Failed to import {len(movies.batch.failed_objects)} objects")
+        print(movies.batch.failed_objects)
 
 finally:
     client.close()
