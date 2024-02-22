b3c412ddf40eca9b1d1e24c56c01c9cee7656d1f
Sebastian Witalec <sebawita@users.noreply.github.com>
2024-02-22 22:00:53 +0100

    update site search

--- src/components/CommandMenu/query.js
+++ src/components/CommandMenu/query.js
@@ -1,11 +1,12 @@
 const url = 'https://search.weaviate.io/v1/graphql';
+const read = 'MgivtQrUg6J5JHNNoyPjHGgv23MdjrTRiKxT';
 
 export const runQuery = async (searchTerm, limit=10) => {
     const parsedSearchTerm = searchTerm.replace(/"/g,"\\\"")
 
     const query = `{
         Get {
-            PageChunkOpenAI(
+            PageChunk(
                 hybrid: {
                     query: \"${parsedSearchTerm}\"
                     alpha: 0.4
@@ -19,7 +20,8 @@ export const runQuery = async (searchTerm, limit=10) => {
     const response = await fetch(url, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${read}`
         },
         body: JSON.stringify({query: query})
     });
