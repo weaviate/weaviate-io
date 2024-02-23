9d58e040c1f43356fba45561293315b964fb7081
JP Hwang <me@jphwang.com>
2024-02-23 21:12:34 +0000

    Update work_with_texts

--- tests/start-weaviate.sh
+++ tests/start-weaviate.sh
@@ -4,4 +4,5 @@ echo "Run Docker compose"
 nohup docker-compose -f ./tests/docker-compose.yml up -d
 nohup docker-compose -f ./tests/docker-compose-anon.yml up -d
 nohup docker-compose -f ./tests/docker-compose-anon-2.yml up -d
+nohup docker-compose -f ./tests/docker-compose-anon-clip.yml up -d
 nohup docker-compose -f ./tests/docker-compose-three-nodes.yml up -d
--- tests/test_academy.py
+++ tests/test_academy.py
@@ -2,15 +2,14 @@ import pytest
 import utils
 
 
-@pytest.mark.pyv3
+@pytest.mark.pyv4
 @pytest.mark.parametrize(
     "script_loc",
     [
-        "./developers/academy/zero_to_mvp/_snippets/setup.py",
-        "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/05_create_instance.py",
-        "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/20_schema.py",
-        "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/30_import.py",
-        "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/40_import_example_1.py",
+        "./developers/academy/py/starter_text_data/_snippets/101_connect.py",
+        "./developers/academy/py/starter_text_data/_snippets/102_collection.py",
+        "./developers/academy/py/starter_text_data/_snippets/103_searches.py",
+        "./developers/academy/py/starter_text_data/_snippets/104_rag.py",
     ],
 )
 def test_against_empty_weaviate_pyv4(empty_weaviates, script_loc):
@@ -18,20 +17,36 @@ def test_against_empty_weaviate_pyv4(empty_weaviates, script_loc):
     exec(proc_script)
 
 
-@pytest.mark.pyv3
-@pytest.mark.parametrize(
-    "script_loc",
-    [
-        "./developers/academy/zero_to_mvp/104_queries_2/_snippets/10_bm25.py",
-        "./developers/academy/zero_to_mvp/104_queries_2/_snippets/20_hybrid.py",
-        "./developers/academy/zero_to_mvp/104_queries_2/_snippets/30_generative.py",
-        "./developers/academy/zero_to_mvp/104_queries_2/_snippets/40_qna.py",
-    ],
-)
-def test_against_edu_demo_pyv3(empty_weaviates, script_loc):
-    temp_proc_script_loc = utils.load_and_prep_temp_file(
-        script_loc,
-        lang="py",
-        custom_replace_pairs=utils.edu_readonly_replacements
-    )
-    exec(temp_proc_script_loc.read_text())
+# @pytest.mark.pyv3
+# @pytest.mark.parametrize(
+#     "script_loc",
+#     [
+#         "./developers/academy/zero_to_mvp/_snippets/setup.py",
+#         "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/05_create_instance.py",
+#         "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/20_schema.py",
+#         "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/30_import.py",
+#         "./developers/academy/zero_to_mvp/103_schema_and_imports/_snippets/40_import_example_1.py",
+#     ],
+# )
+# def test_against_empty_weaviate_pyv4(empty_weaviates, script_loc):
+#     proc_script = utils.load_and_prep_script(script_loc)
+#     exec(proc_script)
+
+
+# @pytest.mark.pyv3
+# @pytest.mark.parametrize(
+#     "script_loc",
+#     [
+#         "./developers/academy/zero_to_mvp/104_queries_2/_snippets/10_bm25.py",
+#         "./developers/academy/zero_to_mvp/104_queries_2/_snippets/20_hybrid.py",
+#         "./developers/academy/zero_to_mvp/104_queries_2/_snippets/30_generative.py",
+#         "./developers/academy/zero_to_mvp/104_queries_2/_snippets/40_qna.py",
+#     ],
+# )
+# def test_against_edu_demo_pyv3(empty_weaviates, script_loc):
+#     temp_proc_script_loc = utils.load_and_prep_temp_file(
+#         script_loc,
+#         lang="py",
+#         custom_replace_pairs=utils.edu_readonly_replacements
+#     )
+#     exec(temp_proc_script_loc.read_text())
