d50093a0eff85d1b07de81f76d6e03c947b1d5c3
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-02-26 15:24:33 +0000

    Component Referencing

--- src/components/scriptSwitch/index.jsx
+++ src/components/scriptSwitch/index.jsx
@@ -14,25 +14,29 @@ const CustomScriptLoader = () => {
       document.body.appendChild(script);
     };
 
-    const scriptsMap = new Map([
-      [
-        '/developers/weaviate',
-        'https://static.scarf.sh/a.png?x-pxid=2758e82f-6546-4356-a8bd-5b5c16368efb',
-      ],
-      [
-        '/pricing',
-        'https://static.scarf.sh/a.png?x-pxid=5c79460c-47af-4477-a1d9-3624dcce35d3',
-      ],
+    const getScriptForPath = (pathname) => {
+      const scriptsMap = {
+        '/developers/':
+          'https://static.scarf.sh/a.png?x-pxid=2758e82f-6546-4356-a8bd-5b5c16368efb',
+        '/pricing':
+          'https://static.scarf.sh/a.png?x-pxid=5c79460c-47af-4477-a1d9-3624dcce35d3',
+      };
 
-      [
-        'default',
-        'https://static.scarf.sh/a.png?x-pxid=a41b0758-a3a9-4874-a880-8b5d5a363d40',
-      ],
-    ]);
+      const defaultScript =
+        'https://static.scarf.sh/a.png?x-pxid=a41b0758-a3a9-4874-a880-8b5d5a363d40';
+
+      // Check for a direct match or base path match (for handling subpages)
+      for (const [basePath, script] of Object.entries(scriptsMap)) {
+        if (pathname === basePath || pathname.startsWith(basePath)) {
+          return script;
+        }
+      }
+
+      return defaultScript;
+    };
 
     const currentPath = window.location.pathname;
-    const scriptToLoad =
-      scriptsMap.get(currentPath) || scriptsMap.get('default');
+    const scriptToLoad = getScriptForPath(currentPath);
     loadScript(scriptToLoad);
   }, []);
 
