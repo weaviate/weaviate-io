ffb50129c77bcd79f48fde80b5f51ef0f34cb044
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-03-12 11:50:38 +0000

    update

--- src/components/Knowledgebase/Knowledgeheader.jsx
+++ src/components/Knowledgebase/Knowledgeheader.jsx
@@ -1,9 +1,38 @@
 import Link from '@docusaurus/Link';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
 import { LinkButton, ButtonContainer } from '/src/theme/Buttons';
+import knowledge from '/data/knowledgecards.json';
 
 export default function KnowledgeHeader() {
+  const [selectedCard, setSelectedCard] = useState('All');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const allCards = knowledge.all;
+
+  const handleCardFilter = (card) => {
+    setSelectedCard(card);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredCards = allCards.filter((card) => {
+    // Check if 'selectedCard' matches and if 'searchQuery' is in 'title', 'text', or any 'tag'
+    return (
+      (selectedCard === 'All' || card.type === selectedCard) &&
+      ((card.title &&
+        card.title.toLowerCase().includes(searchQuery.toLowerCase())) ||
+        (card.text &&
+          card.text.toLowerCase().includes(searchQuery.toLowerCase())) ||
+        (card.tags &&
+          card.tags.some(
+            (tag) =>
+              tag && tag.toLowerCase().includes(searchQuery.toLowerCase())
+          )))
+    );
+  });
   return (
     <header>
       <div className="container">
@@ -16,18 +45,15 @@ export default function KnowledgeHeader() {
             </p>
           </div>
         </div>
-        {/* 
-        <div className={styles.buttons}>
-          <Link className={styles.buttonGradient} to="/developers/weaviate">
-            Documentation
-          </Link>
-          <Link
-            className={styles.buttonOutline}
-            to="https://console.weaviate.cloud/"
-          >
-            Try Now
-          </Link>
-        </div> */}
+      </div>
+      <div className={styles.searchContainer}>
+        <input
+          type="text"
+          className={styles.searchInput}
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
       </div>
     </header>
   );
