2bfbd06b45927df0d6c6f008d4a686020d9d366a
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-03-29 16:20:36 +0000

    Page Update

---
+++ src/components/Service/CaseStudy/Header/index.jsx
@@ -0,0 +1,26 @@
+import Link from '@docusaurus/Link';
+import React from 'react';
+import styles from './styles.module.scss';
+import { LinkButton, ButtonContainer } from '/src/theme/Buttons';
+
+export default function StudyHeader() {
+  return (
+    <header className={styles.headerSecurity}>
+      <div className="container">
+        <div className={styles.box}>
+          <div className={styles.logo}></div>
+          <span>Case Study</span>
+          <h1>
+            Transforming Customer Service<br></br> with Generative AI
+          </h1>
+          <div className={styles.headerBox}>
+            <p>
+              Learn how Neople Assistants transform customer service<br></br>{' '}
+              with Weaviate
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
