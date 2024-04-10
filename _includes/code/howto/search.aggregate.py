76fa46f6a9edfd5c0d4c160e8d2c72e94775ac6d
JP Hwang <me@jphwang.com>
2024-04-10 19:21:14 +0100

    Update syntax on search pages

--- _includes/code/howto/search.aggregate.py
+++ _includes/code/howto/search.aggregate.py
@@ -3,191 +3,188 @@
 # ================================
 
 import weaviate
-import weaviate.classes as wvc
+from weaviate.auth import AuthApiKey
 import os
 
 client = weaviate.connect_to_wcs(
     cluster_url=os.getenv("WCS_DEMO_URL"),
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY")),
+    auth_credentials=AuthApiKey(os.getenv("WCS_DEMO_RO_KEY")),
     headers={
         "X-OpenAI-Api-Key": os.getenv("OPENAI_APIKEY"),
     }
 )
 
-try:
+# ===============================
+# ===== meta count EXAMPLES =====
+# ===============================
 
-    # ===============================
-    # ===== meta count EXAMPLES =====
-    # ===============================
+# MetaCount Python
+jeopardy = client.collections.get("JeopardyQuestion")
+# highlight-start
+response = jeopardy.aggregate.over_all(total_count=True)
+# highlight-end
 
-    # MetaCount Python
-    jeopardy = client.collections.get("JeopardyQuestion")
+print(response.total_count)
+# END MetaCount Python
+
+# Tests
+assert response.total_count > 0
+# End test
+
+
+# ==================================
+# ===== Text property EXAMPLES =====
+# ==================================
+
+# TextProp Python
+from weaviate.classes.query import Metrics
+
+jeopardy = client.collections.get("JeopardyQuestion")
+response = jeopardy.aggregate.over_all(
     # highlight-start
-    response = jeopardy.aggregate.over_all(total_count=True)
+    return_metrics=Metrics("answer").text(
+        top_occurrences_count=True,
+        top_occurrences_value=True,
+        min_occurrences=5  # Threshold minimum count
+    )
     # highlight-end
+)
 
-    print(response.total_count)
-    # END MetaCount Python
+print(response.properties["answer"].top_occurrences)
+# END TextProp Python
 
-    # Tests
-    assert response.total_count > 0
-    # End test
-
-
-    # ==================================
-    # ===== Text property EXAMPLES =====
-    # ==================================
-
-    # TextProp Python
-    import weaviate.classes as wvc
-
-    jeopardy = client.collections.get("JeopardyQuestion")
-    response = jeopardy.aggregate.over_all(
-        # highlight-start
-        return_metrics=wvc.query.Metrics("answer").text(
-            top_occurrences_count=True,
-            top_occurrences_value=True,
-            min_occurrences=5  # Threshold minimum count
-        )
-        # highlight-end
-    )
-
-    print(response.properties["answer"].top_occurrences)
-    # END TextProp Python
-
-    # Tests
-    assert type(response.properties["answer"].top_occurrences) == list
-    assert response.properties["answer"].top_occurrences[0].count >= 5
-    # End test
+# Tests
+assert type(response.properties["answer"].top_occurrences) == list
+assert response.properties["answer"].top_occurrences[0].count >= 5
+# End test
 
 
-    # ====================================
-    # ===== int property EXAMPLES =====
-    # ====================================
+# ====================================
+# ===== int property EXAMPLES =====
+# ====================================
 
-    # IntProp Python
-    import weaviate.classes as wvc
+# IntProp Python
+from weaviate.classes.query import Metrics
 
-    jeopardy = client.collections.get("JeopardyQuestion")
-    response = jeopardy.aggregate.over_all(
-        # highlight-start
-        # Use `.number` for floats (`NUMBER` datatype in Weaviate)
-        return_metrics=wvc.query.Metrics("points").integer(sum_=True, maximum=True, minimum=True),
-        # highlight-end
-    )
-
-    print(response.properties["points"].sum_)
-    print(response.properties["points"].minimum)
-    print(response.properties["points"].maximum)
-    # END IntProp Python
-
-
-    # Tests
-    assert response.properties["points"].sum_ > 0
-    assert response.properties["points"].maximum > 0
-    # End test
-
-
-    # ============================
-    # ===== groupBy EXAMPLES =====
-    # ============================
-
-    # groupBy Python
-    from weaviate.classes.aggregate import GroupByAggregate
-
-    jeopardy = client.collections.get("JeopardyQuestion")
-    response = jeopardy.aggregate.over_all(
-        # highlight-start
-        group_by=GroupByAggregate(prop="round")
-        # highlight-end
-    )
-
-    # print rounds names and the count for each
-    for group in response.groups:
-        print(f"Value: {group.grouped_by.value} Count: {group.total_count}")
-    # END groupBy Python
-
-
-    # Tests
-    assert response.groups[0].grouped_by.prop == "round"
-    assert response.groups[0].total_count > 0
-    # End test
-
-
-    # =========================================
-    # ===== nearTextWithLimit EXAMPLES =====
-    # =========================================
-
-    # nearTextWithLimit Python
-    import weaviate.classes as wvc
-
-    jeopardy = client.collections.get("JeopardyQuestion")
-    response = jeopardy.aggregate.near_text(
-        query="animals in space",
-        # highlight-start
-        object_limit=10,
-        # highlight-end
-        return_metrics=wvc.query.Metrics("points").number(sum_=True),
-    )
-
-    print(response.properties["points"].sum_)
-    # END nearTextWithLimit Python
-
-
-    # Tests
-    assert response.properties["points"].sum_ > 0
-    # End test
-
-
-    # ============================
-    # ===== nearTextWithDistance EXAMPLES =====
-    # ============================
-
-    # nearTextWithDistance Python
-    import weaviate.classes as wvc
-
-    jeopardy = client.collections.get("JeopardyQuestion")
-    response = jeopardy.aggregate.near_text(
-        query="animals in space",
-        # highlight-start
-        distance=0.19,
-        # highlight-end
-        return_metrics=wvc.query.Metrics("points").number(sum_=True),
-    )
-
-    print(response.properties["points"].sum_)
-    # END nearTextWithDistance Python
-
-
-    # Tests
-    assert response.properties["points"].sum_ > 0
-    # End test
-
-
-    # =================================
-    # ===== where filter EXAMPLES =====
-    # =================================
-
-    # whereFilter Python
+jeopardy = client.collections.get("JeopardyQuestion")
+response = jeopardy.aggregate.over_all(
     # highlight-start
-    from weaviate.classes.query import Filter
+    # Use `.number` for floats (`NUMBER` datatype in Weaviate)
+    return_metrics=Metrics("points").integer(sum_=True, maximum=True, minimum=True),
     # highlight-end
+)
 
-    jeopardy = client.collections.get("JeopardyQuestion")
-    response = jeopardy.aggregate.over_all(
-        # highlight-start
-        filters=Filter.by_property("round").equal("Final Jeopardy!"),
-        # highlight-end
-    )
-
-    print(response.total_count)
-    # END whereFilter Python
+print(response.properties["points"].sum_)
+print(response.properties["points"].minimum)
+print(response.properties["points"].maximum)
+# END IntProp Python
 
 
-    # Tests
-    assert response.total_count is not None
-    # End test
+# Tests
+assert response.properties["points"].sum_ > 0
+assert response.properties["points"].maximum > 0
+# End test
 
 
-finally:
-    client.close()
+# ============================
+# ===== groupBy EXAMPLES =====
+# ============================
+
+# groupBy Python
+from weaviate.classes.aggregate import GroupByAggregate
+
+jeopardy = client.collections.get("JeopardyQuestion")
+response = jeopardy.aggregate.over_all(
+    # highlight-start
+    group_by=GroupByAggregate(prop="round")
+    # highlight-end
+)
+
+# print rounds names and the count for each
+for group in response.groups:
+    print(f"Value: {group.grouped_by.value} Count: {group.total_count}")
+# END groupBy Python
+
+
+# Tests
+assert response.groups[0].grouped_by.prop == "round"
+assert response.groups[0].total_count > 0
+# End test
+
+
+# =========================================
+# ===== nearTextWithLimit EXAMPLES =====
+# =========================================
+
+# nearTextWithLimit Python
+from weaviate.classes.query import Metrics
+
+jeopardy = client.collections.get("JeopardyQuestion")
+response = jeopardy.aggregate.near_text(
+    query="animals in space",
+    # highlight-start
+    object_limit=10,
+    # highlight-end
+    return_metrics=Metrics("points").number(sum_=True),
+)
+
+print(response.properties["points"].sum_)
+# END nearTextWithLimit Python
+
+
+# Tests
+assert response.properties["points"].sum_ > 0
+# End test
+
+
+# ============================
+# ===== nearTextWithDistance EXAMPLES =====
+# ============================
+
+# nearTextWithDistance Python
+from weaviate.classes.query import Metrics
+
+jeopardy = client.collections.get("JeopardyQuestion")
+response = jeopardy.aggregate.near_text(
+    query="animals in space",
+    # highlight-start
+    distance=0.19,
+    # highlight-end
+    return_metrics=Metrics("points").number(sum_=True),
+)
+
+print(response.properties["points"].sum_)
+# END nearTextWithDistance Python
+
+
+# Tests
+assert response.properties["points"].sum_ > 0
+# End test
+
+
+# =================================
+# ===== where filter EXAMPLES =====
+# =================================
+
+# whereFilter Python
+# highlight-start
+from weaviate.classes.query import Filter
+# highlight-end
+
+jeopardy = client.collections.get("JeopardyQuestion")
+response = jeopardy.aggregate.over_all(
+    # highlight-start
+    filters=Filter.by_property("round").equal("Final Jeopardy!"),
+    # highlight-end
+)
+
+print(response.total_count)
+# END whereFilter Python
+
+
+# Tests
+assert response.total_count is not None
+# End test
+
+
+client.close()
--- _includes/code/howto/search.rerank.py
+++ _includes/code/howto/search.rerank.py
@@ -1,98 +1,99 @@
 # Howto: Search -> Reranking - Python examples
 
 import weaviate
-import weaviate.classes as wvc
+from weaviate.auth import AuthApiKey
 import os
 
 client = weaviate.connect_to_wcs(
     cluster_url=os.getenv("WCS_DEMO_URL"),
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY")),
+    auth_credentials=AuthApiKey(os.getenv("WCS_DEMO_RO_KEY")),
     headers={
         "X-OpenAI-Api-Key": os.getenv("OPENAI_APIKEY"),
         "X-Cohere-Api-Key": os.getenv("COHERE_APIKEY"),
     }
 )
 
-try:
+# ==================================
+# ===== nearText before rerank =====
+# ==================================
 
-    # ==================================
-    # ===== nearText before rerank =====
-    # ==================================
+# START nearText Python
+jeopardy = client.collections.get("JeopardyQuestion")
+response = jeopardy.query.near_text(
+    query="flying",
+    limit=10,
+)
 
-    # START nearText Python
-    jeopardy = client.collections.get("JeopardyQuestion")
-    response = jeopardy.query.near_text(
-        query="flying",
-        limit=10,
-    )
-
-    for o in response.objects:
-        print(o.properties)
-    # END nearText Python
+for o in response.objects:
+    print(o.properties)
+# END nearText Python
 
 
-    # Tests
-    assert response.objects[0].collection == "JeopardyQuestion"
-    # End test
+# Tests
+assert response.objects[0].collection == "JeopardyQuestion"
+# End test
 
 
-    # =================================
-    # ===== nearText after rerank =====
-    # =================================
+# =================================
+# ===== nearText after rerank =====
+# =================================
 
-    # START nearTextRerank Python
-    jeopardy = client.collections.get("JeopardyQuestion")
+# START nearTextRerank Python
+from weaviate.classes.query import Rerank, MetadataQuery
 
-    response = jeopardy.query.near_text(
-        query="flying",
-        limit=10,
-        rerank=wvc.query.Rerank(
-            prop="question",
-            query="publication"
-        ),
-        return_metadata=wvc.query.MetadataQuery(score=True)
-    )
+jeopardy = client.collections.get("JeopardyQuestion")
 
-    for o in response.objects:
-        print(o.properties)
-        print(o.metadata.score)
-    # END nearTextRerank Python
+response = jeopardy.query.near_text(
+    query="flying",
+    limit=10,
+    rerank=Rerank(
+        prop="question",
+        query="publication"
+    ),
+    return_metadata=MetadataQuery(score=True)
+)
 
-
-    # Tests
-    assert response.objects[0].collection == "JeopardyQuestion"
-    assert response.objects[0].metadata.score is not None
-    # End test
+for o in response.objects:
+    print(o.properties)
+    print(o.metadata.score)
+# END nearTextRerank Python
 
 
-    # ============================
-    # ===== bm25 with rerank =====
-    # ============================
-
-    # START bm25Rerank Python
-    jeopardy = client.collections.get("JeopardyQuestion")
-
-    response = jeopardy.query.bm25(
-        query="paper",
-        limit=10,
-        rerank=wvc.query.Rerank(
-            prop="question",
-            query="publication"
-        ),
-        return_metadata=wvc.query.MetadataQuery(score=True)
-    )
-
-    for o in response.objects:
-        print(o.properties)
-        print(o.metadata.rerank_score)
-    # END bm25Rerank Python
+# Tests
+assert response.objects[0].collection == "JeopardyQuestion"
+assert response.objects[0].metadata.score is not None
+# End test
 
 
-    # Tests
-    assert response.objects[0].collection == "JeopardyQuestion"
-    assert response.objects[0].metadata.score is not None
-    # End test
+# ============================
+# ===== bm25 with rerank =====
+# ============================
+
+# START bm25Rerank Python
+from weaviate.classes.query import Rerank, MetadataQuery
+
+jeopardy = client.collections.get("JeopardyQuestion")
+
+response = jeopardy.query.bm25(
+    query="paper",
+    limit=10,
+    rerank=Rerank(
+        prop="question",
+        query="publication"
+    ),
+    return_metadata=MetadataQuery(score=True)
+)
+
+for o in response.objects:
+    print(o.properties)
+    print(o.metadata.rerank_score)
+# END bm25Rerank Python
 
 
-finally:
-    client.close()
+# Tests
+assert response.objects[0].collection == "JeopardyQuestion"
+assert response.objects[0].metadata.score is not None
+# End test
+
+
+client.close()
