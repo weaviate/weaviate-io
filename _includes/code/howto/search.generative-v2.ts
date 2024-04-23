10899a475f426ad114fd25e565946b98c2e59c33
JP Hwang <me@jphwang.com>
2024-04-23 12:53:23 +0100

    add named vector generative search

--- _includes/code/howto/search.generative-v2.ts
+++ _includes/code/howto/search.generative-v2.ts
@@ -19,6 +19,34 @@ const client = weaviate.client({
 
 let result, generatePrompt, genResults;
 
+// ===============================================
+// ===== QUERY WITH TARGET VECTOR & nearText =====
+// ===============================================
+
+// NamedVectorNearText
+result = await client.graphql
+  .get()
+  .withClassName('WineReviewNV')
+  .withNearText({
+    concepts: ['a sweet German white wine'],
+    // highlight-start
+    targetVectors: ['title_country'],
+    // highlight-end
+  })
+  .withGenerate({
+    singlePrompt: 'Translate this into German: {review_body}',
+    groupedTask: 'Summarize these reviews',
+  })
+  .withLimit(2)
+  .withFields('title review_body country')
+  .do();
+
+console.log(JSON.stringify(result, null, 2));
+// END NamedVectorNearText
+
+// Tests
+assert.deepEqual(result.data.Get.JeopardyQuestion.length, 2);
+
 // =====================================
 // ===== SINGLE GENERATIVE EXAMPLE =====
 // =====================================
