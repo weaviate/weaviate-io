15515476e9636f72dd943f4a360308efa605210a
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2024-05-10 14:44:55 -0400

    generate

--- _includes/code/client-libraries/migrate-additional-ts2.ts
+++ _includes/code/client-libraries/migrate-additional-ts2.ts
@@ -30,3 +30,76 @@ result = await client.graphql
 
 console.log(JSON.stringify(result, null, 2));
 // END BuilderEx
+
+// =============================
+// === BATCH INSERTS
+// =============================
+
+// BatchEx
+let className = 'CollectionName';  // Replace with your collection name
+let dataObject = [...];
+
+let batcher5 = client.batch.objectsBatcher();
+for (const dataObj of dataObject)
+  batcher5 = batcher5.withObject({
+    class: className,
+    properties: dataObj,
+  });
+
+// Flush
+await batcher5.do();
+// END BatchEx
+
+// =============================
+// === FILTER DATA
+// =============================
+
+// FilterDataEx
+result = await client.graphql
+  .get()
+  .withClassName('JeopardyQuestion')
+  .withWhere({
+    operator: 'And',
+    operands: [
+      {
+        path: ['round'],
+        operator: 'Equal',
+        valueText: 'Double Jeopardy!',
+      },
+      {
+        path: ['points'],
+        operator: 'LessThan',
+        valueInt: 600,
+      },
+    ],
+  })
+  .withLimit(3)
+  .withFields('question answer round points')
+  .do();
+
+console.log(JSON.stringify(result, null, 2));
+// END FilterDataEx
+
+// =============================
+// === GENERATE NAMESPACE
+// =============================
+
+// GenerateNamespaceEx
+let result;
+generatePrompt = 'Convert this quiz question: {question} and answer: {answer} into a trivia tweet.';
+
+result = await client.graphql
+  .get()
+  .withClassName('JeopardyQuestion')
+  .withGenerate({
+    singlePrompt: generatePrompt,
+  })
+  .withNearText({
+    concepts: ['World history'],
+  })
+  .withFields('round')
+  .withLimit(2)
+  .do();
+
+console.log(JSON.stringify(result, null, 2));
+// END GenerateNamespaceEx
--- _includes/code/client-libraries/migrate-additional-ts3.ts
+++ _includes/code/client-libraries/migrate-additional-ts3.ts
@@ -90,3 +90,52 @@ result = await myCollection.query.nearText(['animals in movies'],{
 
 console.log(JSON.stringify(result.objects, null, 2));
 // END BuilderEx
+
+// =============================
+// === BATCH INSERTS
+// =============================
+
+// BatchEx
+const questions = client.collections.get("CollectionName")
+const dataObject = [...]; // your data
+
+await questions.data.insertMany(dataBatch);
+// END BatchEx
+
+// =============================
+// === FILTER DATA
+// =============================
+
+// FilterDataEx
+import weaviate, { Filters } from 'weaviate-client';
+const myCollection = client.collections.get('JeopardyQuestion');
+
+const result = await myCollection.query.fetchObjects({
+  returnProperties: ['question', 'answer','round', 'points'],
+  filters: Filters.and(
+     myCollection.filter.byProperty('round').equal('Double Jeopardy!'),
+     myCollection.filter.byProperty('points').lessThan(600)
+    ),
+  limit: 3,
+ })
+
+console.log(JSON.stringify(result, null, 2));
+// END FilterDataEx
+
+// =============================
+// === GENERATE NAMESPACE
+// =============================
+
+// GenerateNamespaceEx
+const generatePrompt = `Convert this quiz question: {question} and answer: {answer} into a trivia tweet.`;
+
+const myCollection = client.collections.get('JeopardyQuestion');
+const result = await myCollection.generate.nearText(['World history'],{
+    singlePrompt: generatePrompt,
+  },{
+    limit: 2,
+    returnProperties: ['round'],
+})
+
+console.log(JSON.stringify(result.objects, null, 2));
+// END GenerateNamespaceEx
