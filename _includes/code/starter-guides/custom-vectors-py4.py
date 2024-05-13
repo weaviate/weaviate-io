989e36c32f4737c92a9e5f8f1c09c7adb2c067e0
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2024-05-13 12:21:15 -0400

    reformat

--- _includes/code/starter-guides/custom-vectors-py4.py
+++ _includes/code/starter-guides/custom-vectors-py4.py
@@ -5,7 +5,7 @@
 # END-ANY
 
 # For testing and rerunning script
-if (client.collections.exists("Question")):
+if client.collections.exists("Question"):
     client.collections.delete("Question")
 
 # START create schema
@@ -15,16 +15,37 @@ import weaviate.classes as wvc
 # The with-as context manager closes the connect when your code exits
 with weaviate.connect_to_wcs(
     cluster_url=os.getenv("WCS_URL"),
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_API_KEY"))
+    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_API_KEY")),
 ) as client:
     questions = client.collections.create(
         name="Question2",
         properties=[
-            wvc.config.Property(name="question", description="What to ask", data_type=wvc.config.DataType.TEXT, tokenization=wvc.config.Tokenization.WORD, index_searchable=True, index_filterable=True,), 
-            wvc.config.Property(name="answer", description="The clue", data_type=wvc.config.DataType.TEXT, tokenization=wvc.config.Tokenization.WORD, index_searchable=True, index_filterable=True,),
-            wvc.config.Property(name="category", description="The subject", data_type=wvc.config.DataType.TEXT, tokenization=wvc.config.Tokenization.WORD, index_searchable=True, index_filterable=True,),
-       ],
-        vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_openai(),  
-        generative_config=wvc.config.Configure.Generative.openai() 
+            wvc.config.Property(
+                name="question",
+                description="What to ask",
+                data_type=wvc.config.DataType.TEXT,
+                tokenization=wvc.config.Tokenization.WORD,
+                index_searchable=True,
+                index_filterable=True,
+            ),
+            wvc.config.Property(
+                name="answer",
+                description="The clue",
+                data_type=wvc.config.DataType.TEXT,
+                tokenization=wvc.config.Tokenization.WORD,
+                index_searchable=True,
+                index_filterable=True,
+            ),
+            wvc.config.Property(
+                name="category",
+                description="The subject",
+                data_type=wvc.config.DataType.TEXT,
+                tokenization=wvc.config.Tokenization.WORD,
+                index_searchable=True,
+                index_filterable=True,
+            ),
+        ],
+        vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_openai(),
+        generative_config=wvc.config.Configure.Generative.openai(),
     )
 # END create schema
