4308637943d454c5a12c10468db618569c326d7f
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2024-05-15 15:26:06 -0400

    DR-363 add-go-example

---
+++ _includes/code/howto/go/docs/manage-data.create_auto-multitenancy.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/weaviate/weaviate-go-client/v4/weaviate"
+	"github.com/weaviate/weaviate/entities/models"
+)
+
+func main() {
+	client, err := weaviate.NewClient(weaviate.Config{
+		Host:   "localhost:8080",
+		Scheme: "http",
+	})
+	if err != nil {
+		log.Fatalf("create client: %v", err)
+	}
+
+	ctx := context.Background()
+
+	createClassWithAutoTenantEnabled(ctx, client)
+	createClassWithoutAutoTenantAndToggleOn(ctx, client)
+}
+
+// START enable autoMT
+func createClassWithAutoTenantEnabled(ctx context.Context, client *weaviate.Client) {
+	class := &models.Class{
+		Class: "AutoTenantEnabledOnCreate",
+		Properties: []*models.Property{
+			{Name: "textProp", DataType: []string{"text"}},
+		},
+		MultiTenancyConfig: &models.MultiTenancyConfig{
+			Enabled:            true,
+			AutoTenantCreation: true,
+		},
+	}
+	err := client.Schema().ClassCreator().WithClass(class).Do(ctx)
+	if err != nil {
+		log.Fatalf("create class: %v", err)
+	}
+}
+
+// END enable autoMT
+
+// Start update autoMT
+func createClassWithoutAutoTenantAndToggleOn(ctx context.Context, client *weaviate.Client) {
+	class := &models.Class{
+		Class: "AutoTenantEnabledOnUpdate",
+		Properties: []*models.Property{
+			{Name: "textProp", DataType: []string{"text"}},
+		},
+		MultiTenancyConfig: &models.MultiTenancyConfig{
+			Enabled: true,
+		},
+	}
+	err := client.Schema().ClassCreator().WithClass(class).Do(ctx)
+	if err != nil {
+		log.Fatalf("create class: %v", err)
+	}
+	// END update autoMT
+
+	existing, err := client.Schema().ClassGetter().
+		WithClassName(class.Class).Do(ctx)
+	if err != nil {
+		log.Fatalf("get existing class %q: %v", class.Class, err)
+	}
+
+	existing.MultiTenancyConfig.AutoTenantCreation = true
+
+	err = client.Schema().ClassUpdater().WithClass(existing).Do(ctx)
+	if err != nil {
+		log.Fatalf("enable autotenant: %v", err)
+	}
+}
