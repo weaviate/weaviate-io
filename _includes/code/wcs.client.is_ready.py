5a61c9b0ed51b56b7e0081a44f0952db711ede35
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2024-05-15 12:37:37 -0400

    includes

--- _includes/code/wcs.client.is_ready.py
+++ _includes/code/wcs.client.is_ready.py
@@ -4,8 +4,8 @@ import weaviate
 import os
 
 with weaviate.connect_to_wcs(
-    cluster_url=os.getenv("WCS_DEMO_URL"),  # Replace with your WCD URL
-    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY")),  # Replace with your WCD key
+    cluster_url=os.getenv("WCS_DEMO_URL"),  # Replace with your Weaviate Cloud URL
+    auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_DEMO_RO_KEY")),  # Replace with your Weaviate Cloud key
     headers={'X-OpenAI-Api-key': os.getenv("OPENAI_APIKEY")}  # Replace with your vectorizer API key
 ) as client:  # Use this context manager to ensure the connection is closed
     print(client.is_ready())
