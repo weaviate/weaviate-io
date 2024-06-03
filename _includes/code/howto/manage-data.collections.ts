a7ebbfb9781cf3a0faa49f517bc762359acb61f7
Sebastian Witalec <sebawita@users.noreply.github.com>
2024-06-03 12:56:04 +0200

    update collection imports

--- _includes/code/howto/manage-data.collections.ts
+++ _includes/code/howto/manage-data.collections.ts
@@ -6,6 +6,7 @@ import assert from 'assert';
 // ===== INSTANTIATION-COMMON =====
 // ================================
 import weaviate, { WeaviateClient } from 'weaviate-client';
+import { vectorizer, generative, dataType, tokenization, configure, reconfigure } from 'weaviate-client';
 
 const client: WeaviateClient = await weaviate.connectToWeaviateCloud(
   process.env.WCD_URL,
@@ -62,17 +63,24 @@ try {
   // ignore error if class doesn't exist
 }
 
+/*
 // START CreateCollectionWithProperties
-newCollection = await client.collections.create({
+import { dataType } from 'weaviate-client';
+
+// END CreateCollectionWithProperties
+*/
+
+// START CreateCollectionWithProperties
+await client.collections.create({
   name: 'Article',
   properties: [
     {
       name: 'title',
-      dataType: weaviate.configure.dataType.TEXT,
+      dataType: dataType.TEXT,
     },
     {
       name: 'body',
-      dataType: weaviate.configure.dataType.TEXT,
+      dataType: dataType.TEXT,
     },
   ],
 })
@@ -94,21 +102,28 @@ console.log(collectionConfig)
 // ===== CREATE A COLLECTION WITH NAMED VECTORS =====
 // ==================================================
 
+/*
 // START BasicNamedVectors
-newCollection = await client.collections.create({
+import { vectorizer, dataType } from 'weaviate-client';
+
+// END BasicNamedVectors
+*/
+
+// START BasicNamedVectors
+await client.collections.create({
   name: 'ArticleNV',
 
   // highlight-start
   vectorizers: [
-    weaviate.configure.vectorizer.text2VecCohere({
+    vectorizer.text2VecCohere({
       name: 'title',
       sourceProperties: ['title']
     }),
-    weaviate.configure.vectorizer.text2VecOpenAI({
+    vectorizer.text2VecOpenAI({
       name: 'body',
       sourceProperties: ['body'],
     }),
-    weaviate.configure.vectorizer.text2VecOpenAI({
+    vectorizer.text2VecOpenAI({
       name: 'title_country',
       sourceProperties: ['title','country'],
     })
@@ -116,22 +131,11 @@ newCollection = await client.collections.create({
   // highlight-end
 
   properties: [
-    {
-      name: 'title',
-      dataType: weaviate.configure.dataType.TEXT,
-    },
-    {
-      name: 'body',
-      dataType: weaviate.configure.dataType.TEXT,
-    },
-    {
-      name: 'country',
-      dataType: weaviate.configure.dataType.TEXT,
-    },
+    { name: 'title', dataType: dataType.TEXT },
+    { name: 'body', dataType: dataType.TEXT },
+    { name: 'country', dataType: dataType.TEXT },
   ],
 })
-// The returned value is the full collection definition, showing all defaults
-console.log(JSON.stringify(newCollection, null, 2));
 // END BasicNamedVectors
 
 // Test
@@ -153,21 +157,22 @@ await client.collections.delete('ArticleNV')
 // ===== CREATE A COLLECTION WITH VECTORIZER =====
 // ===============================================
 
+/*
 // START Vectorizer
-newCollection = await client.collections.create({
+import { vectorizer, dataType } from 'weaviate-client';
+
+// END Vectorizer
+*/
+
+// START Vectorizer
+await client.collections.create({
   name: 'Article',
   // highlight-start
-  vectorizers: weaviate.configure.vectorizer.text2VecOpenAI(),
+  vectorizers: vectorizer.text2VecOpenAI(),
   // highlight-end
   properties: [
-    {
-      name: 'title',
-      dataType: weaviate.configure.dataType.TEXT,
-    },
-    {
-      name: 'body',
-      dataType: weaviate.configure.dataType.TEXT,
-    },
+    { name: 'title', dataType: dataType.TEXT },
+    { name: 'body', dataType: dataType.TEXT },
   ],
 })
 // END Vectorizer
@@ -185,22 +190,23 @@ await client.collections.delete('Article')
 // ===== SetVectorIndexType =====
 // ===========================
 
+/*
 // START SetVectorIndexType
-newCollection = await client.collections.create({
+import { vectorizer, dataType, configure } from 'weaviate-client';
+
+// END SetVectorIndexType
+*/
+
+// START SetVectorIndexType
+await client.collections.create({
   name: 'Article',
-  vectorizers: weaviate.configure.vectorizer.text2VecOpenAI(),
+  vectorizers: vectorizer.text2VecOpenAI(),
   // highlight-start
-  vectorIndex: weaviate.configure.vectorIndex.hnsw(),
+  vectorIndex: configure.vectorIndex.hnsw(),
   // highlight-end
   properties: [
-    {
-      name: 'title',
-      dataType: weaviate.configure.dataType.TEXT,
-    },
-    {
-      name: 'body',
-      dataType: weaviate.configure.dataType.TEXT,
-    },
+    { name: 'title', dataType: dataType.TEXT },
+    { name: 'body', dataType: dataType.TEXT },
   ],
 })
 // END SetVectorIndexType
@@ -219,13 +225,20 @@ await client.collections.delete(collectionName)
 // ===== SetVectorIndexParams =====
 // ===========================
 
+/*
 // START SetVectorIndexParams
-newCollection = await client.collections.create({
+import { configure } from 'weaviate-client';
+
+// END SetVectorIndexParams
+*/
+
+// START SetVectorIndexParams
+await client.collections.create({
   name: 'Article',
   // Additional configuration not shown
   // highlight-start
-  vectorIndex: weaviate.configure.vectorIndex.flat({
-    quantizer: weaviate.configure.vectorIndex.quantizer.bq({
+  vectorIndex: configure.vectorIndex.flat({
+    quantizer: configure.vectorIndex.quantizer.bq({
       rescoreLimit: 200,
       cache: true
     }),
@@ -245,16 +258,23 @@ assert.equal(result.properties.length, 2);
 // Delete the class to recreate it
 await client.collections.delete(collectionName)
 
-
 // ===========================
 // ===== MODULE SETTINGS =====
 // ===========================
+
+/*
+// START ModuleSettings
+import { vectorizer } from 'weaviate-client';
+
+// END ModuleSettings
+*/
+
 
 // START ModuleSettings
 newCollection = await client.collections.create({
   name: 'Article',
   // highlight-start
-  vectorizers: weaviate.configure.vectorizer.text2VecCohere({
+  vectorizers: vectorizer.text2VecCohere({
     model: 'embed-multilingual-v2.0',
     vectorizeCollectionName: true,
   }),
@@ -271,8 +291,6 @@ assert.equal(
   'embed-multilingual-v2.0'
 );
 
-
-
 // Delete the class to recreate it
 await client.collections.delete(collectionName)
 
@@ -280,27 +298,34 @@ await client.collections.delete(collectionName)
 // ===== MODULE SETTINGS PROPERTY =====
 // ====================================
 
+/*
+// START PropModuleSettings
+import { vectorizer, dataType, tokenization, configure } from 'weaviate-client';
+
+// END PropModuleSettings
+*/
+
 // START PropModuleSettings
 newCollection = await client.collections.create({
   name: 'Article',
-  vectorizers: weaviate.configure.vectorizer.text2VecHuggingFace(),
+  vectorizers: vectorizer.text2VecHuggingFace(),
   properties: [
     {
       name: 'title',
-      dataType: weaviate.configure.dataType.TEXT,
+      dataType: dataType.TEXT,
       // highlight-start
       vectorizePropertyName: true,
-      tokenization: 'lowercase'
+      tokenization: tokenization.LOWERCASE // or 'lowercase'
       // highlight-end
     },
     {
       name: 'body',
-      dataType: weaviate.configure.dataType.TEXT,
+      dataType: dataType.TEXT,
       // highlight-start
       skipVectorization: true,
-      tokenization: 'whitespace'
+      tokenization: tokenization.WHITESPACE // or 'whitespace'
       // highlight-end
-    }
+    },
   ],
 })
 // END PropModuleSettings
@@ -321,11 +346,18 @@ await client.collections.delete(collectionName)
 // ===== DISTANCE METRIC =====
 // ===========================
 
+/*
+// START DistanceMetric
+import { configure } from 'weaviate-client';
+
+// END DistanceMetric
+*/
+
 // START DistanceMetric
 newCollection = await client.collections.create({
   name: 'Article',
   // highlight-start
-  vectorIndex: weaviate.configure.vectorIndex.hnsw({
+  vectorIndex: configure.vectorIndex.hnsw({
     distanceMetric: 'cosine'
   })
   // highlight-end
@@ -344,13 +376,20 @@ await client.collections.delete(collectionName)
 // ===== CREATE A COLLECTION WITH CUSTOM INVERTED INDEX SETTINGS =====
 // ===================================================================
 
+/*
+// START SetInvertedIndexParams
+import { dataType } from 'weaviate-client';
+
+// END SetInvertedIndexParams
+*/
+
 // START SetInvertedIndexParams
 newCollection = await client.collections.create({
   name: 'Article',
   properties: [
     {
-      name: 'body',
-      dataType: weaviate.configure.dataType.TEXT,
+      name: 'title',
+      dataType: dataType.TEXT,
       // highlight-start
       indexFilterable: true,
       indexSearchable: true,
@@ -388,12 +427,19 @@ await client.collections.delete(collectionName)
 // ===== CREATE A COLLECTION WITH A GENERATIVE MODULE =====
 // ===============================================
 
+/*
+// START SetGenerative
+import { vectorizer, generative } from 'weaviate-client';
+
+// END SetGenerative
+*/
+
 // START SetGenerative
 newCollection = await client.collections.create({
   name: 'Article',
-  vectorizers: weaviate.configure.vectorizer.text2VecOpenAI(),
+  vectorizers: vectorizer.text2VecOpenAI(),
   // highlight-start
-  generative: weaviate.configure.generative.openAI(),
+  generative: generative.openAI(),
   // highlight-end
 })
 // END SetGenerative
@@ -408,12 +454,19 @@ await client.collections.delete(collectionName)
 // ===== CREATE A COLLECTION WITH A GENERATIVE MODULE AND MODEL NAME =====
 // =======================================================================
 
+/*
+// START SetGenModel
+import { vectorizer, generative } from 'weaviate-client';
+
+// END SetGenModel
+*/
+
 // START SetGenModel
 newCollection = await client.collections.create({
   name: 'Article',
-  vectorizers: weaviate.configure.vectorizer.text2VecOpenAI(),
+  vectorizers: vectorizer.text2VecOpenAI(),
   // highlight-start
-  generative: weaviate.configure.generative.openAI({
+  generative: generative.openAI({
     model: "gpt-4"
   }),
   // highlight-end
@@ -430,11 +483,18 @@ await client.collections.delete(collectionName)
 // ===== REPLICATION =====
 // =======================
 
+/*
+// START ReplicationSettings
+import { configure } from 'weaviate-client';
+
+// END ReplicationSettings
+*/
+
 // START ReplicationSettings
 newCollection = await client.collections.create({
   name: 'Article',
   // highlight-start
-  replication: weaviate.configure.replication({
+  replication: configure.replication({
     factor: 3
   }),
   // highlight-end
@@ -448,11 +508,18 @@ assert.equal(result.replicationConfig.factor, 3);
 // ===== SHARDING =====
 // ====================
 
+/*
+// START ShardingSettings
+import { configure } from 'weaviate-client';
+
+// END ShardingSettings
+*/
+
 // START ShardingSettings
 newCollection = await client.collections.create({
   name: 'Article',
   // highlight-start
-  sharding: weaviate.configure.sharding({
+  sharding: configure.sharding({
     virtualPerPhysical: 128,
     desiredCount: 1,
     desiredVirtualCount: 128,
@@ -476,9 +543,8 @@ assert.equal(result.shardingConfig.actual_virtual_count, 128);
 newCollection = await client.collections.create({
   name: 'Article',
   // highlight-start
-  multiTenancy: weaviate.configure.multiTenancy({
-    enabled: true,
-  })
+  multiTenancy: { enabled: true }
+  // multiTenancy: configure.multiTenancy({ enabled: true }) // alternatively use helper function
   // highlight-end
 })
 // END Multi-tenancy
@@ -512,13 +578,19 @@ newCollection = await client.collections.create({
 
 articles = client.collections.get('Article')
 
+/*
 // START ModifyParam
+import { reconfigure } from 'weaviate-client';
 
+// END ModifyParam
+*/
+
+// START ModifyParam
 // highlight-start
 articles.config.update({
-  invertedIndex: {
-    stopwords: { removals: ['a', 'the'] },
-  }
+  invertedIndex: reconfigure.invertedIndex({
+    stopwordsRemovals: ['a', 'the'],
+  })
 })
 // highlight-end
 // END ModifyParam
@@ -538,11 +610,20 @@ console.log(JSON.stringify(allCollections, null, 2));
 
 articles = client.collections.get('Article')
 
-// START UpdateCollection
 
+/*
+// START UpdateCollection
+import { reconfigure } from 'weaviate-client';
+
+// END UpdateCollection
+*/
+
+// START UpdateCollection
 // highlight-start
 articles.config.update({
-    invertedIndex: { bm25: { k1: 1.5 } }
+  invertedIndex: reconfigure.invertedIndex({ 
+    bm25k1: 1.5
+  })
 })
 // highlight-end
 // END UpdateCollection
