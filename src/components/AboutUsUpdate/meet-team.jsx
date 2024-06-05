1b96af32f34728bcf3b0ca9687d4281f4479a7f2
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-06-05 17:18:27 +0100

    Team update (#2181)
    * Team Update
    
    * Team Merge

--- src/components/AboutUsUpdate/meet-team.jsx
+++ src/components/AboutUsUpdate/meet-team.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
 import { LinkButton } from '/src/theme/Buttons';
 import Link from '@docusaurus/Link';
@@ -14,15 +13,22 @@ export default function MeetTheTeam() {
     setSelectedTeam(team);
   };
 
+  const filterPeople = (person) => {
+    if (selectedTeam === 'All') {
+      return true;
+    }
+    return person.team.includes(selectedTeam);
+  };
+
   return (
     <div className={styles.teamBG}>
       <div className="container" id="meet_the_team">
         <div className={styles.box}>
           <h1>Meet the Team</h1>
           <p className="text-center">
-            Weaviate a global remote-first startup, with teams hailing from many
-            different parts of the world, where it is not totally uncommon for
-            someone to work remotely from fun places.
+            Weaviate is a global remote-first startup, with teams hailing from
+            many different parts of the world, where it is not totally uncommon
+            for someone to work remotely from fun places.
           </p>
           <div className={styles.teamFilter}>
             <Link
@@ -65,19 +71,14 @@ export default function MeetTheTeam() {
             >
               #CTO team
             </Link>
-            <Link
-              className={selectedTeam === 'cloud' ? styles.linkActive : ''}
-              onClick={() => handleTeamFilter('cloud')}
-            >
-              #Cloud
-            </Link>
+
             <Link
               className={
-                selectedTeam === 'customer-success' ? styles.linkActive : ''
+                selectedTeam === 'customer-experience' ? styles.linkActive : ''
               }
-              onClick={() => handleTeamFilter('customer-success')}
+              onClick={() => handleTeamFilter('customer-experience')}
             >
-              #Customer Success
+              #Customer Experience
             </Link>
             <Link
               className={selectedTeam === 'database' ? styles.linkActive : ''}
@@ -102,6 +103,18 @@ export default function MeetTheTeam() {
               onClick={() => handleTeamFilter('developer-relations')}
             >
               #Developer Relations
+            </Link>
+            <Link
+              className={selectedTeam === 'delivery' ? styles.linkActive : ''}
+              onClick={() => handleTeamFilter('delivery')}
+            >
+              #Delivery
+            </Link>
+            <Link
+              className={selectedTeam === 'finance' ? styles.linkActive : ''}
+              onClick={() => handleTeamFilter('finance')}
+            >
+              #Finance
             </Link>
             <Link
               className={selectedTeam === 'marketing' ? styles.linkActive : ''}
@@ -131,20 +144,6 @@ export default function MeetTheTeam() {
             >
               #Sales
             </Link>
-            <Link
-              className={
-                selectedTeam === 'sales-engineering' ? styles.linkActive : ''
-              }
-              onClick={() => handleTeamFilter('sales-engineering')}
-            >
-              #Sales Engineering
-            </Link>
-            <Link
-              className={selectedTeam === 'qa-sre' ? styles.linkActive : ''}
-              onClick={() => handleTeamFilter('qa-sre')}
-            >
-              #QA/SRE Team
-            </Link>
           </div>
           <hr></hr>
         </div>
@@ -154,12 +153,9 @@ export default function MeetTheTeam() {
             styles.peopleContainer
           } ${selectedTeam.toLowerCase()}-team`}
         >
-          {people.map((person) => {
-            if (selectedTeam === 'All' || person.team === selectedTeam) {
-              return <Person key={person.name} details={person} />;
-            }
-            return null;
-          })}
+          {people.filter(filterPeople).map((person) => (
+            <Person key={person.name} details={person} />
+          ))}
         </div>
         <div className={styles.buttonsContainer}>
           <Link className={styles.buttonOutline} to="/company/careers">
