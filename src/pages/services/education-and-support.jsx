af9e8d6bae078b9c792d8cadb52a88f432d513aa
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-06-05 11:35:42 +0100

    Contact update (#2175)
    * Forms Update
    
    * Form Updates

--- src/pages/services/education-and-support.jsx
+++ src/pages/services/education-and-support.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import ServiceHeader from '../../components/Service/EducationSupport/Header/index.jsx';
 import ServicePlan from '../../components/Service/EducationSupport/Plan/index.jsx';
-import ContactUsForm from '../../components/ContactUsForm/index.jsx';
+import ContactUsForm from '../../components/Contact/contactForm.jsx';
 import Highlights from '../../components/Service/EducationSupport/Highlights/index.jsx';
 import ThemeSwitch from '/src/components/ThemeSwitch';
 import QuoteBox from '../../components/Service/EducationSupport/QuoteBox/quoteBox.jsx';
