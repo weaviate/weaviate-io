d9d00e419e590c124107e0ae02f0a656c2d7c9ed
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2024-06-10 11:21:51 -0400

    connection code

--- _includes/code/connections/connect-python-v4.py
+++ _includes/code/connections/connect-python-v4.py
@@ -12,3 +12,52 @@ client = weaviate.connect_to_local(
     )
 )
 # END TimeoutLocal
+client.close()
+
+
+# START TimeoutWCD
+import weaviate, os
+import weaviate.classes as wvc
+
+# Set these environment variables
+URL = os.getenv("WCD_URL")
+APIKEY = os.getenv("WCD_API_KEY")
+
+# Connect to a WCD instance
+client = weaviate.connect_to_WCD(
+    cluster_url=URL,
+    auth_credentials=weaviate.auth.AuthApiKey(APIKEY),
+    # skip_init_checks=True,
+    additional_config=wvc.init.AdditionalConfig(
+        timeout=wvc.init.Timeout(init=10,insert=20,query=20)
+        )
+    )
+
+# END TimeoutWCD
+client.close()
+
+
+# START TimeoutCustom
+import weaviate, os
+import weaviate.classes as wvc
+
+# Set these environment variables
+URL = os.getenv("WCD_URL")
+GPC_URL = os.getenv("WCD_GPC_URL")
+APIKEY = os.getenv("WCD_API_KEY")
+
+# Connect to a WCD instance
+client = weaviate.connect_to_custom(
+    http_host=URL,
+    http_port=8080,
+    http_secure=False,
+    grpc_host=GPC_URL,
+    grpc_port=50051,
+    grpc_secure=False,
+    additional_config=wvc.init.AdditionalConfig(
+        timeout=wvc.init.Timeout(init=10,insert=20,query=20)
+    )
+)
+
+# END TimeoutCustom
+client.close()
