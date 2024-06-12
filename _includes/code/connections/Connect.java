513b6e72a53dc738e5977941192db3f6dccbb045
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2024-06-12 13:54:51 -0400

    java cloud

--- _includes/code/connections/Connect.java
+++ _includes/code/connections/Connect.java
@@ -5,9 +5,23 @@
 // WEAVIATE_URL     your WCD instance URL
 // WEAVIATE_APIKEY  your WCD instance API key
 
+package your.application;
+
 import io.weaviate.client.Config;
+import io.weaviate.client.WeaviateClient;
 import io.weaviate.client.WeaviateAuthClient;
 
-Config config = new Config("https", WEAVIATE_URL);
-WeaviateClient client = WeaviateAuthClient.apiKey(config, WEAVIATE_APIKEY);
+public class Cloud
+{
+    public static void main( String[] args )  throws Exception
+    {
+
+    String scheme = "https";
+    String host = System.getenv("WEAVIATE_URL");
+    String apiKey = System.getenv("WEAVIATE_API_KEY");
+
+    Config config = new Config(scheme, host);
+    WeaviateClient client = WeaviateAuthClient.apiKey(config, apiKey);
+    }
+}
 // END APIKeyWCD
