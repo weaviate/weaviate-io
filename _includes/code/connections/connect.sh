abd08cc41cd447ca1fa9c5225e6d252321b8f348
daveatweaviate <144175318+daveatweaviate@users.noreply.github.com>
2024-06-13 11:48:55 -0400

    grpc timeouts

--- _includes/code/connections/connect.sh
+++ _includes/code/connections/connect.sh
@@ -2,8 +2,20 @@
 
 # START APIKeyWCD
 # Set these environment variables
-# WEAVIATE_URL     your WCD instance URL
-# WEAVIATE_API_KEY  your WCD instance API key
+# WEAVIATE_URL      your Weaviate instance URL
+# WEAVIATE_API_KEY  your Weaviate instance API key
 
-curl https://WEAVIATE_URL/v1/meta -H "Authorization: Bearer ${WEAVIATE_API_KEY}" | jq
+curl https://${WEAVIATE_URL}/v1/meta -H "Authorization: Bearer ${WEAVIATE_API_KEY}" | jq
 # END APIKeyWCD
+
+# START ThirdPartyAPIKeys
+# Set these environment variables
+# WEAVIATE_URL      your Weaviate instance URL
+# WEAVIATE_API_KEY  your Weaviate instance API key
+# COHERE_API_KEY    your Cohere API key
+
+curl https://${WEAVIATE_URL}/v1/meta \
+-H 'Content-Type: application/json' \
+-H "X-Cohere-Api-Key: ${COHERE_API_KEY}" \
+-H "Authorization: Bearer ${WEAVIATE_API_KEY}" | jq
+# END ThirdPartyAPIKeys
