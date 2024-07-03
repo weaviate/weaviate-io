85b5bf9eab00b19b04bc4d3670df28fd7e088f12
AJ Chan <achan99@gmail.com>
2024-07-03 08:06:38 -0700

    Webinars Update (#2272) (#2274)
    * Update
    
    * Blog Update
    
    Co-authored-by: Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -172,6 +172,12 @@ const config = {
                             },
                         },
 
+                         // Add CommonRoom
+            {
+                tagName: 'script',
+                innerHTML: `(function() { if (typeof window === 'undefined') return; if (typeof window.signals !== 'undefined') return; var script = document.createElement('script'); script.src = 'https://cdn.cr-relay.com/v1/site/3709e2b3-c0eb-4239-9087-775e484fab16/signals.js'; script.async = true; window.signals = Object.assign([], ['page', 'identify', 'form'].reduce(function (acc, method){ acc[method] = function () { signals.push([method, arguments]); return signals; }; return acc; }, {})); document.head.appendChild(script); })();`,
+              },
+
                     ],
                 };
             },
