24a6e6924bc8cdd98de0d2bc8378f087cadc8e65
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-07-05 10:10:14 +0100

    Map Update

--- src/components/Remote/Map/index.jsx
+++ src/components/Remote/Map/index.jsx
@@ -1,17 +1,86 @@
-import Link from '@docusaurus/Link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from '@react-google-maps/api';
 import styles from './styles.module.scss';
+import peopleData from '/data/map.json'; // adjust the path as necessary
+
+const containerStyle = {
+  width: '100%',
+  height: '711px',
+};
+
+const center = {
+  lat: 42.3601,
+  lng: -71.0589,
+};
 
 export default function Map() {
+  const [people, setPeople] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [customIcon, setCustomIcon] = useState(null);
+
+  useEffect(() => {
+    setPeople(peopleData);
+  }, []);
+
+  const handleLoad = () => {
+    setCustomIcon({
+      url: '/img/site/weaviate-logo-icon.svg', // Path to your custom icon
+      scaledSize: new window.google.maps.Size(50, 50), // Scale size (width, height)
+    });
+  };
+
   return (
     <div className={styles.mapsContainer}>
       <div className={styles.box}>
         <div className={styles.mapLogos}>
-          <div className={styles.mapDiagram}></div>
+          <LoadScript
+            googleMapsApiKey="AIzaSyCITMKf5AjdwRBY_U7LXkaljJN0olnbsck"
+            onLoad={handleLoad}
+          >
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={2}
+            >
+              {people.map((person, index) => (
+                <Marker
+                  key={index}
+                  position={person.location}
+                  icon={customIcon}
+                  onClick={() => setSelected(person)}
+                />
+              ))}
+              {selected && (
+                <InfoWindow
+                  position={selected.location}
+                  onCloseClick={() => setSelected(null)}
+                >
+                  <div>
+                    {selected.photo && (
+                      <img
+                        src={'/img/people/team/' + selected.photo}
+                        alt={selected.name}
+                        style={{
+                          borderRadius: '50%',
+                          width: '100px',
+                          height: '100px',
+                        }}
+                      />
+                    )}
+                    <h2>{selected.name}</h2>
+                  </div>
+                </InfoWindow>
+              )}
+            </GoogleMap>
+          </LoadScript>
         </div>
         <div className={styles.mapBox}>
           <h2>Weaviate around the world</h2>
-
           <p>
             As a remote company with distributed teams, we embrace the
             flexibility that comes from allowing our employees to live wherever
