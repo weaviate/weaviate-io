84ef74157c9d0da7cff56893953e7c1aaeb1f02c
JP Hwang <me@jphwang.com>
2024-07-29 18:49:23 +0100

    Add new script to tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,6 +8,7 @@ import utils
     [
         "./_includes/code/client-libraries/python_v4.py",
         "./_includes/code/client-libraries/python_slow_connection.py",
+        "./_includes/code/connections/connect-python-v4.py",
     ],
 )
 def test_on_blank_instance_pyv4(empty_weaviates, script_loc):
