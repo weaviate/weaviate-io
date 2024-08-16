10b2a120a4be07abea278e78fcd11199647c536b
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-08-16 14:25:03 +0100

    Sign Up Update (#2470)
    * Cloud Draft
    
    * Signup Draft
    
    * Draft
    
    * Draft Update
    
    * Filter
    
    * Draft
    
    * Update
    
    * Disable

---
+++ src/pages/ai-database-signup-free.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import { MetaSEO } from '/src/theme/MetaSEO';
+import Head from '@docusaurus/Head';
+
+import Introduction from '/src/components/Signup/Alternative/introduction';
+import ThemeSwitch from '/src/components/ThemeSwitch';
+
+export default function signUpPage() {
+  return (
+    <>
+      <Head>
+        <title>Get Started For Free</title>
+        <meta
+          name="description"
+          content="All the power of our open-source vector database, without the burden of self-hosting."
+        />
+        <meta property="og:title" content="Get Started For Free" />
+        <meta
+          property="og:description"
+          content="All the power of our open-source vector database, without the burden of self-hosting."
+        />
+        <meta property="robots" content="noindex,nofollow" />
+      </Head>
+      <div className="custom-page noBG">
+        <Introduction />
+        <ThemeSwitch />
+      </div>
+    </>
+  );
+}
