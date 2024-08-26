93706fa84ec56a99179669ad55d5112d4a60fd6e
Jeremy Price <donomii@gmail.com>
2024-08-26 23:43:32 +0900

    Add test shell scripts

---
+++ _includes/code/howto/go/docs/mainpkg/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export WCD_DEMO_URL=hha2nvjsruetknc5vxwrwa.c0.europe-west2.gcp.weaviate.cloud
+export WCD_DEMO_RO_KEY=nMZuw1z1zVtnjkXXOMGx9Ows7YWGsakItdus
+export OPENAI_APIKEY=learn-weaviate
+go test .
