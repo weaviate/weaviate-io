3d1fac7fc496182d8409cbb5c1bff5b6d1e6bcee
daveatweaviate <dave.cuthbert@weaviate.io>
2024-09-13 22:25:46 -0400

    py3

--- _includes/code/howto/indexes/indexes-v3.py
+++ _includes/code/howto/indexes/indexes-v3.py
@@ -177,3 +177,53 @@ if (schema_response["class"] == class_name) and (
 ):
     correct_index = True
 assert correct_index, "Wrong index type"
+
+########################
+### Inverted Indexes ###
+########################
+
+# Delete data from prior runs
+if client.schema.exists(class_name):
+    client.schema.delete_class(class_name)
+
+# START PropIndex
+class_obj = {
+    "class": class_name,
+    "vectorizer": "text2vec-huggingface",
+    "properties": [
+        {
+            "name": "title",
+            "dataType": ["text"],
+            "indexFilterable": True,
+            "indexSearchable": True,
+            "moduleConfig": {
+                "text2vec-huggingface": {}
+            }
+        },
+        {
+            "name": "chunk",
+            "dataType": ["int"],
+            "indexRangeFilters": True,
+        },
+    ],
+    "invertedIndexConfig": {
+        "bm25": {
+            "b": 0.7,
+            "k1": 1.25
+        },
+        "indexTimestamps": True,
+        "indexNullState": True,
+        "indexPropertyLength": True
+    }
+}
+
+client.schema.create_class(class_obj)
+# END PropIndex
+
+class_response = client.schema.get()
+schema_response = client.schema.get(class_name)
+
+classes = []
+for c in class_response["classes"]:
+    classes.append(c["class"])
+assert class_name in classes, "Class missing"
