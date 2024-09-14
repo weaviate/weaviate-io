5357a788491e4d620b6c588e30783292c56d70ff
daveatweaviate <dave.cuthbert@weaviate.io>
2024-09-13 22:34:52 -0400

    ts3

--- _includes/code/howto/indexes/indexes-v3.ts
+++ _includes/code/howto/indexes/indexes-v3.ts
@@ -2,7 +2,7 @@
 // TODO: Needs tests
 
 // Imports
-import weaviate, { WeaviateClient,vectorizer, configure } from 'weaviate-client';
+import weaviate, { dataType, WeaviateClient,vectorizer, configure } from 'weaviate-client';
 
 // Delete pre-existing collections
 function deleteCollection(client: WeaviateClient, collectionName: string){
@@ -111,6 +111,44 @@ async function createDynamicCollection(client: WeaviateClient, collectionName: s
 }
 // END EnableDynamic
 
+
+//////////////////////
+// Inverted Indexes //
+//////////////////////
+
+// START PropIndex
+// Add this import line
+// import { dataType } from 'weaviate-client';
+
+async function createInvertedCollection(client: WeaviateClient, collectionName: string){
+  await client.collections.create({
+    name: collectionName,
+    properties: [
+      {
+        name: 'TextProperty',
+        dataType: dataType.TEXT,
+        indexFilterable: true,
+        indexSearchable: true,
+      },
+      {
+        name: 'NumericProperty',
+        dataType: dataType.INT,
+        indexRangeFilters: true,
+      },
+    ],
+    invertedIndex: {
+      bm25: {
+        b: 0.7,
+        k1: 1.25
+      },
+      indexNullState: true,
+      indexPropertyLength: true,
+      indexTimestamps: true
+    }
+  })
+}
+// END PropIndex
+
 /////////////////////////////
 /// AVOID TOP LEVEL AWAIT ///
 /////////////////////////////
@@ -145,9 +183,15 @@ async function main(){
   // }
 
   // // Run dynamic collection code
+  // deleteCollection(client, collectionName)
+  // if(await client.collections.get(collectionName).exists() != true){
+  //   createDynamicCollection(client, collectionName);
+  // }
+
+  // Run inverted collection code
   deleteCollection(client, collectionName)
   if(await client.collections.get(collectionName).exists() != true){
-    createDynamicCollection(client, collectionName);
+    createInvertedCollection(client, collectionName);
   }
 }
 
