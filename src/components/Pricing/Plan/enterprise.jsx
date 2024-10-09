ca9a2e3200ece1b8667d7df5d099347cdea890ef
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-10-09 17:25:55 +0100

    Styling

--- src/components/Pricing/Plan/enterprise.jsx
+++ src/components/Pricing/Plan/enterprise.jsx
@@ -87,7 +87,7 @@ export default function PricingEnterprise() {
         style={{ display: isModalOpen ? 'flex' : 'none' }}
       >
         <div className={styles.modalContents}>
-          <span className={styles.closeEnterprise} onClick={closeModal}>
+          <span className={styles.close} onClick={closeModal}>
             &times;
           </span>
 
