3282cf25122cd4a045dc9af7230db8d5a60f9de8
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-10-23 13:42:20 +0100

    OPO SEO Updates (#2715)
    * SEO Updates
    
    * Styling

--- src/pages/deployment/enablement.jsx
+++ src/pages/deployment/enablement.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import ServiceHeader from '../../components/Service/EducationSupport/Header/index.jsx';
-import ServicePlan from '../../components/Service/EducationSupport/Plan/index.jsx';
-import ContactUsForm from '../../components/Contact/contactForm.jsx';
-import Highlights from '../../components/Service/EducationSupport/Highlights/index.jsx';
+import ServiceHeader from '/src/components/Service/EducationSupport/Header/index.jsx';
+import ServicePlan from '/src/components/Service/EducationSupport/Plan/index.jsx';
+import ContactUsForm from '/src/components/Contact/contactForm.jsx';
+import Highlights from '/src/components/Service/EducationSupport/Highlights/index.jsx';
 import ThemeSwitch from '/src/components/ThemeSwitch';
-import QuoteBox from '../../components/Service/EducationSupport/QuoteBox/quoteBox.jsx';
-import Integrations from '../../components/Service/EducationSupport/Integrations/index.jsx';
+import QuoteBox from '/src/components/Service/EducationSupport/QuoteBox/quoteBox.jsx';
+import Integrations from '/src/components/Service/EducationSupport/Integrations/index.jsx';
 
 export default function EducationandSupportPage() {
   return (
     <div className="custom-page noBG">
-      <Layout title="Education and Support" description="Education and Support">
+      <Layout
+        title="Vector Database Training & Enablement"
+        description="Get vector database tutorials, training courses, resources, and support for builders of all levels. We’re with you on your AI journey"
+      >
         <ServiceHeader />
         <ServicePlan />
         <QuoteBox />
--- src/pages/platform.jsx
+++ src/pages/platform.jsx
@@ -4,15 +4,15 @@ import { MetaSEO } from '/src/theme/MetaSEO';
 import PlatformHeader from '/src/components/Platform/Header/index.jsx';
 import SOC2 from '/src/components/Platform/SOC2/soc2';
 import SafetyTypes from '/src/components/Platform/SafetyTypes';
-import Integrations from '../components/Platform/Integrations';
+import Integrations from '/src/components/Platform/Integrations';
 import ThemeSwitch from '/src/components/ThemeSwitch';
 
 export default function PlatformPage() {
   return (
     <div className="custom-page noBG">
       <Layout
-        title="Vector Database"
-        description="The AI-Native Vector Database"
+        title="Open Source Vector Database"
+        description="Simplify the development of AI applications and enable developers of all levels to build, iterate, and scale AI capabilities faster."
       >
         <MetaSEO />
         <PlatformHeader />
