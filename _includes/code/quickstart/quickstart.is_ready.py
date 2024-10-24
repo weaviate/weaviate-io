2baed8580dac92f805c0d2bf9f631892be0712d3
JP Hwang <me@jphwang.com>
2024-10-24 11:08:00 +0100

    Update TS code

--- _includes/code/quickstart/quickstart.is_ready.py
+++ _includes/code/quickstart/quickstart.is_ready.py
@@ -4,20 +4,18 @@ from weaviate.classes.init import Auth
 import os
 
 # Best practice: store your credentials in environment variables
-wcd_url = os.environ["WCD_DEMO_URL"]
-wcd_api_key = os.environ["WCD_DEMO_RO_KEY"]
-openai_api_key = os.environ["OPENAI_APIKEY"]
+wcd_url = os.environ["WCD_URL"]
+wcd_api_key = os.environ["WCD_API_KEY"]
 
 # highlight-start
 client = weaviate.connect_to_weaviate_cloud(
     cluster_url=wcd_url,                                    # Replace with your Weaviate Cloud URL
     auth_credentials=Auth.api_key(wcd_api_key),             # Replace with your Weaviate Cloud key
-    headers={"X-OpenAI-Api-Key": openai_api_key}            # OpenAI API key for Weaviate to use when connecting to the OpenAI API
 )
 # highlight-end
 
 try:
-    assert client.is_ready()  # Should throw an exception if not ready
+    print(client.is_ready())  # Should print True
 
 finally:
     client.close()  # Close client gracefully
---
+++ _includes/code/quickstart/quickstart.is_ready.ts
@@ -0,0 +1,13 @@
+// InstantiationExample
+import weaviate, { WeaviateClient } from 'weaviate-client';
+
+const client: WeaviateClient = await weaviate.connectToWeaviateCloud(
+  process.env.WCD_URL,
+  {
+    authCredentials: new weaviate.ApiKey(process.env.WCD_API_KEY),
+  }
+)
+
+var clientReadiness = await client.isReady();
+console.log(clientReadiness);
+// END InstantiationExample
