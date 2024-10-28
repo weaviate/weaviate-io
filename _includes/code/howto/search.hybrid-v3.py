365c14ba5385262b66236f7435f2fcc2b2c0e685
JP Hwang <61258750+databyjp@users.noreply.github.com>
2024-10-28 12:16:02 +0000

    Add aggregate+hybrid examples (#2728)

--- _includes/code/howto/search.hybrid-v3.py
+++ _includes/code/howto/search.hybrid-v3.py
@@ -10,10 +10,10 @@ import os
 
 # Instantiate the client with the user/password and OpenAI api key
 client = weaviate.Client(
-    "https://edu-demo.weaviate.network",
-    auth_client_secret=weaviate.auth.AuthApiKey("learn-weaviate"),
+    os.getenv("WCD_DEMO_URL"),  # Replace with your Weaviate URL
+    auth_client_secret=weaviate.auth.AuthApiKey(os.getenv("WCD_DEMO_RO_KEY")),  # If authentication is on. Replace with your Weaviate instance API key
     additional_headers={
-        "X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"]
+        "X-OpenAI-Api-Key": os.getenv("OPENAI_APIKEY")  # Replace with your OPENAI API key
     }
 )
 
