b7252b2711d081beecfe72562692c62df8a0f434
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-11-01 16:17:01 +0000

    Demo v2

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -4,19 +4,19 @@ import Link from '@docusaurus/Link';
 import { MetaSEO } from '/src/theme/MetaSEO';
 import Head from '@docusaurus/Head';
 
-import Introduction from '/src/components/Signup/Demo/introduction';
+import Introduction from '/src/components/Signup/Demo/v2';
 import ThemeSwitch from '/src/components/ThemeSwitch';
 
 export default function signUpPage() {
   return (
     <>
       <Head>
-        <title>Get Started For Free</title>
+        <title>Request a Demo</title>
         <meta
           name="description"
           content="All the power of our open-source vector database, without the burden of self-hosting."
         />
-        <meta property="og:title" content="Get Started For Free" />
+        <meta property="og:title" content="Request a Demo" />
         <meta
           property="og:description"
           content="All the power of our open-source vector database, without the burden of self-hosting."
