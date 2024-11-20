f168f6e7f833bab7d2254b758d34db16a2d531f9
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-11-20 12:15:06 +0000

    Update (#2795)

--- src/components/Community/Demos/Highlights/index.jsx
+++ src/components/Community/Demos/Highlights/index.jsx
@@ -17,7 +17,7 @@ export default function Highlights() {
           <div className={styles.card}>
             <div className={styles.cardHeader}>
               <span className={styles.stars}>
-                4900
+                6300
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="19"
@@ -106,7 +106,7 @@ export default function Highlights() {
           <div className={`${styles.card} ${styles.bigCard}`}>
             <div className={styles.cardHeader}>
               <span className={styles.stars}>
-                153
+                163
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="19"
@@ -192,7 +192,7 @@ export default function Highlights() {
           <div className={`${styles.card} ${styles.bigCard}`}>
             <div className={styles.cardHeader}>
               <span className={styles.stars}>
-                69
+                79
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="19"
@@ -269,6 +269,92 @@ export default function Highlights() {
               <source
                 src="/img/site/awesome-moviate-video.webm"
                 poster="img/site/screenshot-healthsearch.png"
+                type="video/webm"
+              />
+            </video>
+          </div>
+        </div>
+
+        <div className={styles.box}>
+          <div className={`${styles.card} ${styles.bigCard}`}>
+            <div className={styles.cardHeader}>
+              <span className={styles.stars}>
+                12
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="19"
+                  height="18"
+                  viewBox="0 0 19 18"
+                  fill="yellow"
+                >
+                  <path
+                    d="M8.82065 0.559817C9.03938 -0.113347 9.99172 -0.113345 10.2104 0.559819L11.8122 5.48936C11.91 5.79041 12.1905 5.99424 12.5071 5.99424H17.6903C18.3981 5.99424 18.6924 6.89997 18.1198 7.31601L13.9264 10.3626C13.6703 10.5487 13.5632 10.8785 13.661 11.1795L15.2627 16.1091C15.4814 16.7822 14.711 17.342 14.1383 16.926L9.94502 13.8794C9.68893 13.6933 9.34217 13.6933 9.08608 13.8794L4.89276 16.926C4.32013 17.342 3.54966 16.7822 3.76839 16.1091L5.37009 11.1795C5.46791 10.8785 5.36075 10.5487 5.10467 10.3626L0.911344 7.31601C0.338717 6.89997 0.63301 5.99424 1.34082 5.99424H6.52405C6.84059 5.99424 7.12113 5.79041 7.21895 5.48936L8.82065 0.559817Z"
+                    fill="#FFD644"
+                  />
+                </svg>
+              </span>
+              <h3>Wealingo</h3>
+              <span>A personalized language-learning app</span>
+              <Link
+                to="http://wealingo.weaviate.io/"
+                className={`${styles.top} ${styles.cardButton} `}
+              >
+                <span className={styles.play}></span> Start Demo
+              </Link>
+            </div>
+            <div className={styles.contentDiv}>
+              <div className={`${styles.textCardContent} ${styles.bigText}`}>
+                <p>
+                  A language-learning app built using Weaviate’s vector database
+                  and Confluent Cloud, a fully managed streaming data service
+                  based on Apache Kafka. See how <strong>real-time data</strong>{' '}
+                  and <strong>vector embeddings</strong> enable personalized
+                  learning by continuously adapting to user performance.
+                </p>
+                <div className={styles.bottomContainer}>
+                  <Link
+                    to="https://weaviate.io/blog/wealingo-demo"
+                    className={styles.cardButton}
+                  >
+                    {' '}
+                    <span className={styles.blog}></span> Blogpost
+                  </Link>
+
+                  <Link
+                    to="https://github.com/weaviate/wealingo-demo"
+                    className={`${styles.gitButton} ${styles.cardButton} `}
+                  >
+                    {' '}
+                    <span className={styles.git}></span> GitHub
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.imageCard} ${styles.movie}`}>
+            <video
+              className={styles.healthVideo}
+              muted="muted"
+              autoplay="autoplay"
+              playsinline="playsinline"
+              loop="loop"
+              controls
+            >
+              <source
+                src="/img/site/Wealingo_Video.mp4"
+                poster="img/site/screenshot-wealingo.png"
+                type="video/mp4"
+              />
+
+              <source
+                src="/img/site/Wealingo_Video.mov"
+                poster="img/site/screenshot-wealingo.png"
+                type="video/mov"
+              />
+
+              <source
+                src="/img/site/Wealingo_Video.webm"
+                poster="img/site/screenshot-wealingo.png"
                 type="video/webm"
               />
             </video>
