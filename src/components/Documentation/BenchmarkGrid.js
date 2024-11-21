7bd7b404ef9a2d62cae2fbc9f8f2de6c9cfd29bd
Sebastian Witalec <sebawita@users.noreply.github.com>
2024-11-21 12:01:17 +0100

    tidy up sorting and display headers

--- src/components/Documentation/BenchmarkGrid.js
+++ src/components/Documentation/BenchmarkGrid.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
-
-import { useState } from 'react';
 
 import { benchmarkData } from './benchmarkData';
 
@@ -25,27 +23,75 @@ export default function BenchmarkGrid({ datasetLabel }) {
     const [rowData, setRowData] = useState(data);
 
     const [colDefs, setColDefs] = useState([
-        { field: "efConstruction", headerName: "efConstruction", width: 150 },
-        { field: "maxConnections", headerName: "maxConnections", width: 150 },
-        { field: "ef", headerName: "ef", width: 75 },
-        { field: "recall", headerName: "Recall", width: 100, valueFormatter: percenageFormatter },
-        { field: "qps", headerName: "QPS", sort: "desc", width: 100 },
-        { field: "meanLatency", headerName: "Mean Latency", width: 125, valueFormatter: msecFormatter },
-        { field: "p99Latency", headerName: "p99 Latency", width: 125, valueFormatter: msecFormatter },
-        { field: "importTime", headerName: "Import Time", width: 125, valueFormatter: secFormatter  },
+        { 
+            field: "efConstruction",
+            headerName: "efConstruction",
+            width: 155,
+            wrapHeaderText: true,
+        },
+        { 
+            field: "maxConnections",
+            headerName: "maxConnections",
+            width: 150,
+            wrapHeaderText: true,
+        },
+        { 
+            field: "ef",
+            headerName: "ef",
+            width: 75,
+        },
+        { 
+            field: "recall",
+            headerName: "Recall",
+            width: 100,
+            sortingOrder: ["desc", "asc", null],
+            valueFormatter: percenageFormatter,
+        },
+        { 
+            field: "qps",
+            headerName: "QPS",
+            width: 100,
+            sort: "desc",
+            sortingOrder: ["desc", "asc", null],
+        },
+        { 
+            field: "meanLatency",
+            headerName: "Mean Latency",
+            width: 125, 
+            wrapHeaderText: true,
+            valueFormatter: msecFormatter,
+        },
+        { 
+            field: "p99Latency",
+            headerName: "p99 Latency",
+            width: 125,
+            wrapHeaderText: true,
+            valueFormatter: msecFormatter,
+        },
+        { 
+            field: "importTime",
+            headerName: "Import Time",
+            width: 125,
+            wrapHeaderText: true,
+            valueFormatter: secFormatter,
+        },
     ]);
+
+    const sortingOrder = useMemo(() => {
+        return ["asc", "desc", null];
+    }, []);
 
     return (
         // wrapping container with theme & size
         <div
-         className="ag-theme-quartz" // applying the Data Grid theme
-         style={{ height: 500 }} // the Data Grid will fill the size of the parent container
+            className="ag-theme-quartz" // applying the Data Grid theme
+            style={{ height: 500 }} // the Data Grid will fill the size of the parent container
         >
-          <AgGridReact
-              rowData={rowData}
-              columnDefs={colDefs}
-              sortingOrder={["desc", "asc", null]}
-          />
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={colDefs}
+                sortingOrder={sortingOrder}
+            />
         </div>
     )
 }
