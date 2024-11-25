214bd1b5c84423c6dfe689d09ee17ffe078ecd1e
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-11-25 09:47:13 +0000

    Update

--- src/pages/partners/aws/awsreinvent.jsx
+++ src/pages/partners/aws/awsreinvent.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import Header from '/src/components/Partners/awsreinvent/Header';
-import UnlockSection from '/src/components/Partners/awsreinvent/UnlockSection';
-import CallingSection from '/src/components/Partners/awsreinvent/CallingSection';
-import Resources from '/src/components/Partners/awsreinvent/Resources/resources';
-import Footer from '/src/components/Partners/awsreinvent/awsFooter';
-import ContactForm from '../../../components/Partners/awsreinvent/ContactForm/contactForm';
-import ThemeSwitch from '/src/components/ThemeSwitch';
+import React, { useEffect } from 'react';
+
+const redirectToExternal = true;
 
 export default function awsReinventPage() {
+  useEffect(() => {
+    if (redirectToExternal) {
+      window.location.href = 'https://events.weaviate.io/aws-reinvent-2024';
+    }
+  }, []);
+
+  if (redirectToExternal) {
+    return (
+      <div>
+        <p>
+          Redirecting to{' '}
+          <a href="https://events.weaviate.io/aws-reinvent-2024">
+            AWS Re:Invent 2024
+          </a>
+          ...
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="custom-page noBG">
       <Layout
