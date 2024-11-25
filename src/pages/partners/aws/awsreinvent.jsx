8ca1b99e71dc83dcbd3d3f10cfb6a4f69127f4c1
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-11-25 09:58:31 +0000

    Imports

--- src/pages/partners/aws/awsreinvent.jsx
+++ src/pages/partners/aws/awsreinvent.jsx
@@ -1,4 +1,13 @@
 import React, { useEffect } from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import Header from '/src/components/Partners/awsreinvent/Header';
+import UnlockSection from '/src/components/Partners/awsreinvent/UnlockSection';
+import CallingSection from '/src/components/Partners/awsreinvent/CallingSection';
+import Resources from '/src/components/Partners/awsreinvent/Resources/resources';
+import Footer from '/src/components/Partners/awsreinvent/awsFooter';
+import ContactForm from '../../../components/Partners/awsreinvent/ContactForm/contactForm';
+import ThemeSwitch from '/src/components/ThemeSwitch';
 
 const redirectToExternal = true;
 
