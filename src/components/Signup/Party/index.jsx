8c4fa84ff94c0e0152337aea48a658bb8a806db2
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-11-29 16:28:05 +0000

    test

--- src/components/Signup/Party/index.jsx
+++ src/components/Signup/Party/index.jsx
@@ -13,7 +13,7 @@ export default function Introduction() {
       if (window.hbspt) {
         window.hbspt.forms.create({
           portalId: '8738733',
-          formId: '62e704b7-b857-421e-a30d-46ac0a3f5d81',
+          formId: 'b1edc7c4-e5ff-4077-832b-26c0ad4f5f7a',
           target: '#hs-form',
         });
       }
@@ -22,48 +22,25 @@ export default function Introduction() {
   }, []);
 
   return (
-    <>
-      <div className={styles.demoContainer}>
-        <div className={styles.contentContainer}>
-          <div className={styles.demoTitle}>
-            <div className={styles.demoLogo}></div>
-            <span></span>
-            <h1 classname={styles.headerTag}>Weaviate AI Secret Party</h1>
+    <div className={styles.demoContainer}>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <div className={styles.contentSideA}>
+            <div className={styles.contentImage} />
           </div>
-          <div className={styles.content}>
-            <div className={styles.contentSideA}>
-              <div className={styles.contentImage} />
-            </div>
-            <div className={styles.contentSideB}>
-              <div className={styles.signUp}>
-                <div className={styles.signUpBox}>
-                  <h3>Request a ticket</h3>
-                  <div className={styles.formWrapper}>
-                    <div id="hs-form" className={styles.hsForm}></div>
-                  </div>
+          <div className={styles.contentSideB}>
+            <div className={styles.signUp}>
+              <div className={styles.signUpBox}>
+                <h1>Weaviate</h1>
+                <h3>AI Builder Secret Party</h3>
+                <div className={styles.formWrapper}>
+                  <div id="hs-form" className={styles.hsForm}></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className={styles.footer}>
-        <span>Weaviate, 2024</span>
-        <div className={styles.footerLinks}>
-          <div className={styles.footerIcon}>
-            <i className="fab fa-linkedin"></i>
-          </div>
-          <div className={styles.footerIcon}>
-            <i className="fab fa-instagram"></i>
-          </div>
-          <div className={styles.footerIcon}>
-            <i className="fab fa-twitter"></i>
-          </div>
-          <div className={styles.footerIcon}>
-            <i className="fab fa-youtube"></i>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
