f65ca284ebf53926f9138ed4f4358d015ff6a95c
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-12-05 21:17:35 +0000

    Link update (#2843)
    * Update
    
    * Import
    
    * Page Links
    
    * Update

---
+++ src/pages/contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import { MetaSEO } from '/src/theme/MetaSEO';
+
+import Header from '/src/components/Contact/Header';
+import ContactFrom from '/src/components/Contact/contactForm.jsx';
+import ThemeSwitch from '/src/components/ThemeSwitch';
+import CTA from '/src/components/Home/Redesign/CTA';
+
+export default function ContactPage() {
+  return (
+    <div className="custom-page noBG">
+      <Layout
+        title="Contact"
+        description="We are here to help you with any questions you might have."
+      >
+        <MetaSEO img="og/content/contact.jpg" />
+        <Header />
+        <ContactFrom />
+        <CTA />
+      </Layout>
+      <ThemeSwitch />
+    </div>
+  );
+}
