662036c247589a3a5fbe9a4a8dbfe9a8ccc21f6d
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2024-12-31 13:34:40 +0000

    Script (#2901)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -221,7 +221,17 @@ const config = {
             reb2b.load("5NRP9HG1YMO1");
           })();`,
       },
-
+       // Microsoft Clarity Script
+       {
+        tagName: 'script',
+        innerHTML: `
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "orzk1oie18");
+        `,
+      },
                     ],
                 };
             },
