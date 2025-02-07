440ef884745cd06b4fdf2c2490dfcd205eeeb719
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-02-07 15:18:59 +0000

    LP entry update (#2975)
    * Update
    
    * Update

--- src/components/Signup/Ebooks/v2.jsx
+++ src/components/Signup/Ebooks/v2.jsx
@@ -22,36 +22,61 @@ export default function Introduction() {
   }, []);
 
   return (
-    <div className={styles.demoContainer}>
-      <div className={styles.demoSideA}>
-        <div className={styles.demoLogo}></div>
-        <span>EBOOK</span>
-        <h1 classname={styles.headerTag}>
-          Choosing the Right Database For AI 
-        </h1>
-
-        <ul>
-          This guide covers:
-          <li>A comparison of AI-native and traditional databases.</li>
-          <li>
-            Key considerations for powering scalable, cost-effective AI
-            applications.{' '}
-          </li>
-          <li>
-            Practical tips for indexing, hybrid search, and seamless integration
-            with AI models.
-          </li>
-        </ul>
-      </div>
-      <div className={styles.demoSideB}>
-        <div className={styles.signUp}>
-          <div className={styles.signUpBox}>
-            <div className={styles.formWrapper}>
-              <div id="hs-form" className={styles.ebookForm}></div>
+    <>
+      <div className={styles.demoContainer}>
+        <div className={styles.contentContainer}>
+          <div className={styles.demoTitle}>
+            <div className={styles.demoLogo}></div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.contentSideA}>
+              <div className={styles.reportImage}></div>
+            </div>
+            <div className={styles.contentSideB}>
+              <span>EBOOK</span>
+              <h1 classname={styles.headerTag}>
+                Choosing the Right Database For AI
+              </h1>
+              <span className={styles.subTitle}>
+                A guide to help you choose the right database for your AI
+                applications.
+              </span>
+              <div className={styles.signUp}>
+                <div className={styles.signUpBox}>
+                  <div className={styles.formWrapper}>
+                    <div id="hs-form" className={styles.ebookForm}></div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className={styles.contentBottom}>
+        <div>
+          <h3>About the guide</h3>
+          <p>
+            Deciding which vector storage technology to use requires
+            understanding more about how LLMs and similar algorithms work as
+            well as what the different capabilities are of vector storage
+            technologies. This guide hopes to walk you through this decision as
+            well as inform you enough to choose the best technology for your
+            application.
+          </p>
+          <br></br>
+          <ul>
+            <li>A comparison of AI-native and traditional databases.</li>
+            <li>
+              Key considerations for powering scalable, cost-effective AI
+              applications.{' '}
+            </li>
+            <li>
+              Practical tips for indexing, hybrid search, and seamless
+              integration with AI models.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
