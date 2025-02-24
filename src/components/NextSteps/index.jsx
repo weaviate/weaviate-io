2eb4b0e0fd09b81886069a913ab5998b4327ef23
g-despot <66276597+g-despot@users.noreply.github.com>
2025-02-24 13:18:13 +0100

    Update docs

--- src/components/NextSteps/index.jsx
+++ src/components/NextSteps/index.jsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 // TODO[g-despot]: Work in progress. Reuse this component where applicable (like quickstarts)
 const NextSteps = ({ cards }) => {
   return (
-    <div className="container margin-top--xs padding-top--xs">
+    <div className="container margin-top--xs padding-top--xs margin-bottom--xs padding-bottom--xs">
       <div className="row">
         {cards.map((card, index) => (
           <div key={index} className="col col--6 margin-bottom--md">
