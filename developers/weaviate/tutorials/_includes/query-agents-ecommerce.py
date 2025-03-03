f5da34b50a951560267055e312cef7f42efab455
g-despot <66276597+g-despot@users.noreply.github.com>
2025-03-03 09:09:16 +0100

    Implement feedback

--- developers/weaviate/tutorials/_includes/query-agents-ecommerce.py
+++ developers/weaviate/tutorials/_includes/query-agents-ecommerce.py
@@ -24,10 +24,13 @@ client = weaviate.connect_to_weaviate_cloud(
     auth_credentials=Auth.api_key(weaviate_api_key),
 )
 
+client.collections.delete_all()
+client.collections.delete("Brands")
+client.collections.delete("ECommerce")
+
 # START DefineCollections
 from weaviate.classes.config import Configure, Property, DataType
 
-client.collections.delete_all()
 client.collections.create(
     "Brands",
     description="A dataset that lists information about clothing brands, their parent companies, average rating and more.",
@@ -35,7 +38,7 @@ client.collections.create(
 )
 
 client.collections.create(
-    "ecommerce",
+    "ECommerce",
     description="A dataset that lists clothing items, their brands, prices, and more.",
     vectorizer_config=Configure.Vectorizer.text2vec_weaviate(),
     properties=[
@@ -90,22 +93,22 @@ ecommerce_dataset = load_dataset(
 )
 
 brands_collection = client.collections.get("Brands")
-ecommerce_collection = client.collections.get("ecommerce")
+ecommerce_collection = client.collections.get("ECommerce")
 
 with brands_collection.batch.dynamic() as batch:
     for item in brands_dataset:
-        batch.add_object(properties=item["properties"])
+        batch.add_object(properties=item["properties"], vector=item["vector"])
 
 with ecommerce_collection.batch.dynamic() as batch:
     for item in ecommerce_dataset:
-        batch.add_object(properties=item["properties"])
+        batch.add_object(properties=item["properties"], vector=item["vector"])
 
 failed_objects = brands_collection.batch.failed_objects
 if failed_objects:
     print(f"Number of failed imports: {len(failed_objects)}")
     print(f"First failed object: {failed_objects[0]}")
 
-print(f"Size of the eCommerce dataset: {len(ecommerce_collection)}")
+print(f"Size of the ECommerce dataset: {len(ecommerce_collection)}")
 print(f"Size of the Brands dataset: {len(brands_collection)}")
 # END PopulateDatabase
 
@@ -114,14 +117,14 @@ from weaviate.agents.query import QueryAgent
 
 agent = QueryAgent(
     client=client,
-    collections=["ecommerce", "Brands"],
+    collections=["ECommerce", "Brands"],
 )
 # END BasicQueryAgent
 
 # START CustomizedQueryAgent
 multi_lingual_agent = QueryAgent(
     client=client,
-    collections=["ecommerce", "Brands"],
+    collections=["ECommerce", "Brands"],
     system_prompt="You are a helpful assistant that always generated the final response in the users language."
     " You may have to translate the user query to perform searches. But you must always respond to the user in their own language.",
 )
