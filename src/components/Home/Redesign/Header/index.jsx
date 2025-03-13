d4b13fb9982309135535857232ac5b7cf40212df
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-03-13 15:45:06 +0000

    Update

--- src/components/Home/Redesign/Header/index.jsx
+++ src/components/Home/Redesign/Header/index.jsx
@@ -183,12 +183,11 @@ export default function HomepageHeader() {
                 <div
                   className={`${styles.customerLogo} ${styles.redhatLogo}`}
                 />
+
                 <div
-                  className={`${styles.customerLogo} ${styles.mulinyLogo}`}
+                  className={`${styles.customerLogo} ${styles.superhumanLogo}`}
                 />
-                <div
-                  className={`${styles.customerLogo} ${styles.shippoLogo}`}
-                />
+                <div className={`${styles.customerLogo} ${styles.ltkLogo}`} />
               </div>
             </div>
 
