07f4b41fd72fb125198061af32fbe5bdf9f1ef03
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-03-19 12:52:36 +0000

    Draft

--- src/components/Partners/Header/index.jsx
+++ src/components/Partners/Header/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { ButtonContainer } from '../../../theme/Buttons';
 import styles from './styles.module.scss';
-import { LinkButton } from '/src/theme/Buttons';
 import Link from '@docusaurus/Link';
 
 export default function PartnersHeader() {
+  const handleTabClick = (tab) => {
+    window.history.pushState(null, '', `#request-form?tab=${tab}`);
+
+    document.getElementById('request-form').scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    window.dispatchEvent(new Event('popstate'));
+  };
+
   return (
     <header>
       <div className={styles.partnersHead}>
         <div className="container">
           <div className={styles.grid}>
             <p className={styles.title}>
-              Weaviate <span>Partners</span>
+              Company <span>Partners</span>
             </p>
             <p className={styles.text}>
               Grow with Weaviate! We bring together cutting-edge AI technologies
@@ -19,8 +28,17 @@ export default function PartnersHeader() {
               organizations worldwide.
             </p>
             <div className={styles.buttons}>
-              <Link className={styles.buttonOutline} to="#request-form">
+              <Link
+                className={styles.buttonOutline}
+                onClick={() => handleTabClick('partner')}
+              >
                 Become a Partner
+              </Link>
+              <Link
+                className={styles.buttonOutline}
+                onClick={() => handleTabClick('deal')}
+              >
+                Register a Deal
               </Link>
             </div>
           </div>
