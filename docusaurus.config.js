306d3f14be7c3cde89861ca1a95f40c10f1bf094
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-03-21 09:57:27 +0000

    Removal

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -480,13 +480,7 @@ const config = {
                         to: 'https://github.com/weaviate/weaviate',
                         position: 'right',
                     },
-                    {
-                    
-                        html: ` <i class="fa-regular fa-user iconHide" style="color:#2c3854;"></i> Login</span>`,
-                        to: 'https://auth.wcs.api.weaviate.io/auth/realms/SeMI/protocol/openid-connect/auth?client_id=wcs-frontend&scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fconsole.weaviate.cloud%2Fapi%2Fauth%2Fcallback%2Fkeycloak&state=P3BeeUEpGZNBXSXHycDajZNjrHT_HliA14qrtGovlk8&code_challenge=VZLcWPLzlwCsrvkYzjY3yC9XwUN3KzJqvzXAlzdNYTo&code_challenge_method=S256',
-                        position: 'right',
-                    
-                },
+                   
                     {
                         label: 'Try Now',
                         className: 'tryNow',
