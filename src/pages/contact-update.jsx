71c333bc879fc6953da85099df82dc29abc9dca6
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-04-22 12:35:26 +0100

    V2 Draft

--- src/pages/contact-update.jsx
+++ src/pages/contact-update.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import { MetaSEO } from '/src/theme/MetaSEO';
 import Head from '@docusaurus/Head';
-import StickyHeader from '/src/components/ContactUpdate/Navigation';
-import Header from '/src/components/ContactUpdate/Header';
-import Studies from '/src/components/ContactUpdate/Studies';
-import Main from '/src/components/ContactUpdate/Main';
-import Integrations from '/src/components/ContactUpdate/IntegrationsUpdate/index.jsx';
-import WhatYouCanDo from '/src/components/ContactUpdate/WhatYouCanDoUpdate/index.jsx';
-import FAQ from '/src/components/ContactUpdate/FAQ/index.jsx';
-import Testimonials from '/src/components/ContactUpdate/Testimonials';
-import ContactFrom from '/src/components/ContactUpdate/contactForm.jsx';
-import Footer from '/src/components/ContactUpdate/Footer';
+import StickyHeader from '/src/components/ContactUpdate/V2/Navigation';
+import Header from '/src/components/ContactUpdate/V2/Header';
+import Studies from '/src/components/ContactUpdate/V2/Studies';
+import Main from '/src/components/ContactUpdate/V2/Main';
+import Benefits from '/src/components/ContactUpdate/V2/Benefits';
+import WhatYouCanDo from '/src/components/ContactUpdate/V2/WhatYouCanDoUpdate/index.jsx';
+import FAQ from '/src/components/ContactUpdate/V2/FAQ/index.jsx';
+import Testimonials from '/src/components/ContactUpdate/V2/Testimonials';
+import ContactFrom from '/src/components/ContactUpdate/V2/contactForm.jsx';
+import Footer from '/src/components/ContactUpdate/V2/Footer';
 import ThemeSwitch from '/src/components/ThemeSwitch';
 import CTA from '/src/components/Home/Redesign/CTA';
 
@@ -28,14 +28,14 @@ export default function ContactPage() {
       <MetaSEO img="og/content/contact.jpg" />
       <StickyHeader />
       <Header />
-      <Studies />
+
       <Main />
-      <WhatYouCanDo />
-      <Integrations />
-      <FAQ />
+      <Benefits />
+
       <Testimonials />
+      <FAQ />
+
       <ContactFrom />
-      <Footer />
 
       <ThemeSwitch />
     </div>
