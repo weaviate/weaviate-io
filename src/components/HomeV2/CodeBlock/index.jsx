90344ad0480bfb0096e4e7c5f9c06d83a036b972
Sebastian Witalec <sebawita@users.noreply.github.com>
2025-06-23 15:20:53 +0200

    update coding examples

--- src/components/HomeV2/CodeBlock/index.jsx
+++ src/components/HomeV2/CodeBlock/index.jsx
@@ -3,25 +3,7 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/nightOwl';
 import styles from './styles.module.scss';
 
-let vectorizing = `from weaviate.classes.config import Configure
-
-# Choose one of the many built-in vectorizers
-collection = client.collections.create(
-    "SupportTickets",
-    vectorizer_config=Configure.Vectorizer.text2vec_weaviate(
-        model="Snowflake/snowflake-arctic-embed-l-v2.0"
-    )
-)
-
-# Insert data in bulk
-collection.data.insert_many([
-    {
-        "title": "Cannot reset password",
-        "content": "User is unable to reset their password using the provided link."
-    }
-])`;
-
-let searching = `# Retrieve the collection
+let searchExample = `# Retrieve the collection
 collection = client.collections.get("SupportTickets")
 
 # Pure vector search
@@ -41,29 +23,87 @@ response = collection.query.hybrid(
     query="login issues after OS upgrade",
     alpha=0.75,
     limit=5
-)`;
+)`
 
-let tenanting = `from weaviate.classes.config import Configure
+let vectorizerExample = `from weaviate.classes.config import Configure
 
-# Optimize for speed (memory) or storage (disk)
-memo_idx = Configure.VectorIndex.flat()
-disk_idx = Configure.VectorIndex.hnsw()
-
-# Create a multi tenancy set-up
+# Choose one of the many built-in vectorizers
 collection = client.collections.create(
-    name="SupportTickets",
-    vector_index_config=disk_idx,
-    multi_tenancy_config=Configure.multi_tenancy()
+    "SupportTickets",
+    vectorizer_config=Configure.Vectorizer.text2vec_weaviate(
+        model="Snowflake/snowflake-arctic-embed-l-v2.0"
+    )
 )
 
+# Insert data in bulk
+collection.data.insert_many([
+    {
+        "title": "Cannot reset password",
+        "content": "User is unable to reset their password using the provided link."
+    }
+])`
+
+const ragExample = `from weaviate.classes.generate import GenerativeConfig
+
+# Select collection
+supportTickets = client.collections.get("SupportTicket")
+
+# Generate an RAG response by piping
+# results through a generative model
+response = supportTickets.generate.near_text(
+    query="Solutions to OS login bug",
+    auto_limit=1,
+    generative_provider=GenerativeConfig.openai(
+        model="gpt-4o-mini",
+    ),
+    grouped_task="How do I fix my latest OS login bug?"
+)`
+
+const tenantExample = `# Create a multi-tenanant collection
+emails = client.collections.create(
+    name="CustomerEmails",
+    vectorizer_config=Configure.Vectorizer.text2vec_weaviate(),
+    multi_tenancy_config=Configure.multi_tenancy(
+        enabled=True,
+        auto_tenant_creation=True
+    )
+)
+
+# Manage tenant data and query
+acmeTenant = emails.tenants.get("ACME")
+acmeTenant.data.insert_many(acme_data)
+
+response = acmeTenant.query.near_text(query="ACME question", limit=3)
+
 # Offload to S3
-collection.tenants.offload("ACME")
+emails.tenants.offload("ACME")
 
-# Active from S3
-collection.tenants.activate("StarkIndustries")
-`;
+# Activate from S3
+emails.tenants.activate("StarkIndustries")`
 
-let agenting = `from weaviate.agents.classes import Operations
+const vectorIndexExample = `# Pick your vector indexing strategy
+memory_index = Configure.VectorIndex.hnsw() # Use HNSW for lightning-fast in-memory search
+disk_index = Configure.VectorIndex.flat()   # Use Flat for fast, disk-based indexing
+
+# Set the index type for your collection in one line
+client.collections.create(
+    name="Products",
+    vectorizer_config=Configure.Vectorizer.text2vec_weaviate(),
+    vector_index_config=disk_index
+)
+
+# Set vector compression to reduce memory footprint
+client.collections.create(
+    name="ProductManuals",
+    vectorizer_config=Configure.Vectorizer.text2vec_weaviate(),
+
+    # Use Product Quantization to save 75% of required RAM
+    vector_index_config=Configure.VectorIndex.hnsw(
+        quantizer=Configure.VectorIndex.Quantizer.pq()
+    ),
+)`
+
+const agentExample = `from weaviate.agents.classes import Operations
 from weaviate.collections.classes.config import DataType
 from weaviate.agents.transformation import TransformationAgent
 
@@ -83,30 +123,15 @@ agent = TransformationAgent(
 )
 
 # Run the transformation on all objects in the collection
-agent.update_all()`;
-
-let rag = `from weaviate.classes.generate import GenerativeConfig
-
-# Select collection
-supportTickets = client.collections.get("SupportTicket")
-
-# Generate an RAG response by piping
-# results through a generative model
-response = supportTickets.generate.near_text(
-    query="Solutions to OS login bug",
-    auto_limit=1,
-    generative_provider=GenerativeConfig.openai(
-        model="gpt-4o-mini",
-    ),
-    grouped_task="How do I fix my latest OS login bug?"
-)`
+agent.update_all()`
 
 const codeExamples = {
-  search: searching,
-  vectorize: vectorizing,
-  RAG: rag,
-  tenants: tenanting,
-  agents: agenting,
+  search: searchExample,
+  vectorize: vectorizerExample,
+  RAG: ragExample,
+  tenants: tenantExample,
+  agents: agentExample,
+  index: vectorIndexExample,
 };
 
 export default function CodeTabs() {
