6b852f4673b1598150e2c1061d7159bf65f1d7f5
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-07-23 10:01:13 +0100

    Meta Update

--- src/pages/cloud.jsx
+++ src/pages/cloud.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import Head from '@docusaurus/Head';
 
 const CloudPage = () => {
   return (
     <>
+      <Head>
+        <meta name="robots" content="noindex" />
+        <link rel="canonical" href="https://console.weaviate.cloud" />
+        <title>Redirecting to Console...</title>
+      </Head>
+
       <BrowserOnly
         fallback={
           <div className="loadingIcon">
             <svg
               width="50px"
               height="50px"
-              xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 200 200"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <circle
                 fill="#130C49"
@@ -29,7 +36,7 @@ const CloudPage = () => {
                   keySplines=".5 0 .5 1;.5 0 .5 1"
                   repeatCount="indefinite"
                   begin="-.4"
-                ></animate>
+                />
               </circle>
               <circle
                 fill="#130C49"
@@ -47,7 +54,7 @@ const CloudPage = () => {
                   keySplines=".5 0 .5 1;.5 0 .5 1"
                   repeatCount="indefinite"
                   begin="-.2"
-                ></animate>
+                />
               </circle>
               <circle
                 fill="#130C49"
@@ -65,22 +72,22 @@ const CloudPage = () => {
                   keySplines=".5 0 .5 1;.5 0 .5 1"
                   repeatCount="indefinite"
                   begin="0"
-                ></animate>
+                />
               </circle>
             </svg>
           </div>
         }
       >
         {() => {
-          window.location.href = `https://console.weaviate.cloud`;
+          window.location.href = 'https://console.weaviate.cloud';
 
           return (
             <div className="loadingIcon">
               <svg
                 width="50px"
                 height="50px"
-                xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 200 200"
+                xmlns="http://www.w3.org/2000/svg"
               >
                 <circle
                   fill="#130C49"
@@ -98,7 +105,7 @@ const CloudPage = () => {
                     keySplines=".5 0 .5 1;.5 0 .5 1"
                     repeatCount="indefinite"
                     begin="-.4"
-                  ></animate>
+                  />
                 </circle>
                 <circle
                   fill="#130C49"
@@ -116,7 +123,7 @@ const CloudPage = () => {
                     keySplines=".5 0 .5 1;.5 0 .5 1"
                     repeatCount="indefinite"
                     begin="-.2"
-                  ></animate>
+                  />
                 </circle>
                 <circle
                   fill="#130C49"
@@ -134,7 +141,7 @@ const CloudPage = () => {
                     keySplines=".5 0 .5 1;.5 0 .5 1"
                     repeatCount="indefinite"
                     begin="0"
-                  ></animate>
+                  />
                 </circle>
               </svg>
             </div>
