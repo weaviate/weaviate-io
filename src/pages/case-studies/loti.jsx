439fb14d89ccbcd745e3cc287eb9a828b2519f4d
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2025-08-06 17:34:31 +0100

    Draft

---
+++ src/pages/case-studies/loti.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '@theme/Layout';
+import StudyHeader from '../../components/Service/Loti/Header';
+import ContactUsForm from '../../components/ContactUsForm';
+import Study from '../../components/Service/Loti/Study';
+import ThemeSwitch from '/src/components/ThemeSwitch';
+import Integrations from '../../components/Service/CaseStudy/Integrations';
+
+export default function CaseStudyPage() {
+  return (
+    <div className="custom-page noBG">
+      <Layout
+        title="Case Study - Loti"
+        description="How Loti AI fights likeness infringement and digital impersonation with one of the world’s largest vector database deployments"
+      >
+        <StudyHeader />
+        <Study />
+        <Integrations />
+        <ContactUsForm />
+      </Layout>
+      <ThemeSwitch />
+    </div>
+  );
+}
