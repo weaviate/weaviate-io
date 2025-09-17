60a5549cdbc56cb125f7fc4c73a5cbba5292908f
Dennis Schipper <dennis@designfordetails.com>
2025-09-17 15:28:40 +0100

    Price Calculator

---
+++ src/components/Pricing/PriceCalculator/PriceCalculator.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import './styles/index.scss'
+import { RecallAccuracy, Throughput, Plans, VectorDimensionsSelect, NumberOfObjectsSelect } from "./components/Elements/Calculator"
+import { PriceReview } from "./components/Elements/PriceReview/PriceReview"
+import { ObjectSizeSelect } from "./components/Elements/Calculator"
+import { IData, starterState } from "./types"
+
+export const PriceCalculator = () => {
+  const [ data, updateData ] = useState<IData>(starterState)
+  const updateValue = (n: string, v: string) => updateData({ ...data, [n]: v })
+
+  return (
+    <div className="priceCalculator">
+      <div className="content">
+        <h1>Estimate costs with Pricing Calculator</h1>
+        <Plans data={data} updateValue={updateValue} />
+        <div className="rowWrapper">
+          <VectorDimensionsSelect data={data} updateValue={updateValue} />
+          <NumberOfObjectsSelect data={data} updateValue={updateValue} />
+          <ObjectSizeSelect data={data} updateValue={updateValue} />
+        </div>
+        <RecallAccuracy data={data} updateValue={updateValue} />
+        <Throughput data={data} updateValue={updateValue} />
+      </div>
+      <PriceReview data={data} />
+    </div>
+  )
+}
+
+// Unused sliders.
+// <VectorDimensions data={data} updateValue={updateValue} />
+// <NumOfObjects data={data} updateValue={updateValue} />
+// <ObjectSize data={data} updateValue={updateValue} />
+// <DataTiering data={data} updateValue={updateValue} />
---
+++ src/components/Pricing/PriceCalculator/components/Elements/Price/Price.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import useMeasure from 'react-use-measure'
+import { AnimatePresence, motion } from "framer-motion"
+import type { MotionProps } from "framer-motion"
+import { calculatePrice } from "../../../helpers/priceHelper"
+import type { IData } from "../../../types"
+
+interface IPriceProps {
+  data: IData
+}
+
+export const Price = (props: IPriceProps) => {
+  const [ ref, bounds ] = useMeasure({ debounce: 100 })
+  
+  const motionProps: MotionProps = {
+    initial: { opacity: 0, y: -20, filter: 'blur(10px)' },
+    animate: { opacity: 1, y: 0, filter: 'blur(0px)' },
+    exit: { opacity: 0, y: 20, filter: 'blur(10px)' },
+    transition: { duration: 0.33 }
+  }
+
+  const price = calculatePrice(props.data).toLocaleString()
+
+  const wrapperMotionProps: MotionProps = {
+    animate: { width: Math.max(bounds.width, 40) + 20 }
+  }
+
+  // If the number is too large, we need to resize the font to fit it's container.
+  const className = `priceContainer ${price.length > 5 ? 'large' : ''}`
+
+  return (
+    <div className={`price `}>
+      <span className="small">$</span>
+      <motion.div className="priceWrapper" {...wrapperMotionProps}>
+        <AnimatePresence>
+          <motion.span className={className} {...motionProps} key={price} ref={ref}>
+            {price}
+          </motion.span>
+        </AnimatePresence>
+      </motion.div>
+      <span className="small">/mo</span>
+    </div>
+  )
+}
