13d9440cab84a0a9dfcb2a2b262728fb3111d043
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2026-01-12 17:00:39 +0000

    Broken Links

--- src/components/Javascript/BWW/index.jsx
+++ src/components/Javascript/BWW/index.jsx
@@ -24,36 +24,6 @@ export default function BuildWith() {
             </h3>
             <div className={styles.boxImage}></div>
           </div>
-          <div className={styles.innerContainer}>
-            <div className={`${styles.smallBox} ${styles.light}`}>
-              <h3>RicAI</h3>
-              <p>
-                An Autonomous AI Agent for automating software testing,
-                generating test cases and performing various tests.
-              </p>
-
-              <Link
-                to="/community/build-with-weaviate"
-                className={styles.boxButton}
-              >
-                {"See the showcase >"}
-              </Link>
-            </div>
-            <div className={`${styles.smallBox} ${styles.dark}`}>
-              <h3>Internship Finder</h3>
-              <p>
-                An interactive web application to help you discover internship
-                opportunities tailored to your resume!
-              </p>
-
-              <Link
-                to="/community/build-with-weaviate"
-                className={styles.boxButton}
-              >
-                {"See the showcase >"}
-              </Link>
-            </div>
-          </div>
         </div>
       </div>
     </div>
