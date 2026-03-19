3a13e572b59eb50da3d882ae311df6db723b789b
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2026-03-19 13:27:11 +0000

    Dev Config Update

--- docusaurus.dev.config.js
+++ docusaurus.dev.config.js
@@ -1,16 +1,27 @@
-let config = require('./docusaurus.config')
+// @ts-check
 
+/** @param {unknown} plugin */
 const isRedirectPlugin = (plugin) => {
-    // the redirect plugin is an array
-    // with '@docusaurus/plugin-client-redirects' as the first item
-    return Array.isArray(plugin) && plugin[0] == '@docusaurus/plugin-client-redirects';
-}
+  return (
+    Array.isArray(plugin) &&
+    typeof plugin[0] === 'string' &&
+    plugin[0] === '@docusaurus/plugin-client-redirects'
+  );
+};
 
-// remove the redirect plugin, to esure that
-// we don't use redirects for links within our docs
-config.plugins = config.plugins.filter(plugin => !isRedirectPlugin(plugin));
+module.exports = async function createDevConfigAsync() {
+  const loadConfig = require('./docusaurus.config');
 
-// use trailing slash, to be able to use link checker on a local build folder
-config.trailingSlash = true;
+  const config =
+    typeof loadConfig === 'function'
+      ? await loadConfig()
+      : loadConfig;
 
-module.exports = config;
+  config.plugins = (config.plugins || []).filter(
+    (plugin) => !isRedirectPlugin(plugin)
+  );
+
+  config.trailingSlash = true;
+
+  return config;
+};
