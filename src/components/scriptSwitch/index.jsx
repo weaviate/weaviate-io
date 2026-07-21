a73657821729e4881114d21798df8380031b635b
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2026-07-21 16:52:52 +0100

    Src Upload (#3661)

--- src/components/scriptSwitch/index.jsx
+++ src/components/scriptSwitch/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const CustomScriptLoader = () => {
   useEffect(() => {
@@ -7,23 +7,23 @@ const CustomScriptLoader = () => {
         .querySelectorAll('img[referrerPolicy="no-referrer-when-downgrade"]')
         .forEach((el) => el.remove());
 
-      const script = document.createElement('img');
+      const script = document.createElement("img");
       script.src = src;
-      script.referrerPolicy = 'no-referrer-when-downgrade';
-      script.style = 'display: none;';
+      script.referrerPolicy = "no-referrer-when-downgrade";
+      script.style = "display: none;";
       document.body.appendChild(script);
     };
 
     const getScriptForPath = (pathname) => {
       const scriptsMap = {
-        '/developers/':
-          'https://static.scarf.sh/a.png?x-pxid=2758e82f-6546-4356-a8bd-5b5c16368efb',
-        '/pricing':
-          'https://static.scarf.sh/a.png?x-pxid=5c79460c-47af-4477-a1d9-3624dcce35d3',
+        "/developers/":
+          "https://pixel.weaviate.cloud/a.png?x-pxid=2758e82f-6546-4356-a8bd-5b5c16368efb",
+        "/pricing":
+          "https://pixel.weaviate.cloud/a.png?x-pxid=5c79460c-47af-4477-a1d9-3624dcce35d3",
       };
 
       const defaultScript =
-        'https://static.scarf.sh/a.png?x-pxid=a41b0758-a3a9-4874-a880-8b5d5a363d40';
+        "https://pixel.weaviate.cloud/a.png?x-pxid=a41b0758-a3a9-4874-a880-8b5d5a363d40";
 
       // Check for a direct match or base path match (for handling subpages)
       for (const [basePath, script] of Object.entries(scriptsMap)) {
