80f9e11337c130655e4f7939f5c95bba794b5352
Shan-Weaviate <137914800+Shan-Weaviate@users.noreply.github.com>
2026-09-16 16:42:53 +0100

    Event Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -516,7 +516,7 @@ module.exports = async function createConfigAsync() {
             
              announcementBar: {
                id: 'announcement-bar-hfresh-ga',
-                content:`<a href="/blog/hfresh">Introducing HFresh  - Read our Blog </a>`,
+                content:`<a href="https://luma.com/5mw2w4n8?lm_source=embed">Inside Weaviate's MCP Ecosystem  - Register for the webinar </a>`,
                backgroundColor: '#160F52',
                 textColor: '#F5F5F5',
                 isCloseable: true,
